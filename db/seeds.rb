f9bad0f88be07f72a6bf60c26acad0526a5ab2ef
marvinick <marviluw@gmail.com>
2013-11-06 23:21:08 -0600

    pre course again

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Futurama = Video.create(title: 'Futurama', description: 'Never watched it', small_cover_url: '/tmp/futurama.jpg', large_cover_url: '/tmp/monk_large.jpg')
+Monk = Video.create(title: 'Futurama', description: 'Almost famous', small_cover_url: '/tmp/monk.jpg', large_cover_url: '/tmp/monk_large.jpg')
