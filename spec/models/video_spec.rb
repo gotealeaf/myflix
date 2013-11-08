3b3b040325ca4f7f6047f2f91ae59ed907f3eeab
Gina Zhou <ginayizhou@gmail.com>
2013-11-07 17:18:11 -0800

    trying to fix search_by_title of mod2

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -7,14 +7,14 @@ describe Video do
 
   describe 'search_by_title' do
     it 'returns empty array if no videos match' do
-      family_guy = Video.create(title: 'Family Guy')
-      breaking_bad = Video.create(title: 'Breaking Bad')
+      family_guy = Video.create(title: 'Family Guy', description: 'In a wacky Rhode Island town, a dysfunctional family strive to cope with everyday life as they are thrown from one crazy scenario to another.')
+      breaking_bad = Video.create(title: 'Breaking Bad', description: "To provide for his family's future after he is diagnosed with lung cancer, a chemistry genius turned high school teacher teams up with an ex-student to cook and sell the world's purest crystal meth.")
       expect(Video.search_by_title('monk')).to eq([])
     end
 
     it 'returns an array of one video for an exact match' do
-      family_guy = Video.create(title: 'Family Guy')
-      breaking_bad = Video.create(title: 'Breaking Bad')
+      family_guy = Video.create(title: 'Family Guy', description: 'In a wacky Rhode Island town, a dysfunctional family strive to cope with everyday life as they are thrown from one crazy scenario to another.')
+      breaking_bad = Video.create(title: 'Breaking Bad', description: "To provide for his family's future after he is diagnosed with lung cancer, a chemistry genius turned high school teacher teams up with an ex-student to cook and sell the world's purest crystal meth.")
       expect(Video.search_by_title('Family')).to eq([family_guy])
     end
 
