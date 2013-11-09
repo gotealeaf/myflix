3652e02896760945a0a0fab01351a043cd7f4b98
Jordan <eightyfour@gmail.com>
2013-11-09 14:19:18 -0500

    added search  method

--- app/models/video.rb
+++ app/models/video.rb
@@ -2,4 +2,9 @@ class Video < ActiveRecord::Base
   validates :name, presence: true
   validates :description, presence: true
   belongs_to :category
+  
+  def self.search_by_name(search_term)
+    return [] if search_term.empty?
+    where("name like ?", "%#{search_term}%")
+  end
 end
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -4,4 +4,16 @@ describe Video do
   it { should belong_to(:category) }
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:description) }
+  
+  it "returns an empty array if no results found" do
+    Video.create(name: "epic", description: "Also epic")
+    Video.search_by_name("chump").should == []
+  end
+  
+  it "returns an array when you search for an item" do
+    Video.create(name: "epic", description: "Also epic")
+    Video.create(name: "double", description: "Also epic")
+    Video.create(name: "epic", description: "Also epic")
+    Video.search_by_name("epic").length.should == 2
+  end
 end
