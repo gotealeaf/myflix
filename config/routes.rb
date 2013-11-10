3df506c31837c2a8994375f489606716efc5becf
Gina Zhou <ginayizhou@gmail.com>
2013-11-10 14:11:43 -0800

    updated routes and spec files (DRY)

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Myflix::Application.routes.draw do
   get '/home', to: 'videos#index'
   get '/register', to: 'users#new'
   get '/sign_in', to: 'sessions#new'
+  post '/sign_in', to: 'sessions#create'
   get '/sign_out', to: 'sessions#destroy'
 
   resources :videos do
@@ -16,6 +17,4 @@ Myflix::Application.routes.draw do
   resources :categories
 
   resources :users, only: [:create]
-
-  resources :sessions, only: [:create]
 end
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -6,33 +6,28 @@ describe Video do
   it { should validate_presence_of(:description) }
 
   describe 'search_by_title' do
+    before :each do
+      @family_guy = Video.create(title: 'Family Guy', description: 'In a wacky Rhode Island town, a dysfunctional family strive to cope with everyday life as they are thrown from one crazy scenario to another.', created_at: 1.day.ago)
+      @breaking_bad = Video.create(title: 'Breaking Bad', description: "To provide for his family's future after he is diagnosed with lung cancer, a chemistry genius turned high school teacher teams up with an ex-student to cook and sell the world's purest crystal meth.")
+    end
+
     it 'returns empty array if no videos match' do
-      family_guy = Video.create(title: 'Family Guy', description: 'In a wacky Rhode Island town, a dysfunctional family strive to cope with everyday life as they are thrown from one crazy scenario to another.')
-      breaking_bad = Video.create(title: 'Breaking Bad', description: "To provide for his family's future after he is diagnosed with lung cancer, a chemistry genius turned high school teacher teams up with an ex-student to cook and sell the world's purest crystal meth.")
       expect(Video.search_by_title('monk')).to eq([])
     end
 
     it 'returns an array of one video for an exact match' do
-      family_guy = Video.create(title: 'Family Guy', description: 'In a wacky Rhode Island town, a dysfunctional family strive to cope with everyday life as they are thrown from one crazy scenario to another.')
-      breaking_bad = Video.create(title: 'Breaking Bad', description: "To provide for his family's future after he is diagnosed with lung cancer, a chemistry genius turned high school teacher teams up with an ex-student to cook and sell the world's purest crystal meth.")
-      expect(Video.search_by_title('Family')).to eq([family_guy])
+      expect(Video.search_by_title('Family')).to eq([@family_guy])
     end
 
     it 'returns an array of one video for a partial match' do
-      family_guy = Video.create(title: 'Family Guy', description: 'In a wacky Rhode Island town, a dysfunctional family strive to cope with everyday life as they are thrown from one crazy scenario to another.')
-      breaking_bad = Video.create(title: 'Breaking Bad', description: "To provide for his family's future after he is diagnosed with lung cancer, a chemistry genius turned high school teacher teams up with an ex-student to cook and sell the world's purest crystal meth.")
-      expect(Video.search_by_title('reak')).to eq([breaking_bad])
+      expect(Video.search_by_title('reak')).to eq([@breaking_bad])
     end
 
-    it 'returns an array of all matches ordered by created_at' do 
-      family_guy = Video.create(title: 'Family Guy', description: 'In a wacky Rhode Island town, a dysfunctional family strive to cope with everyday life as they are thrown from one crazy scenario to another.')
-      breaking_bad = Video.create(title: 'Breaking Bad', description: "To provide for his family's future after he is diagnosed with lung cancer, a chemistry genius turned high school teacher teams up with an ex-student to cook and sell the world's purest crystal meth.", created_at: 1.day.ago)
-      expect(Video.search_by_title('a')).to eq([family_guy, breaking_bad])
+    it 'returns an array of all matches ordered by created_at DESC' do 
+      expect(Video.search_by_title('a')).to eq([@breaking_bad, @family_guy])
     end
 
     it 'returns an empty array for a search with an empty string' do
-      family_guy = Video.create(title: 'Family Guy', description: 'In a wacky Rhode Island town, a dysfunctional family strive to cope with everyday life as they are thrown from one crazy scenario to another.')
-      breaking_bad = Video.create(title: 'Breaking Bad', description: "To provide for his family's future after he is diagnosed with lung cancer, a chemistry genius turned high school teacher teams up with an ex-student to cook and sell the world's purest crystal meth.")
       expect(Video.search_by_title('')).to eq([])
     end
   end
