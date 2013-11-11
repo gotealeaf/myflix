66b7ba911fad1537f96b40489481ff067e3491ac
Mark <hustada80@gmail.com>
2013-11-11 15:35:46 -0600

    search by title with test

--- app/models/video.rb
+++ app/models/video.rb
@@ -2,6 +2,10 @@ class Video < ActiveRecord::Base
   belongs_to :category
   validates :title, presence: true
   validates :description, presence: true
+
+
+ def self.search_by_title(search_term)
+   where("title LIKE ?", "#{search_term}%")
+ end
 end
 
-//comment
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -4,5 +4,50 @@ describe Video do
   it {should belong_to(:category)}
   it {should validate_presence_of(:title)}
   it {should validate_presence_of(:description)}
+
+
+  describe "search_by_title" do
+    it "returns and empty array if there is no match" do
+      south_park = Video.create(
+        title:"South Park", 
+        description: "Cartman")
+      robocop = Video.create(
+        title:"Robocop", 
+        description:"A action movie from the 80's about cheesy looking robot cop")
+      expect(Video.search_by_title("Suckas")).to eq([])
+  end
+
+    it "returns an array of one video for an exact match" do
+      south_park = Video.create(
+        title:"South Park", 
+        description: "Cartman")
+      robocop = Video.create(
+        title:"Robocop", 
+        description:"A action movie from the 80's about cheesy looking robot cop")
+      expect(Video.search_by_title("South Park")).to eq([south_park])
+    end
+
+      it "returns an array of one video for a partial match" do
+      south_park = Video.create(
+        title:"South Park", 
+        description: "Cartman")
+      robocop = Video.create(
+        title:"Robocop", 
+        description:"A action movie from the 80's about cheesy looking robot cop")
+      expect(Video.search_by_title("sou")).to eq([south_park])
+    end
+
+      it "returns an array of all matches ordered by created_at" do
+      south_park = Video.create(
+        title:"South Park", 
+        description: "Cartman")
+      robocop = Video.create(
+        title:"Robocop", 
+        description:"A action movie from the 80's about cheesy looking robot cop")
+      expect(Video.search_by_title("sou")).to eq([south_park])
+    end
+  end
 end
+
+
 
