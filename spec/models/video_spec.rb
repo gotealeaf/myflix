d879258aff104d3f9e927c2af2fdf500343d4399
Mark <hustada80@gmail.com>
2013-11-10 20:54:28 -0600

    testing validations

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -13,4 +13,20 @@ it "belongs to category" do
   monk = Video.create(title:'Monk', description:'About an OCD detective', category: dramas)
   expect(monk.category).to eq(dramas)
     end
+
+  
+it "does not save a video without a title" do
+  video = Video.create(description: "A great video")
+  expect(Video.count).to eq(0)
 end
+
+
+
+it "does not save a video without a description" do
+  video = Video.create(title:"Out of the Silent Planet")
+  expect(Video.count).to eq(0)
+end
+end
+
+
+
