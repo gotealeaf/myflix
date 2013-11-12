163d0d00d7520bcd85298d86b1fd4252aa3dd3a9
Denny Santoso <dennysantoso.com@gmail.com>
2013-11-12 19:47:09 +0700

    refactor sessions test

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -13,36 +13,31 @@ describe SessionsController do
     end
   end
   describe "POST create" do
-    context "with valid credentials" do
+    let(:alice) { Fabricate(:user) }
+    context "with valid credentials" do      
       it "puts the signed in user in the session" do
-        alice = Fabricate(:user)
         post :create, email: alice.email, password: alice.password
         expect(session[:user_id]).to eq(alice.id)
       end
       it "redirects to the home page" do
-        alice = Fabricate(:user)
         post :create, email: alice.email, password: alice.password
         expect(response).to redirect_to home_path
       end
       it "set the notice" do
-        alice = Fabricate(:user)
         post :create, email: alice.email, password: alice.password
         expect(flash[:notice]).not_to be_blank
       end
     end
     context "with invalid credentials" do
       it "doesn't put the user into the session" do
-        alice = Fabricate(:user)
         post :create, email: alice.email, password: 'admin'
         expect(session[:user_id]).to be_nil
       end
       it "redirects to sign in path" do
-        alice = Fabricate(:user)
         post :create, email: alice.email, password: 'admin'
         expect(response).to redirect_to sign_in_path
       end
       it "sets the error message" do
-        alice = Fabricate(:user)
         post :create, email: alice.email, password: 'admin'
         expect(flash[:error]).not_to be_blank
       end
