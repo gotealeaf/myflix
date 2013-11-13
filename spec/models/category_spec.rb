21274ec01d11f6ccd239ef55c513e030c8d295aa
Mark <hustada80@gmail.com>
2013-11-13 13:53:00 -0600

    category tests for recent video search

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -2,4 +2,36 @@ require 'spec_helper'
 
 describe Category do
   it {should have_many(:videos)}
+
+  describe "#recent_videos" do
+    it "returns the videos in reverse chronological order by created at" do
+    comedies = Category.create(name: "comedies")
+    family_guy = Video.create(title:"Family Guy", description:"funny cartoon", category: comedies, created_at: 1.day.ago)
+    south_park = Video.create(title:"South Park", description:"funny cartoon", category: comedies)
+    expect(comedies.recent_videos).to eq([south_park, family_guy])
+  end
+
+    it "returns all the videos if there are less than 6 videos" do
+    comedies = Category.create(name: "comedies")
+    family_guy = Video.create(title:"Family Guy", description:"funny cartoon", category: comedies, created_at: 1.day.ago)
+    south_park = Video.create(title:"South Park", description:"funny cartoon", category: comedies)
+    expect(comedies.recent_videos.count).to eq(2)
+  end
+
+    it "returns 6 videos if there are more than 6 videos" do
+    comedies = Category.create(name: "comedies")
+    7.times {Video.create(title: "foo", description:"bar",category: comedies)}
+    expect(comedies.recent_videos.count).to eq(6)
+  end
+    it "returns the most recent 6 videos" do
+    comedies = Category.create(name: "comedies")
+    6.times {Video.create(title: "foo", description:"bar",category: comedies)}
+    friends = Video.create(title:"Friends", description:"funny sitcom", category: comedies, created_at: 1.day.ago)
+    expect(comedies.recent_videos).not_to include(friends)
+  end
+    it "returns an empty array if the category does not have any videos" do
+      comedies = Category.create(name:"comedies")
+      expect(comedies.recent_videos).to eq([])
+    end
+  end
 end
