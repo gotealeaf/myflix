1ed3d74801c921226cd0c721f0bb12ca7c36c35d
Mark <hustada80@gmail.com>
2013-11-13 16:12:22 -0600

    show only recent videos on UI

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,7 @@ comedies = Category.create(name: 'Comedies')
 dramas = Category.create(name: 'Dramas')
 reality = Category.create(name: 'Reality')
 documentaries = Category.create(name: 'Documentaries')
-
+scifi = Category.create(name:'Scifi')
 
 Loose_Change = Video.create(
 title:'Loose Change Final Cut',
@@ -48,4 +48,40 @@ description: 'Hilarious animated sitcom about a truly dysfunctional family from 
 small_cover_url: 'tmp/family_guy.jpg', 
 large_cover_url: 'tmp/family_guy.jpg', 
 category: comedies)
+
+Fringe = Video.create(
+title: 'Fringe', 
+description: 'Fringe is an American science fiction television series created by J. J. Abrams, Alex Kurtzman, and Roberto Orci', 
+small_cover_url: 'tmp/fringe.jpg', 
+large_cover_url: 'tmp/fringe_large.jpg', 
+category: scifi)
+
+Modern_Family = Video.create(
+title: 'Modern Family', 
+description: 'Told from the perspective of an unseen documentary filmmaker, the series offers an honest and often hilarious perspective of family life', 
+small_cover_url: 'tmp/modern_family.jpg', 
+large_cover_url: 'tmp/modern_family_large.jpg', 
+category: comedies)
+
+Big_Bang_Theory = Video.create(
+title: 'Big Bang Theory', 
+description: 'The Big Bang Theory is a comedy series about four young scientists who know all about the world of physics, and one girl', 
+small_cover_url: 'tmp/big_bang_theory.jpg', 
+large_cover_url: 'tmp/big_bang_theory_large.jpg', 
+category: comedies)
+
+How_I_Met_Your_Mother = Video.create(
+title: 'How I Met Your Mother', 
+description: 'Sitcom series that follows main character, Ted Mosby, and his group of friends in Manhattan', 
+small_cover_url: 'tmp/met_mother.jpg', 
+large_cover_url: 'tmp/met_mother_large.jpg', 
+category: comedies)
+
+Burning_Love = Video.create(
+title: 'Burning Love', 
+description: 'A scripted comedy series which is a web spoof of the television shows The Bachelor, The Bachelorette and Bachelor Pad', 
+small_cover_url: 'tmp/burning_love.jpg', 
+large_cover_url: 'tmp/burning_love_large.jpg', 
+category: comedies)
+
 
