0260e04b057f3857eb73113eef3cf8be25201cc8
Mark <hustada80@gmail.com>
2013-11-13 00:19:32 -0600

    modified a bit

--- app/models/video.rb
+++ app/models/video.rb
@@ -5,7 +5,8 @@ class Video < ActiveRecord::Base
 
 
  def self.search_by_title(search_term)
-   where("title LIKE ?", "#{search_term}%")
+   return [] if search_term.blank?
+   where("title LIKE ?", "#{search_term}%").order("created_at DESC")
  end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Myflix::Application.routes.draw do
   root to: 'videos#index'
-
-  get '/home', to: 'videos#index'
-  get '/genre', to: 'categories#index'
-  resources :categories
-
   resources :videos, only: [:show] do
     collection do
     post :search, to: "videos#search"
