5c8655178b4e54e154546755de9c5902da5cd44b
Denny Santoso <dennysantoso.com@gmail.com>
2013-11-14 22:58:23 +0700

    video spec add have many

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -5,6 +5,7 @@ describe Video do
   it { should belong_to(:category) }
   it { should validate_presence_of(:title) }
   it { should validate_presence_of(:description) }
+  it { should have_many(:reviews) }
   describe ".search_by_title" do
     it "returns empty array if no match in search result" do
       monk = Video.create(title: "Monk", description: "This is monk movie")
