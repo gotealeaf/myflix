1064f147f27a2d9ed3f2078c6e046d28191e1c34
Jordan <eightyfour@gmail.com>
2013-11-13 22:02:28 -0500

    refactored authentication tests

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -15,50 +15,58 @@ describe SessionsController do
 
 	describe "POST create" do
 		context "with valid credentials" do
+			before do
+				@jordan = Fabricate(:user)
+				post :create, email: @jordan.email, password: @jordan.password
+			end
+
 			it "creates a session" do
-				jordan = Fabricate(:user)
-				post :create, email: jordan.email, password: jordan.password
-				expect(session[:user_id]).to eq(jordan.id)
+				expect(session[:user_id]).to eq(@jordan.id)
 			end 
 			
 			it "redirects to the videos path" do
-				jordan = Fabricate(:user)
-				post :create, email: jordan.email, password: jordan.password
 				expect(response).to redirect_to(videos_path)
 			end
 
 			it "sets a welcome notice" do
-				jordan = Fabricate(:user)
-				post :create, email: jordan.email, password: jordan.password
 				expect(flash[:notice]).not_to be_blank
 			end
 		end
 
 		context "with invalid credentials"  do
-			it "does not put the signed in user into the session" do
+			before do
 				jordan = Fabricate(:user)
 				post :create, email: jordan.email, password: jordan.password + "1"
+			end
+			it "does not put the signed in user into the session" do
 				expect(session[:user_id]).to be(nil)
 			end
 			it "gives an error" do
-				jordan = Fabricate(:user)
-				post :create, email: jordan.email, password: jordan.password + "1" 
 				expect(flash[:error]).to_not be_blank
 			end
 			it "renders the new template" do
-				jordan = Fabricate(:user)
-				post :create, email: jordan.email, password: jordan.password + "1"
 				expect(response).to render_template(:new)
 			end
 		end
 	end
 
 	describe "GET destroy" do
+		before do
+			session[:user_id] = Fabricate(:user).id
+		end
 		it "clears the session for the user" do
-			jordan = Fabricate(:user)
-			session[:user_id] = jordan.id
 			get :destroy
 			expect(session[:user_id]).to be_blank
+		end
+		
+		it "redirects to root path" do
+			get :destroy
+			expect(response).to redirect_to root_path
+		end
+
+		it "sets the notice" do
+			get :destroy
+			expect(:notice).to_not be_blank
 		end
 	end
 end
