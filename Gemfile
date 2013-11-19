a21db8bcfb9fcb6beb5b8f9a3682c9df56b26b48
Kevin Wang <knwang@gmail.com>
2013-11-18 23:50:15 -0500

    Upgrade to Rails 4

--- Gemfile
+++ Gemfile
@@ -1,23 +1,21 @@
 source 'https://rubygems.org'
 
-gem 'rails', '3.2.11'
-gem 'haml-rails'
 gem 'bootstrap-sass'
-
-group :assets do
-  gem 'sass-rails',   '~> 3.2.3'
-  gem 'coffee-rails', '~> 3.2.1'
-  gem 'uglifier', '>= 1.0.3'
-end
+gem 'coffee-rails'
+gem 'rails'
+gem 'haml-rails'
+gem 'sass-rails'
+gem 'uglifier'
+gem 'jquery-rails'
 
 group :development do
   gem 'sqlite3'
   gem 'pry'
   gem 'pry-nav'
+  gem 'thin'
 end
 
 group :production do
   gem 'pg'
 end
 
-gem 'jquery-rails'
--- config/application.rb
+++ config/application.rb
@@ -2,12 +2,7 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-if defined?(Bundler)
-  # If you precompile assets before deploying to production, use this line
-  Bundler.require(*Rails.groups(:assets => %w(development test)))
-  # If you want your assets lazily compiled in production, use this line
-  # Bundler.require(:default, :assets, Rails.env)
-end
+Bundler.require(:default, Rails.env)
 
 module Myflix
   class Application < Rails::Application
@@ -15,9 +10,7 @@ module Myflix
     config.filter_parameters += [:password]
     config.active_support.escape_html_entities_in_json = true
 
-    config.active_record.whitelist_attributes = false
     config.assets.enabled = true
-    config.assets.version = '1.0'
     config.generators do |g|
       g.orm :active_record
       g.template_engine :haml
