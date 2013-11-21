3341348ed3d42e180f304045e01873febf7c8ae2
Denny Santoso <dennysantoso.com@gmail.com>
2013-11-21 16:15:30 +0700

    add macros and shared example

---
+++ spec/support/shared_examples.rb
@@ -0,0 +1,8 @@
+shared_examples "requires sign in" do
+  it "redirects to the sign in page" do
+    session[:user_id] = nil
+    action
+    expect(response).to redirect_to sign_in_path
+  end
+
+end
