b5ef85fe74c5f7c14822e21cc903fcebdfda7871
Denny Santoso <dennysantoso.com@gmail.com>
2013-11-30 14:20:56 +0700

    heroku gmail setting added'

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,14 @@ Myflix::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                 587,
+  domain:               'powerful-brook-2693.herokuapp.com',
+  user_name:            ENV['gmail_username'],
+  password:             ENV['gmail_password'],
+  authentication:       'plain',
+  enable_starttls_auto: true  }
 end
