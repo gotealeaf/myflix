6520dcbc1ad1ffb40fcbbab4e8c78fcdc2292c43
Denny Santoso <dennysantoso.com@gmail.com>
2013-12-07 22:05:36 +0700

    fixing mailgun

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,7 +2,7 @@ class ReviewsController < ApplicationController
   before_action :require_user
   def create
     @video = Video.find(params[:video_id])
-    review = @video.reviews.new(review_params.merge(user_id: current_user.id ))
+    review = @video.reviews.new(review_params)
     if review.save
       redirect_to video_path(@video)
     else 
@@ -13,6 +13,6 @@ class ReviewsController < ApplicationController
 
   private
     def review_params
-      params.require(:review).permit(:content, :rating)
+      params.require(:review).permit(:video_id, :user_id, :content, :rating)
     end
 end
