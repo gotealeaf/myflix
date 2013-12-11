80f7dfee66da7a93aff4870891bc95295f2ac2df
Mark <hustada80@gmail.com>
2013-12-11 14:03:27 -0600

    finish mod 6 with feature tests

--- spec/features/user_interacts_with_queue_spec.rb
+++ spec/features/user_interacts_with_queue_spec.rb
@@ -10,14 +10,56 @@ feature "User interacts with queue" do
     futurama = Fabricate(:video, title: "Futurama", category: comedies)
 
     sign_in
-    find("a[href='/videos/#{monk.id}']").click
-    page.should have_content(monk.title)
 
-    click_link "+ My Queue"
-    page.should have_content(monk.title)
+    add_video_to_queue(monk)
+    expect_video_to_be_in_queue(monk)
+    
 
     visit video_path(monk)
-    page.should_not have_content "+ My queue"
+    expect_link_not_to_be_seen("+ My Queue")
+    
 
-  end  
-end
+
+    add_video_to_queue(futurama)
+    add_video_to_queue(south_park)
+      
+ 
+    set_video_position(monk, 3)
+    set_video_position(south_park, 1)
+    set_video_position(futurama, 2)
+    update_queue
+
+    expect_video_position(south_park, 1)
+    expect_video_position(futurama, 2)
+    expect_video_position(monk, 3)
+  end
+
+  def expect_video_to_be_in_queue(video)
+    page.should have_content(video.title)
+  end
+
+  def expect_link_not_to_be_seen(link_text)
+    page.should_not have_content(link_text)
+  end
+
+  def update_queue
+    click_button "Update Instant Queue"
+  end
+
+    
+    def add_video_to_queue(video)
+      visit home_path
+      find("a[href='/videos/#{video.id}']").click
+      click_link "+ My Queue"
+    end
+    
+    def set_video_position(video, position)
+      within(:xpath, "//tr[contains(.,'#{video.title}')]") do
+      fill_in "queue_items[][position]", with: position
+    end
+  end
+
+    def expect_video_position(video, position)
+      expect(find(:xpath, "//tr[contains(.,'#{video.title}')]//input[@type='text']").value).to eq(position.to_s)
+    end
+  end
