76e0cc91f5699d57e3e7bf1ca49ad372b5d2d23b
Jack Collins <BlueSkyFlyer2@gmail.com>
2013-12-28 17:43:55 -0600

    Basic video and category functionality up and running

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Myflix::Application.routes.draw do
+	root 'videos#index'
+
   get 'ui(/:action)', controller: 'ui'
+
+  resources :videos
+  resources :categories
 end
+
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Video.find_or_create_by(title: "Futurama") do |video|
+  video.description = "A show about a pizza guy who was cryogenically frozen"
+  video.small_cover_url = "/tmp/futurama.jpg"
+end
+
+Video.find_or_create_by(title: "South Park") do |video|
+  video.description = "Animated friends raise havok in Colorado"
+  video.small_cover_url = "/tmp/south_park.jpg"
+end
+
+Video.find_or_create_by(title: "Family Guy") do |video|
+  video.description = "Peter and his family go on all sorts of adventures"
+  video.small_cover_url = "/tmp/family_guy.jpg"
+end
+
+Video.find_or_create_by(title: "Monk") do |video|
+  video.description = "Something something detective something."
+  video.small_cover_url = "/tmp/monk.jpg"
+  video.large_cover_url = "monk_large.jpg"
+end
