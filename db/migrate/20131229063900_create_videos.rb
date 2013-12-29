e1f10ac09deeaab4ef6b1bc61810438c8c04cf60
ianknauer <ian.knauer@gmail.com>
2013-12-28 23:39:33 -0800

    created video model and seeded database

---
+++ db/migrate/20131229063900_create_videos.rb
@@ -0,0 +1,9 @@
+class CreateVideos < ActiveRecord::Migration
+  def change
+    create_table :videos do |t|
+      t.string :name
+      t.text  :description
+      t.timestamps
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Video.create(name: "Community", description: "A funny show about people going to a community college.", small_thumb: "community_sm", large_thumb: "community_lg")
+Video.create(name: "Sherlock", description: "A detective uses his smarts to outsmart smart criminals. Plus he's British!", small_thumb: "sherlock_sm", large_thumb: "sherlock_lg")
