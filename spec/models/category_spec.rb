78a1d58b18c568dfb9a0fc64354b5e3353f03f7d
Jack Collins <BlueSkyFlyer2@gmail.com>
2013-12-31 16:08:04 -0600

    Consolidated tests with shoulda-matchers

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,17 +1,5 @@
 require 'spec_helper'
 
 describe Category do
-	it "saves itself" do
-    category = Category.new(name: "Comedies")
-    category.save
-    expect(Category.first).to eq(category)
-	end
-
-	it "has many videos" do
-    comedies = Category.create(name: "Comedies")
-    south_park = Video.create(title: "South Park", description: "Funny show", category: comedies)
-    futurama = Video.create(title: "Futurama", description: "Space Travel", category: comedies)
-
-    expect(comedies.videos).to eq([futurama, south_park])
-	end
+  it { should have_many(:videos)}
 end
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -1,27 +1,7 @@
 require 'spec_helper'
 
 describe Video do
-  it "saves itself" do
-    video = Video.new(title: "Shark Tank", description: "A great entrepreneurial show")
-    video.save
-    expect(Video.first).to eq(video)
-  end
-
-  it "belongs to category" do
-    dramas = Category.create(name: "Dramas")
-    monk = Video.create(title: "Monk", description: "A detective show", category: dramas)
-    expect(monk.category).to eq(dramas)
-  end
-
-  it "requires a title" do
-    video = Video.new(description: "A description without a title")
-    video.save
-    expect(Video.first).to be_nil
-  end
-
-  it "requires a description" do
-    video = Video.new(title: "A video without a description")
-    video.save
-    expect(Video.first).to be_nil
-  end
+  it { should belong_to(:category)}
+  it { should validate_presence_of(:title)}
+  it { should validate_presence_of(:description)}
 end
