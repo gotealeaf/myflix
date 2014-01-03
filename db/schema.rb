849a14e1c834b17f05192202bcc9fab1a866aa10
Michael van Holst <mikevanholst@gmail.com>
2014-01-02 21:06:20 -0500

    changes description from string to text

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140103005921) do
+ActiveRecord::Schema.define(version: 20140103020215) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 20140103005921) do
 
   create_table "videos", force: true do |t|
     t.string   "title"
-    t.string   "description"
+    t.text     "description"
     t.string   "small_cover_url"
     t.string   "large_cover_url"
     t.datetime "created_at"
