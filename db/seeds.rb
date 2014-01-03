eef95b5bc75419a999723c71ec688d1803f62919
David <htw0819@gmail.com>
2014-01-03 23:52:58 +0800

    Create video model and seed the db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Video.create(title: "monk",
+  description: "an American comedy-drama detective mystery television series created by Andy Breckman and starring Tony Shalhoub as the eponymous character, Adrian Monk.",
+  small_cover_url: "/tmp/monk.jpg",
+  large_cover_url: "/tmp/monk_large.jpg")
