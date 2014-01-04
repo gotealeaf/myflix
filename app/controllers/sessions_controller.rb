29970bceea3ddc223a5e54b0235c0f705bb72df9
Jack Collins <BlueSkyFlyer2@gmail.com>
2014-01-04 10:29:09 -0600

    Sign in form tweaks

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
   		session[:user_id] = user.id
   		redirect_to home_path, notice: "You are signed in. Enjoy!"
   	else
-  		flash[:error] = "Invalid email or password"
+  		flash[:danger] = "Invalid email or password"
   		redirect_to sign_in_path
   	end
   end
