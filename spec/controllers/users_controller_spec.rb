65958e712511bfda1b7869a6435b8fd1eb565f1a
Jack Collins <BlueSkyFlyer2@gmail.com>
2014-01-05 10:57:41 -0600

    Finished users_controller tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -7,14 +7,37 @@ describe UsersController do
 			expect(assigns(:user)).to be_instance_of(User)
 		end
 	end
+
 	describe "POST create" do
+		
 		context "with valid input" do
+			before { post :create, user: Fabricate.attributes_for(:user) }
+
 			it "creates the user" do
+				expect(User.count).to eq(1)
 			end
-			it "redirects to the sign in page"
+
+			it "redirects to the sign in page" do
+			  expect(response).to redirect_to sign_in_path
+			end
+
 	  end
+
 	  context "with invalid input" do
-		  it "does not create the user"
+	  	before { post :create, user: { password: "password", full_name: "Kevin Smith" } }
+
+		  it "does not create the user" do
+		  	expect(User.count).to eq(0)
+		  end
+
+		  it "renders the :new template" do
+		  	expect(response).to render_template :new
+		  end
+
+		  it "sets @user" do 
+		  	expect(assigns(:user)).to be_instance_of(User)
+		  end
+
 	  end
 	end
 end
