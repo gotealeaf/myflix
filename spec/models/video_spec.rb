d77280d9b72cd370ab4491af74dca9391aee2c6b
David <htw0819@gmail.com>
2014-01-07 00:38:15 +0800

    Test the category model and associations

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -6,4 +6,11 @@ describe Video do
     video.save
     expect(Video.first.title).to eq("Matrix")
   end
+
+  it "belongs to category" do
+    cat = Category.create(name: "TV drama")
+    video = Video.create(title: "Monk", description: "A drama", category: cat)
+
+    expect(video.category).to eq(cat)
+  end
 end
