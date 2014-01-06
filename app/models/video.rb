f632d30b591a44bae7e635dde6c98ae7cc09ea38
ianknauer <ian.knauer@gmail.com>
2014-01-06 11:02:01 -0800

    implemented search by title on video model

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,6 +1,10 @@
 class Video < ActiveRecord::Base
   belongs_to :category
-  
   validates :name, presence: true
   validates :description, presence: true
-end
+  
+  def self.search_by_title(search_term)
+    return [] if search_term.blank?
+    where("name LIKE ?", "%#{search_term}%").order("created_at DESC")
+  end
+end 
