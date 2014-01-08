a325867d768605f43ff41c84064df67d4653e86d
Neil <neil@iobits-iMac.local>
2014-01-08 11:00:35 +0800

    add category_id in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,8 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-Video.create(title: "Toy Story", description: "Toy Story", small_cover_url: "/tmp/futurama.jpg", large_cover_url: "/tmp/monk_large.jpg")
+Video.create(title: "Toy Story", description: "Toy Story", small_cover_url: "/tmp/futurama.jpg", large_cover_url: "/tmp/monk_large.jpg", category_id: 1)
 
-Video.create(title: "south_park", description: "south_park", small_cover_url: "/tmp/south_park.jpg", large_cover_url: "/tmp/monk_large.jpg")
+Video.create(title: "south_park", description: "south_park", small_cover_url: "/tmp/south_park.jpg", large_cover_url: "/tmp/monk_large.jpg", category_id: 1)
 
-Video.create(title: "family_guy", description: "family_guy", small_cover_url: "/tmp/family_guy.jpg", large_cover_url: "/tmp/monk_large.jpg")
+Video.create(title: "family_guy", description: "family_guy", small_cover_url: "/tmp/family_guy.jpg", large_cover_url: "/tmp/monk_large.jpg", category_id: 1)
