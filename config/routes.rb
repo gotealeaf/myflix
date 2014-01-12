17c9da5a3ef9349e51213a2480db2509d3e63b46
Jack Collins <BlueSkyFlyer2@gmail.com>
2014-01-12 16:47:45 -0600

    Users can remove videos from their queue

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Myflix::Application.routes.draw do
     resources :reviews, only: [:create]
   end
   resources :categories
-  resources :queue_items, only: [:create]
+  resources :queue_items, only: [:create, :destroy]
 
   resources :users, only: [:create]
 
