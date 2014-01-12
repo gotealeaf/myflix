69d5d112def31edd429c3b8944da82a651d3a27f
ianknauer <ian.knauer@gmail.com>
2014-01-11 20:26:45 -0800

    added authentication and fixed up ui elements

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,4 +1,5 @@
 class VideosController < ApplicationController
+  before_filter :require_user
   before_action :set_video, only: [:show]
   
   def index
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Myflix::Application.routes.draw do
   get '/home', to: 'videos#index'
   get 'register', to: "users#new"
   get '/sign_in', to: 'sessions#new'
+  get '/sign_out', to: 'sessions#destroy'
   
   resources :videos, only: [:show] do
     collection do
@@ -13,4 +14,5 @@ Myflix::Application.routes.draw do
   end
   resources :categories
   resources :users, only: [:create]
+  resources :sessions, only: [:create]
 end
