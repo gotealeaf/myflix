3e06ba873f4671f024c6e9913334c7503773a90d
Joe Levinger <jlevinger@jtonedesigns.com>
2014-01-14 12:01:02 -0500

    Mod 3 Changes
    Refactored code from Mod 2. The sign in form is a non-model-backed
    form, and I had been using bootstrap_form_for to render the form.
    Refactored the code to use form_tag, which simplified the code, and
    allowed user input to be saved and sent back to the form in the event
    of invalid user input.

--- spec/controllers/videos_controller_spec.rb
+++ spec/controllers/videos_controller_spec.rb
@@ -5,19 +5,57 @@ describe VideosController do
     my_user = User.create(email: 'jlevinger@jtonedesigns.com', password: 'joelevinger', full_name: 'Joe Levinger')
     session[:user_id] = my_user.id
   end
+  describe 'GET index' do
+    it "builds a Categories collection" do
+      romantic_dramas = Category.create(name: "Romantic Dramas")
+      biographies = Category.create(name: "Biographies")
+      get :index
+      expect(assigns(:categories).first).to eq biographies
+    end
+    it "renders the index template" do
+      get :index
+      expect(response).to render_template :index
+    end
+    it "redirects to root if not logged in" do
+      session[:user_id] = nil
+      get :index
+      expect(response).to redirect_to root_path
+    end
+  end
+  describe 'GET show' do
+    it "finds a video" do
+      video = Video.create(title: "Che", description: "great video")
+      get :show, id: video.id
+      expect(assigns(:video)).to eq(video)
+    end
+    it "renders the show template" do
+      video = Video.create(title: "Che", description: "great video")
+      get :show, id: video.id
+      expect(response).to render_template :show
+    end
+    it "redirects to root if not logged in" do
+      session[:user_id] = nil
+      video = Video.create(title: "Che", description: "great video")
+      get :show, id: video.id
+      expect(response).to redirect_to root_path
+    end
+  end
   describe 'POST search' do
+    it "sets the search term variable" do
+      video = Video.create(title: "Che", description: "great video")
+      post :search, search_term: 'Che'
+      expect(assigns(:search_term)).to eq "Che"
+    end
     it "returns videos" do
       video = Video.create(title: "Che", description: "great video")
       post :search, search_term: 'Che'
       expect(assigns(:videos)).to eq([video])
     end
-
     it "renders search" do
       video = Video.create(title: "Che", description: "great video")
       post :search, search_term: 'Che'
       expect(response).to render_template :search
     end
-
     it "redirects to root if not logged in" do
       session[:user_id] = nil
       video = Video.create(title: "Che", description: "great video")
