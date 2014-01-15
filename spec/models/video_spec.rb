e1ee8d7a9bb959be5c939deed2af0b6e4d82f7e5
Rick Cid <rick@hispand.com>
2014-01-14 23:18:52 -0700

    Added Test for Video & Category models and included shoulda_matchers

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -1,9 +1,7 @@
 require 'spec_helper' 
 
 describe Video do
-  it "saves itself" do
-    video = Video.new(title: "monk", description: "a great video!")
-    video.save
-    expect(Video.first).to eq(video)
-  end
+  it { should belong_to(:category)}
+  it { should validate_presence_of(:title)}
+  it { should validate_presence_of(:description)}
 end
