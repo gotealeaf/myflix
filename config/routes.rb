5741337ac99358182fe2dd170a43d66dc34dfe57
Charlie <ankothari@gmail.com>
2014-01-16 19:10:05 +0530

    My final soln to mod0

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Myflix::Application.routes.draw do
+  get "videos/show"
+  get "homes/home"
   get 'ui(/:action)', controller: 'ui'
+  get 'home' => "homes#home"
+  resources :videos,only: [:show]
+  get '/video', to: "videos#show"
 end
