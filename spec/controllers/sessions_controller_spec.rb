0f283734d302a9dfc96458f7f399c1f05f5b468e
j1wu <ji.wu@outlook.com>
2014-01-16 15:01:09 +0800

    added controller tests for authentication

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -16,23 +16,52 @@ describe SessionsController do
 
   describe 'POST create' do
     context 'with valid credentials' do
+      let(:bob) { bob = Fabricate(:user) }
       before do
-        bob = Fabricate(:user)
-        post :create, email: bob.email, password: bob.password
+        post :create, user: { email: bob.email, password: bob.password }
       end
       it 'creates a new session for the signed in user' do
         expect(session[:user_id]).to eq(bob.id) 
       end
-      it 'redirects to the home page'
+      it 'redirects to the home page' do
+        expect(response).to redirect_to home_path
+      end
+      it 'sets the notice' do
+        expect(flash[:notice]).not_to be_blank
+      end
     end
 
     context 'with invalid credentials' do
-
+      let(:bob) { bob = Fabricate(:user) }
+      before do
+        post :create, user: { email: bob.email, password: '?' }
+      end
+      it 'does not create a new session' do
+        expect(session[:user_id]).to be_nil 
+      end
+      it 'redirects user to the sign in page' do
+        expect(response).to redirect_to sign_in_path
+      end
+      it 'sets the error message' do
+        expect(flash[:danger]).not_to be_blank
+      end
     end
   end
 
   describe 'GET destroy' do
-
+    before do
+      session[:user_id] = Fabricate(:user).id
+      get :destroy
+    end
+    it 'sets session to nil' do
+      expect(session[:user_id]).to be_nil  
+    end
+    it 'redirects user to root path' do
+      expect(response).to redirect_to root_path
+    end
+    it 'sets the notice message' do
+      expect(flash[:notice]).not_to be_blank
+    end
   end
 
 end
