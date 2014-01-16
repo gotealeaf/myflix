ba1f2c8e3ca1da22550dbf40f3533c49a1545a35
Michael Huang <hyc1124@gmail.com>
2014-01-16 16:54:38 +0800

    test video validations

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -12,4 +12,14 @@ describe Video do
 		inception = Video.create(title: "Inception", description: "brilliant movie", category: dramas)
 		expect(inception.category).to eq(dramas)
 	end
+
+	it "doen't save a video without a title" do
+		video = Video.create(description: "nice video")
+		expect(Video.count).to eq(0)
+	end
+
+	it "doen't save a video without a description" do
+		video = Video.create(title: "Inception")
+		expect(Video.count).to eq(0)
+	end
 end
