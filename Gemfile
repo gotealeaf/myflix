1290ace8890bae26497aa9d8d381985968b17efd
Michael Huang <hyc1124@gmail.com>
2014-01-16 17:11:10 +0800

    use shoulda-matchers

--- Gemfile
+++ Gemfile
@@ -24,3 +24,7 @@ end
 group :test, :development do
 	gem 'rspec-rails'
 end
+
+group :test do
+	gem 'shoulda-matchers'
+end
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -1,25 +1,7 @@
 require 'spec_helper'
 
 describe Video do
-	it "saves itself" do
-		video = Video.new(title: "terminator", description: "sci-fi movie")
-		video.save
-		expect(Video.first).to eq(video)
-	end
-
-	it "belongs to category" do
-		dramas = Category.create(name: "Dramas")
-		inception = Video.create(title: "Inception", description: "brilliant movie", category: dramas)
-		expect(inception.category).to eq(dramas)
-	end
-
-	it "doen't save a video without a title" do
-		video = Video.create(description: "nice video")
-		expect(Video.count).to eq(0)
-	end
-
-	it "doen't save a video without a description" do
-		video = Video.create(title: "Inception")
-		expect(Video.count).to eq(0)
-	end
+	it {should belong_to(:category)}
+	it {should validate_presence_of(:title)}
+	it {should validate_presence_of(:description)}
 end
