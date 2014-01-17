466f579c266caecc89631716e338fdbc4f0c024e
Michael Huang <hyc1124@gmail.com>
2014-01-17 17:16:51 +0800

    implement recent videos on the category model

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,12 @@ Myflix::Application.routes.draw do
   get 'ui(/:action)', controller: 'ui'
 
 	get '/home', to: 'videos#home'
+  
   resources :videos do
   	collection do
   		post :search
   	end
   end
+  
   resources :categories
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,39 @@
 require 'spec_helper'
 
 describe Category do
-	it {should have_many(:videos)}
+	it { should have_many(:videos) }
+
+	describe "#recent_videos" do
+		it "returns the videos in the reverse chonical order by created at" do
+			comedies = Category.create(name: "Comedies")
+			yes_man = Video.create(title: "Yes Man", description: "amused movie", category: comedies)
+			the_mask = Video.create(title: "The Mask", description: "funny movie", category: comedies, created_at: 1.day.ago)
+			expect(comedies.recent_videos).to eq([yes_man, the_mask])
+		end
+
+		it "returns all the videos if there are less than 6 videos" do
+			comedies = Category.create(name: "Comedies")
+			yes_man = Video.create(title: "Yes Man", description: "amused movie", category: comedies)
+			the_mask = Video.create(title: "The Mask", description: "funny movie", category: comedies, created_at: 1.day.ago)
+			expect(comedies.recent_videos.count).to eq(2)
+		end
+
+		it "returns 6 videos if there are more than 6 videos" do
+			comedies = Category.create(name: "Comedies")
+			7.times{ Video.create(title: "Yes Man", description: "amused movie", category: comedies)}
+			expect(comedies.recent_videos.count).to eq(6)
+		end
+
+		it "returns the most recent 6 videos" do
+			comedies = Category.create(name: "Comedies")
+			6.times{ Video.create(title: "Yes Man", description: "amused movie", category: comedies)}
+			the_mask = Video.create(title: "The Mask", description: "funny movie", category: comedies, created_at: 1.day.ago)
+			expect(comedies.recent_videos).not_to include(the_mask)
+		end
+
+		it "returns an empty array if the category doesn't have any videos" do
+			comedies = Category.create(name: "Comedies")
+			expect(comedies.recent_videos).to eq([])
+		end
+	end
 end
