f832aa6ddb578b517e3efd1b2b8cf1117652c7a1
ianknauer <ian.knauer@gmail.com>
2014-01-17 09:25:33 -0800

    fixed en/end typo and changed filter to action

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,5 @@
 class ReviewsController < ApplicationController
-  before_filter :require_user
+  before_action :require_user
   def create
     @video = Video.find(params[:video_id])
     review = @video.reviews.build(review_params.merge!(user: current_user))
