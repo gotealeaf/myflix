c9eea48a3a2eb8e5687d827b6349dace1924a691
vivijie <j.uloveit@gmail.com>
2014-01-19 21:16:52 +0800

    keep 1 decimal in rating

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,7 +3,7 @@ class ReviewsController < ApplicationController
   
   def create
     @video = Video.find(params[:video_id])
-    @review = @video.reviews.build(review_params)
+    @review = @video.reviews.new(review_params)
     @review.user = current_user 
     if @review.save
       redirect_to @video
