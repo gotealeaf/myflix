190af2d713acb371e66a37a374acddc8a9f063ee
ianknauer <ian.knauer@gmail.com>
2014-01-20 14:23:16 -0800

    added ability for user to change video ratings in the queue

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,10 @@ class User < ActiveRecord::Base
   validates :email, presence: true
   validates :full_name, presence: true
   validates :password, presence: true, on: :create
+  
+  def normalize_queue_item_positions
+    queue_items.each_with_index do |queue_item, index|
+      queue_item.update_attributes(position: index+1)
+    end
+  end
 end
--- spec/models/queue_item_spec.rb
+++ spec/models/queue_item_spec.rb
@@ -29,6 +29,33 @@ describe QueueItem do
     end
   end
   
+  describe "#rating=" do
+    it "changes the rating of the review if review is present" do
+      video = Fabricate(:video)
+      user = Fabricate(:user)
+      review = Fabricate(:review, user: user, video: video, rating: 2)
+      queue_item = Fabricate(:queue_item, user: user, video: video)
+      queue_item.rating = 4
+      expect(review.reload.rating).to eq(4)
+    end
+    
+    it "clears the rating of the review if the review is present" do
+      video = Fabricate(:video)
+      user = Fabricate(:user)
+      review = Fabricate(:review, user: user, video: video, rating: 2)
+      queue_item = Fabricate(:queue_item, user: user, video: video)
+      queue_item.rating = nil
+      expect(review.reload.rating).to be_nil
+    end
+    it "creates a review with the rating if the review is not present" do
+      video = Fabricate(:video)
+      user = Fabricate(:user)
+      queue_item = Fabricate(:queue_item, user: user, video: video)
+      queue_item.rating = 2
+      expect(Review.first.rating).to eq(2)
+    end
+  end
+  
   describe "#category_name" do
     it "returns the category name for the video" do
       category = Fabricate(:category, name: "comedies")
