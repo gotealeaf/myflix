af2378cdba2bbd99fe7dfa552a680f2a77759c6f
Jack Collins <BlueSkyFlyer2@gmail.com>
2014-01-20 20:05:35 -0600

    Basic welcome email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,6 +8,9 @@ Myflix::Application.configure do
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
 
+  # Open email in browser
+  config.action_mailer.delivery_method = :letter_opener
+
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
 
