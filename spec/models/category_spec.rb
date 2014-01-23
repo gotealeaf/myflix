3a9e200fc85ed5c021cc3f5e4a3027fba615d387
Brandon Conway <brandoncc@gmail.com>
2014-01-22 17:48:42 -0800

    test that categories can be saved

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,6 +1,12 @@
 require 'spec_helper'
 
 describe Category do
+  it 'saves itself' do
+    category = Category.new(name: 'drama')
+    category.save
+    expect(Category.first).to eq(category)
+  end
+
   it 'can have many videos' do
     drama = Category.create(name: 'drama')
     Video.create(title: 'First video', category: drama)
