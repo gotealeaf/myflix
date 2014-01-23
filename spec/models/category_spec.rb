854225305c55138fb88f1358216f51fc1462909c
Brandon Conway <brandoncc@gmail.com>
2014-01-22 21:30:05 -0800

    update test to pass new requirements

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -9,8 +9,8 @@ describe Category do
 
   it 'can have many videos' do
     drama = Category.create(name: 'drama')
-    Video.create(title: 'First video', category: drama)
-    Video.create(title: 'Second video', category: drama)
+    Video.create(title: 'First video', description: 'Hilarious!', category: drama)
+    Video.create(title: 'Second video', description: 'So sad :-(', category: drama)
     expect(drama.videos.count).to eq(2)
   end
 end
