8702e4128e2c1f5262dc7945168726f752bf5eff
Brandon Conway <brandoncc@gmail.com>
2014-01-22 21:56:29 -0800

    get rid of will save tests

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,12 +1,6 @@
 require 'spec_helper'
 
 describe Category do
-  it 'saves itself' do
-    category = Category.new(name: 'drama')
-    category.save
-    expect(Category.first).to eq(category)
-  end
-
   it { should have_many(:videos) }
   it { should validate_presence_of(:name) }
 end
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -1,12 +1,6 @@
 require 'spec_helper'
 
 describe Video do
-  it 'saves a video' do
-    video = Video.new(title: 'A video', description: 'A really long description')
-    video.save
-    Video.first.title.should == 'A video'
-  end
-
   it { should belong_to(:category) }
   it { should validate_presence_of(:title) }
   it { should validate_presence_of(:description) }
