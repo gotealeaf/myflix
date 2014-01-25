5573ea0f55084d8733ad52ff528a0a22d03a5d9b
Brandon Conway <brandoncc@gmail.com>
2014-01-24 22:38:58 -0800

    remove environment test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
   end
 
   def require_user
-    if !Rails.env.test? && !logged_in?
+    if !logged_in?
       flash[:info] = 'Access reserved for members only. Please sign in first.'
       redirect_to root_path
     end
