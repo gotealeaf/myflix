24e5ad5f86150fd82c73885a0e431089235a6b40
Brandon Conway <brandoncc@gmail.com>
2014-01-26 22:51:29 -0800

    implement Video#average_rating

--- app/models/video.rb
+++ app/models/video.rb
@@ -10,4 +10,18 @@ class Video < ActiveRecord::Base
     self.where('lower(title) LIKE :search_string', search_string: '%' + title.to_s.downcase + '%').
         order(created_at: :desc)
   end
+
+  def average_rating
+     (total_reviews_rating / reviews_count).round(3)
+  end
+
+  private
+
+  def total_reviews_rating
+     self.reviews.inject(0) { |sum, review| sum + review.rating }.to_f
+  end
+
+  def reviews_count
+    self.reviews.count
+  end
 end
