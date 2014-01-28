e021f6092eae74e62328ef0f07aef78de7719939
Matt Pedersen <mpedersen2054@gmail.com>
2014-01-28 13:31:14 -0600

    create category_spec -- wrote specs for the video and category association

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -1,10 +1,17 @@
 require 'spec_helper'
 
 describe Video do
-  it "describes itself" do
+  it "saves itself" do
     video = Video.new(title: "Monk", description: "great video")
     video.save
 
     expect(Video.first).to eq(video)
+  end
+
+  it "belongs to category" do
+    dramas = Category.create(name: "dramas")
+    monk = Video.create(title: "Monk", description: "a cool video", category: dramas)
+
+    expect(monk.category).to eq(dramas)
   end
 end
