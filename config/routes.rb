fe5f8a76c89f47688adb7837f550b003499aa8a4
vivijie <j.uloveit@gmail.com>
2014-01-29 22:05:52 +0800

    Assignment: Implement the user profile page

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Myflix::Application.routes.draw do
   get 'sign_in', to: "sessions#new"
   get 'sign_out', to: "sessions#destroy"
   get 'my_queue', to: "queue_items#index"
+  get 'user', to: "users#show"
   
   post 'update_queue', to: "queue_items#update_queue"
 
