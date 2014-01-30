b34e665946823903ad71c6a378bca0aaeb21e6d3
Jack Collins <BlueSkyFlyer2@gmail.com>
2014-01-30 10:13:02 -0600

    Production email tweaks

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,7 +21,7 @@ Myflix::Application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: "example.com",
+    domain: "collins-myflix.herokuapp.com",
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: ENV["GMAIL_USERNAME"],
