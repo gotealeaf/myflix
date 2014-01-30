fd97fbd5f0615cf4427cfdc33b4a9ed15f03cb33
Jack Collins <BlueSkyFlyer2@gmail.com>
2014-01-30 10:22:37 -0600

    Production email fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,6 +18,10 @@ Myflix::Application.configure do
 
   config.active_support.deprecation = :notify
 
+  config.action_mailer.default_url_options = { host: "collins-myflix.herokuapp.com" }
+
+  config.action_mailer.delivery_method = :smtp
+
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
