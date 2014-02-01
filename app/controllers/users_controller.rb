04bd77ee2029cb79707fbdbd0d79e00e0854f868
Brandon Conway <brandoncc@gmail.com>
2014-01-31 23:35:25 -0800

    implement rating videos from the queue

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,9 @@ class UsersController < ApplicationController
   end
 
   def update_queue
-    if !reorder_queue_items
+    if reorder_queue_items && update_queue_item_ratings
+      flash[:success] = 'Your queue has been updated successfully.'
+    else
       flash[:danger] =
       'There was a problem updating your queue. Please try again.'
     end
@@ -32,15 +34,13 @@ class UsersController < ApplicationController
     transaction_was_successful = true
 
     current_user.transaction do
-      params[:user][:queue_item].each do |id, parameters|
+      queue_items_in_params_with_parameter(:position).each do |id, parameters|
         queue_item = QueueItem.find(id)
         if parameters[:position] =~ /\A\d+\z/ && queue_item.user == current_user
           queue_item.position = parameters[:position]
           queue_item.save
         else
           transaction_was_successful = false
-
-          flash[:danger] = 'There was a problem updating your queue. Please try again.'
           raise ActiveRecord::Rollback
         end
       end
@@ -48,6 +48,44 @@ class UsersController < ApplicationController
     end
 
     transaction_was_successful
+  end
+
+  def update_queue_item_ratings
+    transaction_was_successful = true
+
+    queue_items_in_params_with_valid_rating = queue_items_in_params_with_parameter(:rating)
+
+    queue_items_in_params_with_valid_rating.delete_if{|_,v| v[:rating].blank?}
+
+    params[:user][:queue_item].select{|_,v|v.include?(:rating) && (1..5).include?(v['rating'].to_i)}
+
+    current_user.transaction do
+      queue_items_in_params_with_valid_rating.each do |id, parameters|
+        if Review.valid_ratings.include?(parameters[:rating].to_i)
+          queue_item = QueueItem.find(id)
+          review = Review.find_by_creator_and_video(current_user, queue_item.video)
+          if review
+            review.rating = parameters[:rating]
+          else
+            review = Review.new(creator: current_user, video: queue_item.video, rating: parameters[:rating])
+          end
+
+          if !review.save(validate: false)
+            transaction_was_successful = false
+            raise ActiveRecord::Rollback
+          end
+        else
+          transaction_was_successful = false
+          raise ActiveRecord::Rollback
+        end
+      end
+    end
+
+    transaction_was_successful
+  end
+
+  def queue_items_in_params_with_parameter(parameter)
+    params[:user][:queue_item].select{|_,v|v.include?(parameter)}
   end
 
   def user_params
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -54,8 +54,10 @@ describe UsersController do
 
     context 'authorized user' do
       let!(:user) { Fabricate(:user) }
-      let!(:queue_item1) { Fabricate(:queue_item, position: QueueItem.next_available_position(user), user: user, created_at: 1.day.ago) }
-      let!(:queue_item2) { Fabricate(:queue_item, position: QueueItem.next_available_position(user), user: user) }
+      let!(:video1) { Fabricate(:video) }
+      let!(:video2) { Fabricate(:video) }
+      let!(:queue_item1) { Fabricate(:queue_item, video: video1, position: QueueItem.next_available_position(user), user: user, created_at: 1.day.ago) }
+      let!(:queue_item2) { Fabricate(:queue_item, video: video2, position: QueueItem.next_available_position(user), user: user) }
 
       before do
         session[:user_id] = user.id
@@ -87,7 +89,13 @@ describe UsersController do
           expect(queue_item2.reload.position).to eq(1)
         end
 
-        it 'redirects to my queue page' do
+        it 'sets success message' do
+          patch :update_queue, id: user.id, user: { queue_item: { queue_item2.id => { position: 2 }, queue_item1.id => { position: 4 } } }
+
+          expect(flash[:success]).not_to be_blank
+        end
+
+        it 'redirects to previous page' do
           patch :update_queue, id: user.id, user: { queue_item: { queue_item1.id => { position: 2 }, queue_item2.id => { position: 1 } } }
           expect(response).to redirect_to(request.env['HTTP_REFERER'])
         end
@@ -117,6 +125,60 @@ describe UsersController do
           expect(queue_item2.reload.position).to eq(2)
         end
       end
+
+      context 'with valid rating parameters' do
+        let!(:review) { Fabricate(:review, rating: 3, creator: user, video: video1) }
+
+        it 'updates existing review ratings' do
+          review2 = Fabricate(:review, rating: 3, creator: user, video: video2)
+          patch :update_queue, id: user.id, user: { queue_item: { queue_item1.id => { rating: 1 }, queue_item2.id => { rating: 2 } } }
+          expect(review.reload.rating).to eq(1)
+          expect(review2.reload.rating).to eq(2)
+        end
+
+        it 'creates new, blank reviews for any previously unreviewed videos' do
+          patch :update_queue, id: user.id, user: { queue_item: { queue_item2.id => { rating: 2 } } }
+          expect(Review.find_rating_by_creator_and_video(user, video2)).to eq(2)
+        end
+
+        it 'sets success message' do
+          patch :update_queue, id: user.id, user: { queue_item: { queue_item2.id => { rating: 2 } } }
+
+          expect(flash[:success]).not_to be_blank
+        end
+
+        it 'redirects to the previous page' do
+          patch :update_queue, id: user.id, user: { queue_item: { queue_item2.id => { rating: 2 } } }
+          expect(response).to redirect_to(request.env['HTTP_REFERER'])
+        end
+      end
+
+      context 'with invalid rating parameters' do
+        let!(:review) { Fabricate(:review, rating: 3, creator: user, video: video1) }
+
+        it 'does not alter the ratings of the items in the queue' do
+          patch :update_queue, id: user.id, user: { queue_item: { queue_item1.id => { rating: '' } } }
+          expect(review.reload.rating).to eq(3)
+        end
+
+        it 'sets an error message' do
+          patch :update_queue, id: user.id, user: { queue_item: { queue_item1.id => { rating: '' }, queue_item2.id => { rating: 100 } } }
+          expect(flash[:danger]).not_to be_blank
+        end
+
+        it 'redirects to the previous page' do
+          patch :update_queue, id: user.id, user: { queue_item: { queue_item1.id => { rating: '' } } }
+          expect(response).to redirect_to(request.env['HTTP_REFERER'])
+        end
+
+        it 'rolls back the transaction' do
+          review2 =  Fabricate(:review, rating: 3, creator: user, video: video2)
+          patch :update_queue, id: user.id, user: { queue_item: { queue_item1.id => { rating: 2 }, queue_item2.id => { rating: 100 } } }
+          expect(review.reload.rating).to eq(3)
+        end
+      end
+
+      context 'with both rating and position parameters'
     end
 
     context 'unauthorized user' do
