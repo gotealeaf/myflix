95657a7666da320e20f0b6a0d0704facfe9a34fe
Jack Collins <BlueSkyFlyer2@gmail.com>
2014-02-02 11:18:58 -0600

    Add video as admin working

--- app/controllers/admin/videos_controller.rb
+++ app/controllers/admin/videos_controller.rb
@@ -6,6 +6,17 @@ class Admin::VideosController < ApplicationController
 		@video = Video.new
 	end
 
+	def create
+		@video = Video.new(video_params)
+		if @video.save
+		  flash[:success] = "You have successfully added the video '#{@video.title}"
+		  redirect_to new_admin_video_path
+		else
+			flash[:danger] = "You cannot add this video. Please check the errors"
+			render :new
+		end
+	end
+
 	private
 
 	def require_admin
@@ -13,5 +24,9 @@ class Admin::VideosController < ApplicationController
 			flash[:error] = "You are not authorized to do that"
 			redirect_to root_path unless current_user.admin?
 		end
+	end
+
+	def video_params
+		params.require(:video).permit!
 	end
 end
--- spec/support/shared_examples.rb
+++ spec/support/shared_examples.rb
@@ -6,6 +6,14 @@ shared_examples "requires sign in" do
 	end
 end
 
+shared_examples "requires admin" do
+	it "redirects to the root path" do
+		session[:user_id] = Fabricate(:user)
+		action
+		expect(response).to redirect_to root_path
+	end
+end
+
 shared_examples "tokenable" do
 	it "generates a random token when user is created" do
 		expect(object.token).to be_present
