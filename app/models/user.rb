835229091286a0f4c96d494b2ea48a486bbf8f95
Brandon Conway <brandoncc@gmail.com>
2014-02-02 11:19:08 -0800

    improve position breaking algorithm

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,19 +14,7 @@ class User < ActiveRecord::Base
   end
 
   def break_position_ties
-    while self.queue_items.reload.map(&:position).uniq.count != self.queue_items.count
-      items_to_sort = self.queue_items.map { |queue_item| [queue_item.id, queue_item.position] }
-
-      items_to_sort.each_with_index do |item, index|
-        if index < items_to_sort.count - 1
-          if items_to_sort[index][1] >= items_to_sort[index + 1][1]
-            next_queue_item = QueueItem.find(items_to_sort[index + 1][0])
-            next_queue_item.position += 1
-            next_queue_item.save
-          end
-        end
-      end
-    end
+    self.queue_items.each_with_index { |item, index| item.position = index }
   end
 
   def fill_queue_item_position_gaps
