02bf8d631b78368fe8f849792d0cf95d06da687f
Matthew Barram <matthewbarram@gmail.com>
2014-02-04 13:20:35 +1000

    Created association tests for video and categories.
    This was the first time I have written this test and am not sure it is correct.

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -7,3 +7,13 @@ describe Category do
     expect(Category.first).to eq(category)
   end
 end
+
+describe Category do
+  it "has videos asociated" do
+    video = Video.create(title: "Test video", description: 'This is a really long description video.')
+    category = Category.create(title: 'Scary movies')
+    video.category = category
+    video.save
+    expect(category.videos.first).to eq(video)
+  end
+end
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -1,8 +1,20 @@
 require 'spec_helper'
 
-describe Category do
+describe Video do
   it "saves itself" do
     video = Video.new(title: "Test video", description: 'This is a really long description video.')
     video.save
     expect(Video.first).to eq(video)
   end
+end
+
+describe Video do
+  it "has categoies through video" do
+    video = Video.create(title: "Test video", description: 'This is a really long description video.')
+    category = Category.create(title: 'Scary movies')
+    video.category = category
+    video.save
+    video1 = Video.first
+    expect(video.category.title).to eq(category.title)
+  end
+end
