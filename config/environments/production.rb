085c075c1e37fb03e221e01b725f0a9856cf3ab2
vivijie <j.uloveit@gmail.com>
2014-02-05 12:30:55 +0800

    set production mail url options

---
+++ config/environments/production.rb
@@ -0,0 +1,30 @@
+Myflix::Application.configure do
+
+  config.cache_classes = true
+  config.eager_load = true
+  config.consider_all_requests_local       = false
+  config.action_controller.perform_caching = true
+
+  config.serve_static_assets = false
+
+  config.assets.compress = true
+  config.assets.js_compressor = :uglifier
+
+  config.assets.compile = false
+
+  config.assets.digest = true
+
+  config.i18n.fallbacks = true
+
+  config.active_support.deprecation = :notify
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'example.com',
+    user_name:            'brincells@gmail.com',
+    password:             'brincellstime',
+    authentication:       'plain',
+    enable_starttls_auto: true  }  
+  config.action_mailer.default_url_options = {'http://pure-refuge-7304.herokuapp.com/'}
+end
