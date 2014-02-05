955a17dd49a3f418f92ef72bb0a9d97545d6dd83
vivijie <j.uloveit@gmail.com>
2014-02-05 12:41:15 +0800

    remove example domain name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,7 +21,6 @@ Myflix::Application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'example.com',
     user_name:            'brincells@gmail.com',
     password:             'brincellstime',
     authentication:       'plain',
