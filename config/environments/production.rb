7bf7090bb66e2a9c02853e718431679382679fb8
vivijie <j.uloveit@gmail.com>
2014-02-05 13:01:40 +0800

    add mailgun config to production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,13 +17,17 @@ Myflix::Application.configure do
   config.i18n.fallbacks = true
 
   config.active_support.deprecation = :notify
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    user_name:            'brincells@gmail.com',
-    password:             'brincellstime',
-    authentication:       'plain',
-    enable_starttls_auto: true  }  
   config.action_mailer.default_url_options = { host: 'http://pure-refuge-7304.herokuapp.com/'}
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.smtp_settings = {
+     :authentication => :plain,
+     :address => "smtp.mailgun.org",
+     :port => 587,
+     :domain => "sandbox23726.mailgun.org",
+     :user_name => "brincells@gmail.com",
+     :password => "xiay123456"
+}
 end
