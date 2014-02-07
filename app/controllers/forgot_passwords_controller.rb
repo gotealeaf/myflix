4cb152769e8236ab898b50bbb7b36d215f141186
Michael van Holst <mikevanholst@gmail.com>
2014-02-06 20:54:21 -0500

    converts forgot password email to background

--- app/controllers/forgot_passwords_controller.rb
+++ app/controllers/forgot_passwords_controller.rb
@@ -3,7 +3,7 @@ class ForgotPasswordsController < ApplicationController
   def create
     user = User.where(email: params[:email]).first
     if user
-      AppMailer.send_forgot_password(user).deliver
+      AppMailer.delay.send_forgot_password(user.id)
       redirect_to forgot_password_confirmation_path
     else
       flash[:error] = params[:email].blank? ? "You must enter your email address.": "The email address is not in the system."
