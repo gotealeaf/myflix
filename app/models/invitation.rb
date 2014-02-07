b5ff808d04c6597d7ec110a08d48c58bc268201b
vivijie <j.uloveit@gmail.com>
2014-02-07 14:00:16 +0800

    unique recipient_email

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -3,4 +3,5 @@ class Invitation < ActiveRecord::Base
   belongs_to :inviter, foreign_key: 'inviter_id', class_name: 'User'
 
   validates_presence_of :recipient_name, :recipient_email, :message
+  validates_uniqueness_of :recipient_email
 end
