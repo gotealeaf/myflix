cd116fb9afd7461c3dee0fbafbce238cc3bca02c
vivijie <j.uloveit@gmail.com>
2014-02-07 12:00:42 +0800

    production: change host url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,7 +17,7 @@ Myflix::Application.configure do
   config.i18n.fallbacks = true
 
   config.active_support.deprecation = :notify
-  config.action_mailer.default_url_options = { host: 'http://pure-refuge-7304.herokuapp.com/'}
+  config.action_mailer.default_url_options = { host: 'http://myflix-neil.herokuapp.com'}
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
