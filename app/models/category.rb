489d9b8287698b0be6618769aa4d763a30500933
Matthew Barram <matthewbarram@gmail.com>
2014-02-09 10:47:00 +1000

    Added recent videos method

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,7 @@ class Category < ActiveRecord::Base
   has_many :videos, order: :title
   validates_presence_of :title, :description
 
+  def display_most_recent_videos
+    videos.reorder("created_at DESC").take(6)
+  end
 end
