1f681c79b03c8ef043298a1c6f66d8ebfcbd40d8
Matt Pedersen <mpedersen2054@gmail.com>
2014-02-10 16:25:45 -0600

    add functionality to allow users to remove items from their queue, wrote specs also

--- config/routes.rb
+++ config/routes.rb
@@ -9,18 +9,16 @@ Myflix::Application.routes.draw do
   end
 
   get '/genre/:id', to: 'categories#show', as: 'category'
-  resources :queue_items, only: [:create]
 
-  get '/register', to: 'users#new'
-  resources :users, only: [:create]
-
-  get '/sign_in', to: 'sessions#new'
-  get '/sign_out', to: 'sessions#destroy'
-  resources :sessions, only: [:create]
-
+  resources :queue_items, only: [:create, :destroy]
   get '/my_queue', to: 'queue_items#index'
 
-  root to: 'pages#front'
+  resources :sessions, only: [:create]
+  resources :users, only: [:create]
+  get '/register', to: 'users#new'
+  get '/sign_in', to: 'sessions#new'
+  get '/sign_out', to: 'sessions#destroy'
 
+  root to: 'pages#front'
   get 'ui(/:action)', controller: 'ui'
 end
--- spec/controllers/queue_items_controller_spec.rb
+++ spec/controllers/queue_items_controller_spec.rb
@@ -24,18 +24,21 @@ describe QueueItemsController do
       post :create, video_id: video.id
       expect(response).to redirect_to my_queue_path
     end
+
     it "creates a queue item" do
       session[:user_id] = Fabricate(:user).id
       video = Fabricate(:video)
       post :create, video_id: video.id
       expect(QueueItem.count).to eq(1)
     end
+
     it "creates the queue item that is associated with the video" do
       session[:user_id] = Fabricate(:user).id
       video = Fabricate(:video)
       post :create, video_id: video.id
       expect(QueueItem.first.video).to eq(video)
     end
+
     it "creates the queue item that is associated with the signed in user" do
       santa = Fabricate(:user)
       session[:user_id] = santa.id
@@ -43,6 +46,7 @@ describe QueueItemsController do
       post :create, video_id: video.id
       expect(QueueItem.first.user).to eq(santa)
     end
+
     it "puts the video as the last one in the queue" do
       santa = Fabricate(:user)
       session[:user_id] = santa.id
@@ -53,6 +57,7 @@ describe QueueItemsController do
       south_park_queue_item = QueueItem.where(video_id: south_park.id, user_id: santa.id).first
       expect(south_park_queue_item.position).to eq(2)
     end
+
     it "does not add the video to the queue if the video is already in the queue" do
       santa = Fabricate(:user)
       session[:user_id] = santa.id
@@ -61,9 +66,41 @@ describe QueueItemsController do
       post :create, video_id: monk.id
       expect(santa.queue_items.count).to eq(1)
     end
+
     it "redirects to the sign in page for unauthenticated users" do
       post :create, video_id: 3
       expect(response).to redirect_to sign_in_path
+    end
+  end
+
+  describe "DELETE destroy" do
+    it "redirects back to the my_queue page" do
+      session[:user_id] = Fabricate(:user).id
+      q_item = Fabricate(:queue_item)
+      delete :destroy, id: q_item.id
+      expect(response).to redirect_to my_queue_path
+    end
+
+    it "destroys the record for authenticated user" do
+      santa = Fabricate(:user)
+      session[:user_id] = santa.id
+      q_item = Fabricate(:queue_item, user: santa)
+      delete :destroy, id: q_item.id
+      expect(QueueItem.count).to eq(0)
+    end
+
+    it "redirects to sign_in page for unauthenticated user" do
+      delete :destroy, id: 3
+      expect(response).to redirect_to sign_in_path
+    end
+
+    it "does not delete the queue_item if the item is not in the current user's queue" do
+      santa = Fabricate(:user)
+      bobafet = Fabricate(:user)
+      session[:user_id] = santa.id
+      queue_item = Fabricate(:queue_item, user: bobafet)
+      delete :destroy, id: queue_item.id
+      expect(QueueItem.count).to eq(1)
     end
   end
 end
