7bff7d8f2376b165bf7620b3cff2d1e0d518829b
Nathan Rosquist <nrosquist@gmail.com>
2014-02-09 16:34:34 -0800

    Create the video model, and seed the database

---
+++ db/migrate/20140210000129_create_videos.rb
@@ -0,0 +1,13 @@
+class CreateVideos < ActiveRecord::Migration
+  def change
+    create_table :videos do |t|
+      t.string :title
+      t.string :director
+      t.text :description
+      t.string :small_cover_url
+      t.string :large_cover_url
+
+      t.timestamps
+    end
+  end
+end
