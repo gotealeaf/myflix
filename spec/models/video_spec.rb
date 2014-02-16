d52a4a0e8d3694f1905384aed00ad80a6c5815a1
Jorge Sanchez <jsanchezsilvera@gmail.com>
2014-02-16 22:17:26 +0000

    Video model now has test/specs for presence validation of :title, :description

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -2,9 +2,9 @@ require 'spec_helper'
 
 describe Video do
   it "saves itself" do
-    video = Video.new(name: 'Video', description: 'description', short_image_url: '/tmp/test.jpg', large_image_url: '/tmp/test.jpg')
+    video = Video.new(title: 'Video', description: 'description', short_image_url: '/tmp/test.jpg', large_image_url: '/tmp/test.jpg')
     video.save
-    Video.first.name.should == 'Video'
+    Video.first.title.should == 'Video'
     Video.first.description.should == 'description'
     Video.first.large_image_url.should == '/tmp/test.jpg'
     Video.first.short_image_url.should == '/tmp/test.jpg'
@@ -12,5 +12,10 @@ describe Video do
 
   it "belongs_to Category model" do
     should belong_to(:category)
+  end
+
+  it "validates presence of :title, :description" do
+    should validate_presence_of(:title)
+    should validate_presence_of(:description)
   end
 end
