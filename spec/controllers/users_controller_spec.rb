5f41154ca1c0eae93c5d70166a46a7674901fcc6
Jack Collins <BlueSkyFlyer2@gmail.com>
2014-02-15 19:40:06 -0600

    Added user_registers_spec

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -10,10 +10,10 @@ describe UsersController do
 
 	describe "POST create" do
 		
-		context "with valid input" do
-
+		context "with valid personal info and valid card" do
+			let(:charge) { double(:charge, successful?: true) }
 			before do 
-				StripeWrapper::Charge.stub(:create)
+				StripeWrapper::Charge.should_receive(:create).at_least(1).times.and_return(charge)
 				post :create, user: Fabricate.attributes_for(:user) 
 			end
 
@@ -45,10 +45,30 @@ describe UsersController do
 				post :create, user: {email: 'joe@example.com', password: "password", full_name: "Joe Doe"}, invitation_token: invitation.token
 				expect(Invitation.first.token).to be_nil
 			end
-
 	  end
 
-	  context "with invalid input" do
+	  context "valid personal info and declined card" do
+	  	it "does not create a new user record" do
+	  		charge = double(:charge, successful?: false, error_message: "Your card was declined.")
+	  		StripeWrapper::Charge.should_receive(:create).and_return(charge)
+	  		post :create, user: Fabricate.attributes_for(:user), stripeToken: '123123'
+	  		expect(User.count).to eq(0)
+	  	end
+	  	it "renders the new template" do
+	  		charge = double(:charge, successful?: false, error_message: "Your card was declined.")
+	  		StripeWrapper::Charge.should_receive(:create).and_return(charge)
+	  		post :create, user: Fabricate.attributes_for(:user), stripeToken: '123123'
+	  		expect(response).to render_template :new
+	  	end
+	  	it "sets the flash error message" do
+	  		charge = double(:charge, successful?: false, error_message: "Your card was declined.")
+	  		StripeWrapper::Charge.should_receive(:create).and_return(charge)
+	  		post :create, user: Fabricate.attributes_for(:user), stripeToken: '123123'
+	  		expect(flash[:danger]).to be_present
+	  	end
+	  end
+
+	  context "with invalid personal info" do
 	  	 
 	  	before { post :create, user: { password: "password", full_name: "Kevin Smith" } }
 
@@ -63,10 +83,21 @@ describe UsersController do
 		  it "sets @user" do 
 		  	expect(assigns(:user)).to be_instance_of(User)
 		  end
+
+		  it "does not charge the card" do
+		  	StripeWrapper::Charge.should_not_receive(:create)
+		  	post :create, user: { email: "kevin@example.com" }
+		  end
+
+		  it "does not send out an email with invalid inputs" do
+	  		post :create, user: { email: "jack@example.com" }
+	  		expect(ActionMailer::Base.deliveries).to be_empty
+	  	end
 	  end
 
 	  context "sending emails" do 
-	  	before {StripeWrapper::Charge.stub(:create) }
+	  	let(:charge) { double(:charge, successful?: true) }
+	  	before {StripeWrapper::Charge.should_receive(:create).and_return(charge) }
 	  	after { ActionMailer::Base.deliveries.clear }
 
 	  	it "sends out an email to the user with valid inputs" do
@@ -78,13 +109,7 @@ describe UsersController do
 	  		post :create, user: { email: "jack@example.com", password: "password", full_name: "Jack Collins" }
 	  		expect(ActionMailer::Base.deliveries.last.body).to include("Jack Collins")
 	  	end
-
-	  	it "does not send out an email with invalid inputs" do
-	  		post :create, user: { email: "jack@example.com" }
-	  		expect(ActionMailer::Base.deliveries).to be_empty
-	  	end
 	  end
-
 	end
 
 	describe "GET show" do
--- spec/models/stripe_wrapper_spec.rb
+++ spec/models/stripe_wrapper_spec.rb
@@ -4,7 +4,6 @@ describe StripeWrapper do
 	describe StripeWrapper::Charge do
 		describe ".create" do
 			it "makes a successful charge", :vcr do
-				Stripe.api_key = ENV['STRIPE_SECRET_KEY']
 				token = Stripe::Token.create(
           :card => {
 				    :number => "4242424242424242",
@@ -19,9 +18,43 @@ describe StripeWrapper do
 					card: token,
 					description: "A valid charge"
 				)
+				expect(response).to be_successful
+			end
 
-				expect(response.amount).to eq(999)
-				expect(response.currency).to eq("usd")
+			it "makes a card declined charge", :vcr do
+				token = Stripe::Token.create(
+          :card => {
+				    :number => "4000000000000002",
+				    :exp_month => 2,
+				    :exp_year => 2018,
+				    :cvc => "314"
+				  }
+				).id
+
+				response = StripeWrapper::Charge.create(
+					amount: 999,
+					card: token,
+					description: "An invalid charge"
+				)
+				expect(response).not_to be_successful
+			end
+
+			it "returns the error message for declined charges", :vcr do
+				token = Stripe::Token.create(
+          :card => {
+				    :number => "4000000000000002",
+				    :exp_month => 2,
+				    :exp_year => 2018,
+				    :cvc => "314"
+				  }
+				).id
+
+				response = StripeWrapper::Charge.create(
+					amount: 999,
+					card: token,
+					description: "An invalid charge"
+				)
+				expect(response.error_message).to eq("Your card was declined.")
 			end
 		end
 	end
