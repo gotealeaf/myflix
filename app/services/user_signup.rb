a435aeca8644bc779ce4b7e0e83359aa78c05308
Jack Collins <BlueSkyFlyer2@gmail.com>
2014-02-16 12:42:09 -0600

    Implementing UserSignup service object and tests

---
+++ app/services/user_signup.rb
@@ -0,0 +1,48 @@
+class UserSignup
+
+	attr_reader :error_message
+
+	def initialize(user)
+		@user = user
+	end
+
+	def sign_up(sign_up_params)
+		if @user.valid?
+      charge = StripeWrapper::Charge.create(
+          :amount => 999, # amount in cents, again
+          :card => sign_up_params[:stripe_token],
+          :description => "Sign up charge for #{@user.email}"
+      )
+      if charge.successful?
+        @user.save
+        handle_invitation(sign_up_params[:invitation_token])
+        AppMailer.delay.send_welcome_email(@user.id)
+        @status = :success
+        self
+      else 
+        @status = :failed
+        @error_message = charge.error_message
+        self
+      end
+  	else
+  		@status = :failed
+  		@error_message = "Invalid user information. Please check the errors below."
+  		self
+  	end
+	end
+
+	def successful?
+		@status == :success
+	end
+
+	private
+
+	def handle_invitation(invitation_token)
+    if invitation_token.present?
+      invitation = Invitation.find_by(token: invitation_token)
+      @user.follow(invitation.inviter)
+      invitation.inviter.follow(@user)
+      invitation.update_column(:token, nil)
+    end
+  end
+end
