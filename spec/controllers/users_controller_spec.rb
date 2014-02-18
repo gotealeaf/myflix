f038308fbd8ee9ce7d71826e90a5ce926fa840ab
Matthew Barram <matthewbarram@gmail.com>
2014-02-18 17:01:50 +1000

    Added params testing to user_controller tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -14,7 +14,19 @@ describe UsersController do
     end
   end
   describe "POST #create" do
-    it 'sets up the @user with the user_params'
+    before(:each) do
+    end
+    it 'sets up the @user with the user_params' do
+      user_sample_params = Fabricate.attributes_for(:user)
+      post :create, user: user_sample_params
+      user = User.find_by(email: user_sample_params[:email])
+      expect(user.authenticate(user_sample_params[:password])).to be_instance_of(User)
+    end
+    it 'it fails to save if params incorrect' do
+      post :create, user: Fabricate.attributes_for(:user, fullname: nil)
+      expect(User.count).to eq(0)
+    end
+
     context 'attempts to save @user' do
       it 'sets the session[user_id] if sucessfully saved'
       it 'redirects the user if save was successful'
