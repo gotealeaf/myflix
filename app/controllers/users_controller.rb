bb5bdc24a3fd760beb202168147941858a26fd0e
Matthew Barram <matthewbarram@gmail.com>
2014-02-18 17:01:23 +1000

    Formatting fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,6 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-
     if @user.save
       flash[:success] = "Your account has been created!"
       session[:user_id] = @user.id
