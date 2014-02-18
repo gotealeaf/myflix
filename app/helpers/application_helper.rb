05856bbb4b6a253ec2a1ab558fe9720d10cd9114
Matthew Barram <matthewbarram@gmail.com>
2014-02-18 12:20:26 +1000

    Removed unneeded code re flash message

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,3 @@
 module ApplicationHelper
 
-  # def flash_class(level)
-  #   case level
-  #   when :notice then "alert alert-info"
-  #   when :success then "alert alert-success"
-  #   when :error, :alert then "alert alert-danger"
-  #   end
-  # end
 end
