7250f4ac71a7ce35d4110d570ed777747e5c57ef
Matt Pedersen <mpedersen2054@gmail.com>
2014-02-18 18:30:56 -0600

    add functionality to allow a user to follow another user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ActiveRecord::Base
 
   has_many :reviews, -> { order("created_at DESC") }
   has_many :queue_items, -> { order("position") }
+  has_many :following_relationships, class_name: 'Relationship', foreign_key: :follower_id
+  has_many :leading_relationships, class_name: 'Relationship', foreign_key: :leader_id
 
   has_secure_password validations: false
 
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,9 @@ Myflix::Application.routes.draw do
 
   get '/genre/:id', to: 'categories#show', as: 'category'
 
+  resources :relationships, only: [:destroy]
+  get '/people', to: 'relationships#index'
+
   resources :queue_items, only: [:create, :destroy]
   get '/my_queue', to: 'queue_items#index'
   post 'update_queue', to: 'queue_items#update_queue'
