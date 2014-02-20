4b230a7e20cdf7481fd816374f84dc30a9be728b
Rob Jewell <jewell.robertp@gmail.com>
2014-02-20 16:44:01 -0500

    Fixes Bug

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,7 +1,7 @@
 class VideosController < ApplicationController
 
 	def index
-		@show = Video.find_by(params[:id])
+		@videos = Video.all
 		#@tv_comedies = Video.first(6)
 		#@tv_dramas = Video.last(6)
 	end
