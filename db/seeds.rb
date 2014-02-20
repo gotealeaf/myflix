ab0206a5d58c1006a5218c761d0847dadd319f2d
Rob Jewell <jewell.robertp@gmail.com>
2014-02-20 12:05:39 -0500

    Seeds Database with Videos

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Video.destroy_all
+
+10.times do
+	Video.create!(title: "Futurama", description: "An awesome, nerdy spin off of the Simpsons ...")
+	Video.create!(title: "South Park", description: "A show with crude language and dark surreal humor...")
+	Video.create!(title: "Family Guy", description: "A cartoon that exhibits much of its humor in the form of cutaway gags...")
+end
