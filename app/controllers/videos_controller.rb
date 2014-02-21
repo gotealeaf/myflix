fbd364c9b2afbb80c5840d7f7ebd452a9aaae28e
Rob Jewell <jewell.robertp@gmail.com>
2014-02-21 14:15:17 -0500

    Creates Category Functionality and Improves Image to Object Programming

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -2,6 +2,9 @@ class VideosController < ApplicationController
 
 	def index
 		@videos = Video.all
+		@comedy = Category.find(1)
+		@drama = Category.find(2)
+		@reality = Category.find(3)
 		#@tv_comedies = Video.first(6)
 		#@tv_dramas = Video.last(6)
 	end
