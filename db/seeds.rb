0293c4bb13f57f64cc4b8f7049f8e72311c1ce3b
Rob Jewell <jewell.robertp@gmail.com>
2014-02-21 13:11:31 -0500

    Creating Category and Seeding Database

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,16 @@
 
 Video.destroy_all
 
-10.times do
-	Video.create!(title: "Futurama", description: "An awesome, nerdy spin off of the Simpsons ...")
-	Video.create!(title: "South Park", description: "A show with crude language and dark surreal humor...")
-	Video.create!(title: "Family Guy", description: "A cartoon that exhibits much of its humor in the form of cutaway gags...")
+4.times do
+	Video.create!(title: "Futurama", description: "An awesome, nerdy spin off of the Simpsons ...", category: 1)
+	Video.create!(title: "South Park", description: "A show with crude language and dark surreal humor...", category: 1)
+	Video.create!(title: "Family Guy", description: "A cartoon that exhibits much of its humor in the form of cutaway gags...", category: 1)
+
+	Video.create!(title: "Futurama", description: "An awesome, nerdy spin off of the Simpsons ...", category: 2)
+	Video.create!(title: "South Park", description: "A show with crude language and dark surreal humor...", category: 2)
+	Video.create!(title: "Family Guy", description: "A cartoon that exhibits much of its humor in the form of cutaway gags...", category: 2)
+
+	Video.create!(title: "Futurama", description: "An awesome, nerdy spin off of the Simpsons ...", category: 3)
+	Video.create!(title: "South Park", description: "A show with crude language and dark surreal humor...", category: 3)
+	Video.create!(title: "Family Guy", description: "A cartoon that exhibits much of its humor in the form of cutaway gags...", category: 3)
 end
