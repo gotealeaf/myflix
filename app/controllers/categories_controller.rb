89a0d05cc1809f685c11eb6c67334c1a81e4376b
Rob Jewell <jewell.robertp@gmail.com>
2014-02-22 00:38:46 -0500

    Creates Category View For Each Category

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,22 @@
+class CategoriesController < ApplicationController
+	def show
+		@categories = Category.all
+		@videos = Video.all
+		#@categories = Category.find_by(params[:id])
+		@comedy = Video.comedy
+		@drama = Video.drama
+		@reality = Video.reality
+	end
+
+	def comedy
+		@comedy = Video.comedy
+	end
+
+	def drama
+		@drama = Video.drama
+	end
+
+	def reality
+		@reality = Video.reality
+	end
+end
