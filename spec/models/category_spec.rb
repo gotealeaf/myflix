fec30df5f74a078ea4553eb6c82062dd42229ef1
Rob Jewell <jewell.robertp@gmail.com>
2014-02-23 16:57:26 -0500

    tests for models

---
+++ spec/models/category_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe Category do 
+	it "saves itself" do
+		category = Category.new(name: "comedies")
+		category.save
+		expect(Category.first).to eq(category)
+	end
+
+	it "has many videos" do
+		comedies = Category.create(name: "comedies")
+		south_park = Video.create(title: "South Park", description: "A really funny show", category: comedies)
+		futurama = Video.create(title: "Futurama", description: "it's funny and like the simpsons", category: comedies)
+		expect(comedies.videos).to include(south_park, futurama)
+	end
+end
---
+++ spec/models/video_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe Video do
+	it "saves itself" do
+		video = Video.new(title: "test title", description: "this is only a test....")
+		video.save
+		expect(Video.first.title).to eq("test title")
+	end	
+
+	it "belongs to a category" do
+		dramas = Category.create(name: "dramas")
+		true_detective = Video.new(title: "True Detective", description: "this movie is hilarious....", category: dramas)
+		expect(true_detective.category).to eq(dramas)
+	end
+
+
+end
+
+
