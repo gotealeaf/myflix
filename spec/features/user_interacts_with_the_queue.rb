e18abc7484910af862494f0da9d10fb5bf0b4f22
MattFlaig <matt.flaig@web.de>
2014-02-23 09:47:39 +0100

    feature spec: user interacts with queue

---
+++ spec/features/user_interacts_with_the_queue.rb
@@ -0,0 +1,62 @@
+require 'spec_helper'
+
+feature "User interacts with the queue" do
+  scenario "user adds and reorders videos in the queue" do
+  	thriller = Fabricate(:category)
+  	lost = Fabricate(:video, title: "Lost Highway", category: thriller)
+  	inc = Fabricate(:video, title: "Inception", category: thriller)
+  	drive = Fabricate(:video, title: "Drive", category: thriller)
+
+    sign_in
+
+    add_video_to_queue(lost)
+    expect_video_to_be_in_queue(lost)
+
+    visit video_path(lost)
+    expect_link_not_to_be_seen("+ My Queue")
+ 
+    add_video_to_queue(inc)
+    add_video_to_queue(drive)
+    
+    set_video_position(lost, 3)
+    set_video_position(inc, 1)
+    set_video_position(drive, 2)
+    update_queue
+
+    expect_video_position(lost,3)
+    expect_video_position(inc,1)
+    expect_video_position(drive,2)
+
+  end
+
+
+  def add_video_to_queue(video)
+    find("a[href='/videos/#{video.id}']").click
+  end
+
+  def expect_video_to_be_in_queue(video)
+    page.should have_content(video.title)
+  end
+
+  def expect_link_not_to_be_seen(link_text)
+    page.should_not have_content(link_text)
+  end
+
+  def add_video_to_queue(video)
+    visit home_path
+    find("a[href='/videos/#{video.id}']").click
+    click_link "+ My Queue"
+  end
+
+  def set_video_position(video,position)
+    find("input[data-video-id='#{video.id}']").set(position)
+  end
+
+  def update_queue
+    click_button "Update Instant Queue"
+  end
+
+  def expect_video_position(video,position)
+    expect(find("input[data-video-id='#{video.id}']").value).to eq(position.to_s)
+  end
+end
--- spec/features/user_sign_in_spec.rb
+++ spec/features/user_sign_in_spec.rb
@@ -3,10 +3,7 @@ require 'spec_helper'
 feature "user signs in" do
   scenario "with valid email and password" do
   	amanda = Fabricate(:user)
-  	visit sign_in_path
-  	fill_in "Email Address", with: amanda.email
-  	fill_in "Password", with: amanda.password
-  	click_button "Sign in"
+  	sign_in(amanda)
   	page.should have_content amanda.full_name
   end
 end
