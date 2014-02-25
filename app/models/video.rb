9c5436a72e4cc703cf76f4f21b26e6a42a96e39b
Rob Jewell <jewell.robertp@gmail.com>
2014-02-25 12:56:35 -0500

    Uses Shoulda Matchers in Specs

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,5 +1,5 @@
 class Video < ActiveRecord::Base
-	belongs_to :category	
-  validates :title, presence: true
-  validates :description, presence: true
+	belongs_to :category
+
+  validates_presence_of :title, :description
 end
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -1,17 +1,11 @@
 require 'spec_helper'
 
 describe Video do
-	it "saves itself" do
-		video = Video.new(title: "test title", description: "this is only a test....")
-		video.save
-		expect(Video.first.title).to eq("test title")
-	end	
 
-	it "belongs to a category" do
-		dramas = Category.create(name: "dramas")
-		true_detective = Video.new(title: "True Detective", description: "this movie is hilarious....", category: dramas)
-		expect(true_detective.category).to eq(dramas)
-	end
+  it { should belong_to(:category) }
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:description) }
+
 end
 
 
