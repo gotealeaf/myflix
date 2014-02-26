92eca7685eef9e2c350b393052ef131a9a74fc9d
Rob Jewell <jewell.robertp@gmail.com>
2014-02-25 19:00:51 -0500

    Trying to create 4 tests cases for video search method

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -6,6 +6,13 @@ describe Video do
   it { should validate_presence_of(:title) }
   it { should validate_presence_of(:description) }
 
+  describe ".search_by_title(search_term)" do
+    it "should return an empty array if it finds no videos"
+    it "should return an array of one video if it is an exact match"
+    it "should return an array of one video for a partial match"
+    it "should return an array of all videos" 
+  end
+
 end
 
 
