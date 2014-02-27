e6510e1ab9180136dc0f4ebea9736a76c375adfe
Rob Jewell <jewell.robertp@gmail.com>
2014-02-27 15:59:05 -0500

    6 tests pass for recent video method

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -30,7 +30,24 @@ describe Category do
       @beavis_butthead = Video.create(title: "Beavis and Butthead", description: "too much mtv", created_at: 10.day.ago, category: @comedy)
       expect(@comedy.recent_videos.count).to eq(6)
     end
-    it "should return the most recent 6 videos"
-    it "should return an empty array if the category does not have any videos"
+    it "should return the most recent 6 videos" do
+      @comedy = Category.create(name: "Comedy")
+      @family_guy = Video.create(title: "Family Guy", description: "A really twisted versino of the Simpsons", created_at: 3.day.ago, category: @comedy)
+      @family_matters = Video.create(title: "Family Matters", description: "Steve Urkel...Yay!", created_at: 1.day.ago, category: @comedy)
+      @always_sunny = Video.create(title: "Always Sunny", description: "O Philidelphia", created_at: 2.day.ago, category: @comedy)
+      @the_office = Video.create(title: "The Office", description: "Those brits sure are funny", created_at: 5.day.ago, category: @comedy)
+      @seinfeld = Video.create(title: "Seinfeld", description: "New York, New York", created_at: 7.day.ago, category: @comedy)
+      @king_of_hill = Video.create(title: "King of the Hill", description: "Texas is the best.", created_at: 6.day.ago, category: @comedy)
+      @simpsons = Video.create(title: "This simpsons", description: "Home is an OG", created_at: 4.day.ago, category: @comedy)
+      @beavis_butthead = Video.create(title: "Beavis and Butthead", description: "too much mtv", created_at: 10.day.ago, category: @comedy)
+      expect(@comedy.recent_videos).to eq([@family_matters, @always_sunny, @family_guy, @simpsons, @the_office, @king_of_hill])
+    end
+    it "should return an empty array if the category does not have any videos" do
+      @comedy = Category.create(name: "Comedy")
+      @drama = Category.create(name: "Drama")
+      @family_guy = Video.create(title: "Family Guy", description: "A really twisted versino of the Simpsons", created_at: 3.day.ago, category: @comedy)
+      @family_matters = Video.create(title: "Family Matters", description: "Steve Urkel...Yay!", created_at: 1.day.ago, category: @comedy)
+      expect(@drama.recent_videos.count).to eq(0)
+    end
   end
 end
