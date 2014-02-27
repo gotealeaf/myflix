e87d7c17627c33c796f0d3009c312452c30651ff
Rob Jewell <jewell.robertp@gmail.com>
2014-02-27 11:06:22 -0500

    works on tests for recent video method in category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,8 +1,3 @@
 class Category < ActiveRecord::Base
 	has_many :videos, -> { order('title') } 
-
-  def recent_videos
-    self.order(:created_at)
-    
-  end
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -3,21 +3,4 @@ require 'spec_helper'
 describe Category do 
 
 	it { should have_many(:videos) }
-
-  describe "#recent_videos" do
-
-    it "should return at most 6 videos per category" do
-      @comedy = Category.create(name: "Comedy")
-      @drama = Category.create(name: "Drama")
-      @videos = Video.create([{title: "Family Guy", description: "A really twisted versino of the Simpsons", created_at: 3.day.ago, category: @comedy}, 
-        {title: "True Detective", description: "A dark representation of crime and its investigation", created_at: 2.day.ago, category: @drama},
-        {title: "Breaking Bad", description: "A high school chemistry teacher cooks meth to pay for his medical bills", created_at: 5.day.ago, category: @drama},
-        {title: "Family Matters", description: "Steve Urkel...Yay!", created_at: 1.day.ago, category: @comedy}])
-      @comedy.videos.each do |comedy|
-        comedy
-      expect(@comedy.videos).to eq([{title: "Family Guys"}, {title: "Family Matters"}])
-      #expect(@drama.videos).to eq([@true_detective, @breaking_bad])
-    end
-
-  end
 end
