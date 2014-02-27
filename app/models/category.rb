1108dec28f138fb0a941dda45724e96c7fedf4e7
Rob Jewell <jewell.robertp@gmail.com>
2014-02-27 15:30:14 -0500

    three passing tests

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,7 @@
 class Category < ActiveRecord::Base
-	has_many :videos, -> { order('title') } 
+	has_many :videos, -> { order('created_at DESC') } 
 
   def recent_videos
-    videos
+    videos.limit(6)
   end
 end
