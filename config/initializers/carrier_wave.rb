4fffc09babdc6f162aaf224fe525f2fb94497f8a
vivijie <j.uloveit@gmail.com>
2014-02-27 22:10:29 +0800

    edit carrier wave config

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,5 +1,6 @@
 CarrierWave.configure do |config|
-  if Rails.env.production?
+  if Rails.env.staging? || Rails.env.production?
+    config.storage = :fog
     config.fog_credentials = {
       :provider               => 'AWS',                        # required
       :aws_access_key_id      => ENV['S3_KEY'],                        # required
