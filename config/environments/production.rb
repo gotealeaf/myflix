e9b88ab4834deb5e14fc74ce4a840fb6f509e0a3
Michael Huang <hyc1124@gmail.com>
2014-02-28 04:22:51 +0800

    set host parameter in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,8 @@ Myflix::Application.configure do
 
   config.action_mailer.delivery_method = :smtp
 
+  config.action_mailer.default_url_options = { host: 'michael-myflix.heroku.com' }
+
   ActionMailer::Base.smtp_settings = {
     :port           => ENV['MAILGUN_SMTP_PORT'],
     :address        => ENV['MAILGUN_SMTP_SERVER'],
