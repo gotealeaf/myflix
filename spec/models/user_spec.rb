c22d470fadb3ccc891de1147622f55e1343192bb
Michael Huang <hyc1124@gmail.com>
2014-02-27 19:28:39 +0800

    Refactor with Concern

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,9 +8,8 @@ describe User do
 	it { should have_many(:queue_items).order(:position)}
 	it { should have_many(:reviews).order("created_at DESC")}
 
-	it "generates a random token when the user is created" do
-		alice = Fabricate(:user)
-		expect(alice.token).to be_present
+	it_behaves_like "tokenable" do
+		let(:object) { Fabricate(:user) }
 	end
 
 	describe "#queued_video?" do
