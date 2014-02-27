966b317620d9e4d9fe2f7aeb3c74c933b808856b
Rob Jewell <jewell.robertp@gmail.com>
2014-02-27 16:23:59 -0500

    seeds database with varying date data

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,16 +8,20 @@
 
 Video.destroy_all
 
-10.times do
-	Video.create!(title: "Futurama", description: "An awesome, nerdy spin off of the Simpsons ...", category_id: 1, small_cover: "/tmp/futurama.jpg", large_cover: "/tmp/monk_large.jpg")
-	#Video.create!(title: "South Park", description: "A show with crude language and dark surreal humor...", category_id: 1, small_cover: "/tmp/south_park.jpg", large_cover: "/tmp/monk_large.jpg")
-	#Video.create!(title: "Family Guy", description: "A cartoon that exhibits much of its humor in the form of cutaway gags...", category_id: 1, small_cover: "/tmp/family_guy.jpg", large_cover: "/tmp/monk_large.jpg")
 
-	#Video.create!(title: "Futurama", description: "An awesome, nerdy spin off of the Simpsons ...", category_id: 2, small_cover: "/tmp/futurama.jpg", large_cover: "/tmp/monk_large.jpg")
-	Video.create!(title: "South Park", description: "A show with crude language and dark surreal humor...", category_id: 2, small_cover: "/tmp/south_park.jpg", large_cover: "/tmp/monk_large.jpg")
-	#Video.create!(title: "Family Guy", description: "A cartoon that exhibits much of its humor in the form of cutaway gags...", category_id: 2, category_id: 1, small_cover: "/tmp/family_guy.jpg", large_cover: "/tmp/monk_large.jpg")
+Video.create!(title: "Futurama", description: "An awesome, nerdy spin off of the Simpsons ...", category_id: 1, small_cover: "/tmp/futurama.jpg", large_cover: "/tmp/monk_large.jpg", created_at: 1.day.ago)
+Video.create!(title: "Futurama", description: "An awesome, nerdy spin off of the Simpsons ...", category_id: 1, small_cover: "/tmp/futurama.jpg", large_cover: "/tmp/monk_large.jpg", created_at: 2.day.ago)
+Video.create!(title: "Futurama", description: "An awesome, nerdy spin off of the Simpsons ...", category_id: 1, small_cover: "/tmp/futurama.jpg", large_cover: "/tmp/monk_large.jpg", created_at: 3.day.ago)
+Video.create!(title: "Futurama", description: "An awesome, nerdy spin off of the Simpsons ...", category_id: 1, small_cover: "/tmp/futurama.jpg", large_cover: "/tmp/monk_large.jpg", created_at: 4.day.ago)
+Video.create!(title: "Futurama", description: "An awesome, nerdy spin off of the Simpsons ...", category_id: 1, small_cover: "/tmp/futurama.jpg", large_cover: "/tmp/monk_large.jpg", created_at: 5.day.ago)
+Video.create!(title: "Futurama", description: "An awesome, nerdy spin off of the Simpsons ...", category_id: 1, small_cover: "/tmp/futurama.jpg", large_cover: "/tmp/monk_large.jpg", created_at: 6.day.ago)
+Video.create!(title: "Futurama", description: "An awesome, nerdy spin off of the Simpsons ...", category_id: 1, small_cover: "/tmp/futurama.jpg", large_cover: "/tmp/monk_large.jpg", created_at: 7.day.ago)
+Video.create!(title: "Futurama", description: "An awesome, nerdy spin off of the Simpsons ...", category_id: 1, small_cover: "/tmp/futurama.jpg", large_cover: "/tmp/monk_large.jpg", created_at: 8.day.ago)
 
-	#Video.create!(title: "Futurama", description: "An awesome, nerdy spin off of the Simpsons ...", category_id: 3, small_cover: "/tmp/futurama.jpg", large_cover: "/tmp/monk_large.jpg")
-	#Video.create!(title: "South Park", description: "A show with crude language and dark surreal humor...", category_id: 3, small_cover: "/tmp/south_park.jpg", large_cover: "/tmp/monk_large.jpg")
-	Video.create!(title: "Family Guy", description: "A cartoon that exhibits much of its humor in the form of cutaway gags...", category_id: 3, small_cover: "/tmp/family_guy.jpg", large_cover: "/tmp/monk_large.jpg")
-end
+Video.create!(title: "South Park", description: "A show with crude language and dark surreal humor...", category_id: 2, small_cover: "/tmp/south_park.jpg", large_cover: "/tmp/monk_large.jpg", created_at: 2.day.ago)
+Video.create!(title: "South Park", description: "A show with crude language and dark surreal humor...", category_id: 2, small_cover: "/tmp/south_park.jpg", large_cover: "/tmp/monk_large.jpg", created_at: 1.day.ago)
+
+Video.create!(title: "Family Guy", description: "A cartoon that exhibits much of its humor in the form of cutaway gags...", category_id: 3, small_cover: "/tmp/family_guy.jpg", large_cover: "/tmp/monk_large.jpg", created_at: 1.day.ago)
+Video.create!(title: "Family Guy", description: "A cartoon that exhibits much of its humor in the form of cutaway gags...", category_id: 3, small_cover: "/tmp/family_guy.jpg", large_cover: "/tmp/monk_large.jpg", created_at: 2.day.ago)
+Video.create!(title: "Family Guy", description: "A cartoon that exhibits much of its humor in the form of cutaway gags...", category_id: 3, small_cover: "/tmp/family_guy.jpg", large_cover: "/tmp/monk_large.jpg", created_at: 3.day.ago)
+Video.create!(title: "Family Guy", description: "A cartoon that exhibits much of its humor in the form of cutaway gags...", category_id: 3, small_cover: "/tmp/family_guy.jpg", large_cover: "/tmp/monk_large.jpg", created_at: 4.day.ago)
