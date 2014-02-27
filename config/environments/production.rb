b4cdd6dfc2fad895b957f16f01f7693bed61de7f
Michael Huang <hyc1124@gmail.com>
2014-02-28 01:55:23 +0800

    Use Mailgun to send out emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,4 +17,18 @@ Myflix::Application.configure do
   config.i18n.fallbacks = true
 
   config.active_support.deprecation = :notify
+
+  config.action_mailer.delivery_method = :smtp
+
+  ActionMailer::Base.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'michael-myflix.heroku.com',
+    :authentication => :plain,
+  }
+  ActionMailer::Base.delivery_method = :smtp
+
+
 end
