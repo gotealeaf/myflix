27a519e408c44d1d498e59462c56798ea2c4b69b
Rob Jewell <jewell.robertp@gmail.com>
2014-02-28 14:23:13 -0500

    works on views for sign in and log in

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Myflix::Application.routes.draw do
+  get 'log_in', to: 'sessions#create'
+  get 'signup', to: 'users#new'
+  root to: 'welcome#index'  
+
   resources :videos, only: [:index, :show] do
     collection do
       get 'search', to: 'videos#search'
