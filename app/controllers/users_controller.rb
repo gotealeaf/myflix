d1611507fd3b0940b78fff7573f0a8b597f3c420
Rob Jewell <jewell.robertp@gmail.com>
2014-03-02 14:21:53 -0500

    log in and registration forms and validations are working. must wire up authentication next

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(params[:user])
+    @user = User.new(user_params)
     if @user.save
       redirect_to sign_in_path, notice: "Thank you for signing up"
     else
