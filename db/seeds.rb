719e9ab587464c1bb834b1f4ec51d620db51d788
Bruce Li <ascendbruce@gmail.com>
2014-03-02 09:21:53 +0800

    Create the video model, and seed the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+def path_to_url(path)
+  host = "http://localhost:3000"
+  "#{host}#{path}"
+end
+
+(1..4).each do |i|
+  Video.create!(
+    title: "Monk - #{i}",
+    description: "Monk is an American comedy-drama detective mystery television series created by Andy Breckman and starring Tony Shalhoub as the eponymous character, Adrian Monk. It originally ran from 2002 to 2009 and is primarily a police procedural series, and also exhibits comic and dramatic tones in its exploration of the main characters' personal lives. The series was produced by Mandeville Films and Touchstone Television in association with Universal Television.",
+    small_cover_url: path_to_url("/tmp/monk.jpg"),
+    large_cover_url: path_to_url("/tmp/monk_large.jpg")
+  )
+
+  Video.create!(
+    title: "Family Guy - #{i}",
+    description: "Family Guy is an American adult animated sitcom created by Seth MacFarlane for the Fox Broadcasting Company. The series centers on the Griffins, a family consisting of parents Peter and Lois; their children Meg, Chris, and Stewie; and their anthropomorphic pet dog Brian. The show is set in the fictional city of Quahog, Rhode Island, and exhibits much of its humor in the form of cutaway gags that often lampoon American culture.",
+    small_cover_url: path_to_url("/tmp/family_guy.jpg"),
+    large_cover_url: path_to_url("/tmp/family_guy_large.jpg")
+  )
+
+  Video.create!(
+    title: "Futurama - #{i}",
+    description: "Futurama is an American adult animated science fiction sitcom created by Matt Groening and developed by Groening and David X. Cohen for the Fox Broadcasting Company. The series follows the adventures of a late-20th-century New York City pizza delivery boy, Philip J. Fry, who, after being unwittingly cryogenically frozen for one thousand years, finds employment at Planet Express, an interplanetary delivery company in the retro-futuristic 31st century. The series was envisioned by Groening in the late 1990s while working on The Simpsons, later bringing Cohen aboard to develop storylines and characters to pitch the show to Fox.",
+    small_cover_url: path_to_url("/tmp/futurama.jpg"),
+    large_cover_url: path_to_url("/tmp/futurama_large.jpg")
+  )
+
+  Video.create!(
+    title: "South Park - #{i}",
+    description: "South Park is an American adult animated sitcom created by Trey Parker and Matt Stone for the Comedy Central television network. Intended for mature audiences, the show has become famous for its crude language and dark, surreal humor that satirizes a wide range of topics. The ongoing narrative revolves around four boys—Stan Marsh, Kyle Broflovski, Eric Cartman, and Kenny McCormick—and their bizarre adventures in and around the titular Colorado town.",
+    small_cover_url: path_to_url("/tmp/south_park.jpg"),
+    large_cover_url: path_to_url("/tmp/south_park_large.jpg")
+  )
+end
