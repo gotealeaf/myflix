34a83b700fef34dccfc3bfda57d1c7a9bbb51f82
AZS <ambshar@gmail.com>
2014-03-05 00:11:12 +0100

    created recent_videos method using TDD

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,4 +3,8 @@ class Category < ActiveRecord::Base
   #has_many :videos, -> { distinct }, through: :video_categories
   has_many :videos, -> { order(:title) }
   validates :name, presence: true, uniqueness: true
+
+  def recent_videos
+    Video.where(category_id: self.id).order('created_at DESC').first(6)
+  end
 end
