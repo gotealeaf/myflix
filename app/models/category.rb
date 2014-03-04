81a9435afbd6b45b2b389fd2d19f91ab8020e5e9
John Morgan <jtmorgan61@gmail.com>
2014-03-04 14:39:21 -0800

    recent_videos method w/tests (pre-solution)

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,7 @@
 class Category < ActiveRecord::Base
-  has_many :videos, -> { order "title" }
+  has_many :videos
+
+  def recent_videos
+    self.videos.order("created_at DESC").limit(6).to_a
+  end
 end
