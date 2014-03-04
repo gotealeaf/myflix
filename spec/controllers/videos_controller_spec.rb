0fb4087a5327f534781668496d69a7f83802ecd5
Rob Jewell <jewell.robertp@gmail.com>
2014-03-04 11:43:20 -0500

    passing tests for rendering show action for setting @video variable

--- spec/controllers/videos_controller_spec.rb
+++ spec/controllers/videos_controller_spec.rb
@@ -1,14 +1,20 @@
 require 'spec_helper'
 
 describe VideosController do 
+  
   describe "GET show" do
     it "sets the @video variable" do
+      session[:user_id] = Fabricate(:user).id
       south_park = Fabricate(:video)
 
-      get :show, id: '1'
-      expect(assigns(:videos)).to eq(south_park)
+      get :show, id: 1
+      expect(assigns(:video)).to eq(south_park)
     end
-
-    it "renders the show template"
+    it "renders the show template" do
+      session[:user_id] = Fabricate(:user).id
+      south_park = Fabricate(:video)
+      get :show, id: 1
+      expect(response).to render_template :show
+    end
   end
 end
