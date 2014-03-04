74ab4776cea174df8ee8b35156687a0d4f15815f
Rick Cid <rick@hispand.com>
2014-03-04 10:25:07 -0700

    Added email configurations for welcome email

---
+++ app/mailers/app_mailer.rb
@@ -0,0 +1,7 @@
+class AppMailer < ActionMailer::Base
+  
+  def send_welcome_email(user)
+    @user = user
+    mail to: user.email, from: "info@myflix.com", subject: "Welcome to Myflix!"
+  end
+end
