aa792c5adc00c15140becac33b07e28fd6c2deb2
John Morgan <jtmorgan61@gmail.com>
2014-03-04 13:10:28 -0800

    search bar added

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Myflix::Application.routes.draw do
   get 'ui(/:action)', controller: 'ui'
   get '/home', to: 'videos#index'
 
-  resources :videos, only: [:show]
+  resources :videos, only: [:show] do
+    collection do
+      get 'search', to: 'videos#search'
+    end
+  end
   resources :categories, only: [:show]
 end
