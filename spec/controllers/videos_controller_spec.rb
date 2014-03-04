ee77df4a715fbaee93767e92346ed788519d4431
Rob Jewell <jewell.robertp@gmail.com>
2014-03-04 09:33:00 -0500

    debugging show action in test folder for video controller

--- spec/controllers/videos_controller_spec.rb
+++ spec/controllers/videos_controller_spec.rb
@@ -1,13 +1,11 @@
 require 'spec_helper'
-# show
-# search
 
 describe VideosController do 
   describe "GET show" do
     it "sets the @video variable" do
       south_park = Fabricate(:video)
 
-      get :show
+      get :show, id: '1'
       expect(assigns(:videos)).to eq(south_park)
     end
 
