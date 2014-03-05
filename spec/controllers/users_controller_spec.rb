54fc0d1b9161b579284e01286562871f09c3d7f9
Rob Jewell <jewell.robertp@gmail.com>
2014-03-05 14:45:34 -0500

    follows Kevin's solutions for testing user controller. however database bug still not fixed. there are still 3 users when there shouldn't be any

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -4,48 +4,40 @@ require 'pry'
 describe UsersController do
 
   describe "GET new" do
-    it "redirects the user away from the sign in page if user is authenticated" do
-      session[:user_id] = Fabricate(:user).id
-
-      get :new
-      expect(response).to redirect_to videos_path
-    end
-
     it "sets the @user variable if the user is inauthenticated" do
-      user = Fabricate.build(:user)
-
-      get :new
-      expect(assigns(:user)).to be_new_record
-    end
-
-    it "sets the @user variable which is an instance of the User class" do
-      user = Fabricate.build(:user)
-
       get :new
       expect(assigns(:user)).to be_instance_of(User)
     end
   end
 
   describe "POST create" do
-    it "adds a new record to the database when input is valid" do
-      user = Fabricate(:user)
-      post :create, user: {full_name: user.full_name, email: user.email, password: user.password}
-      
-      expect(User.first.full_name).to eq(user.full_name)
-      expect(User.first.email).to eq(user.email)
-      expect(User.first.password_digest).to eq(user.password_digest)
-    end
+    context "with valid input" do
+      it "creates the user" do
+        post :create, user: { full_name: "Bill", email: "bill@example.com", password: 'password' }
+        
+        expect(User.count).to eq(1)
+      end
 
-    it "does not create a record when input is invalid " do
-      post :create, user: { full_name: "jane" }
-      expect(User.count).to eq(0)
+      it "redirects to the videos path" do
+        post :create, user: { full_name: "Bill", email: "bill@example.com", password: 'password' }
+        expect(response).to redirect_to videos_path
+      end
     end
+    context "with invalid input" do
+      it "does not create a record when input is invalid " do
+        post :create, user: { full_name: "jane" }
+        expect(User.count).to eq(0)
+      end
 
-    it "renders new template when input is invalid" do
-      post :create, user: { full_name: "jane" }
-      expect(response).to render_template :new
+      it "renders new template when input is invalid" do
+        post :create, user: { full_name: "jane" }
+        expect(response).to render_template :new
+      end
+      it "sets @user" do
+        post :create, user: { full_name: "jane" }
+        expect(assigns(:user)).to be_instance_of(User)
+      end
     end
-
   end
 end
 
