c5ab8432f23d010c874f2b517af1833b90fe8000
Rob Jewell <jewell.robertp@gmail.com>
2014-03-05 11:19:55 -0500

    finishes sessions controller tests, fixing failed tests in users controller

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -3,9 +3,28 @@ require 'spec_helper'
 
 describe SessionsController do
 
-  describe 'GET new' do
-    it "redirects the user away from the log in page if user is authenticated"
-    it "sets the user in the session when the password and email are valid"
-    it "redirects the user to the videos path when the password and email are valid"
+  describe 'POST create' do
+    
+    it "sets the user in the session if user is authenticated" do
+      user = Fabricate(:user)
+
+      post :create, {email: user.email, password: user.password}
+      expect(session[:user_id]).to eq(user.id)
+    end
+
+    it "redirects the user to the new tempate when the password and email are invalid" do
+      user = Fabricate(:user)
+
+      post :create, {email: user.email}
+      expect(response).to render_template('sessions/new')
+    end
+
+    it "sends the user to his dashboard when the user is authenticated" do
+      user = Fabricate(:user)
+
+      post :create, {email: user.email, password: user.password}
+      user.id == session[:user_id]
+      expect(response).to redirect_to videos_path
+    end
   end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,27 +3,30 @@ require 'pry'
 
 describe UsersController do
 
-  describe "GET new"
+  describe "GET new" do
     it "redirects the user away from the sign in page if user is authenticated" do
       session[:user_id] = Fabricate(:user).id
 
       get :new
       expect(response).to redirect_to videos_path
     end
+
     it "sets the @user variable if the user is inauthenticated" do
       user = Fabricate.build(:user)
 
       get :new
       expect(assigns(:user)).to be_new_record
     end
+
     it "sets the @user variable which is an instance of the User class" do
       user = Fabricate.build(:user)
 
       get :new
       expect(assigns(:user)).to be_instance_of(User)
     end
+  end
 
-  describe "POST create"
+  describe "POST create" do
     it "adds a new record to the database when input is valid" do
       user = Fabricate(:user)
       post :create, user: {full_name: user.full_name, email: user.email, password: user.password}
@@ -35,7 +38,6 @@ describe UsersController do
 
     it "does not create a record when input is invalid " do
       post :create, user: { full_name: "jane" }
-
       expect(User.count).to eq(0)
     end
 
@@ -43,5 +45,7 @@ describe UsersController do
       post :create, user: { full_name: "jane" }
       expect(response).to render_template :new
     end
+
+  end
 end
 
