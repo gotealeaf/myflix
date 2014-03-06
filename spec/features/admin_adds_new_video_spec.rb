dd20d606b40730c30ada6888fb0a152d2fb8296d
Michael Huang <hyc1124@gmail.com>
2014-03-06 17:37:03 +0800

    Feature test for adding video

---
+++ spec/features/admin_adds_new_video_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+feature "Admin adds new video" do
+	scenario "Admin successfully adds a new video" do
+		admin = Fabricate(:admin)
+		Fabricate(:category, name: "Drama")
+		sign_in(admin)
+		visit new_admin_video_path
+
+		fill_in "Title", with: "Monk"
+		select "Drama", from: "Category"
+		fill_in "Description", with: "nice movie"
+		attach_file "Large Cover", "spec/support/uploads/monk_large.jpg"
+		attach_file "Small Cover", "spec/support/uploads/monk.jpg"
+		fill_in "Video URL", with: "http://www.example.com/my_video.mp4"
+		click_button "Add Video"
+
+		sign_out
+		sign_in
+
+		visit video_path(Video.first)
+		expect(page).to have_selector("img[src='/uploads/monk_large.jpg']")
+		expect(page).to have_selector("a[href='http://www.example.com/my_video.mp4']")
+	end
+end
