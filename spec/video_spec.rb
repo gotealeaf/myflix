892d9b22fa7bf991b51e3a88407732acbf7d8474
crokobit <crokobit@gmail.com>
2014-03-07 11:13:29 +0800

    merge shoulda_matchers

---
+++ spec/video_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe Video do
+  it { should have_many(:categories).through(:video_categories) }
+  it { should validate_presence_of :title}
+  it { should validate_presence_of :description}
+end
