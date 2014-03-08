62ffc5327d916b16a208bfdfe98666bdd9f4a79a
Bruce Li <ascendbruce@gmail.com>
2014-03-08 20:16:49 +0800

    Test the presence validations of videos model

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -24,4 +24,14 @@ describe Video do
 
     video.category.should == category_animation
   end
+
+  it "requires title to be present" do
+    video = Video.new(description: "Frozen is a 2013 American 3D computer-animated musical fantasy-comedy film produced by Walt Disney Animation Studios and released by Walt Disney Pictures.")
+    expect { video.save }.to change { Video.count }.by(0)
+  end
+
+  it "requires description to be present" do
+    video = Video.new(title: "Frozen")
+    expect { video.save }.to change { Video.count }.by(0)
+  end
 end
