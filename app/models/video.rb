03e7874ef84439d7f0263843a00a7698375f871a
crokobit <crokobit@gmail.com>
2014-03-08 17:45:49 +0800

    Assignment: Implement 'search_by_title' on the video model

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,5 +1,11 @@
 class Video < ActiveRecord::Base
   has_many :video_categories
   has_many :categories, through: :video_categories
+  #video should not have many category in this project; however, I am lazy to fix this...
   validates_presence_of :title, :description
+
+  def self.search_by_title(search_term)
+    return [] if search_term.blank?
+    where("title LIKE ?", "%#{search_term}%").order("created_at DESC")
+  end
 end
--- spec/video_spec.rb
+++ spec/video_spec.rb
@@ -1,7 +1,36 @@
+require 'pry'
 require 'spec_helper'
 
 describe Video do
   it { should have_many(:categories).through(:video_categories) }
   it { should validate_presence_of :title}
   it { should validate_presence_of :description}
+
+  describe "search_by_title" do
+      
+    before(:each) do
+      @sport_1 = Video.create(title: "sport_1", description: "x", created_at: 1.day.ago)
+      @sport_2 = Video.create(title: "sport_2", description: "x")
+      @news_1 = Video.create(title: "social news", description: "x")
+    end
+
+    it "returns [] when no match"  do
+      expect(Video.search_by_title("gossip")).to eq []
+    end
+    
+    it "returns one element array for single exact match" do
+       expect(Video.search_by_title("sport_1")).to eq [@sport_1]
+    end
+    it "returns one element array for single partial match" do
+       expect(Video.search_by_title("news")).to eq [@news_1]
+    end
+    it "returns matched element array for mutiple matchs by order of created time" do
+       expect(Video.search_by_title("sport")).to eq [@sport_2, @sport_1]
+    end
+    it "returns [] for empty search" do
+      expect(Video.search_by_title(" ")).to eq []
+    end
+  end
+
 end
+
