7e10f8dcb7e70049861d8e79eea464767117fa0b
Jose Casanova <josescasanova@gmail.com>
2014-03-10 10:27:38 -0400

    created video model

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Myflix::Application.routes.draw do
   get 'ui(/:action)', controller: 'ui'
+  get 'home', to: 'videos#index'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+video = Video.create(title: 'Family Guy', description: "Family guy description here", small_cover_url: "/tmp/family_guy.jpg", large_cover_url: "/tmp/family_guy.jpg")
+video = Video.create(title: 'Futurama', description: "Futurama description here", small_cover_url: "/tmp/futurama.jpg", large_cover_url: "/tmp/futurama.jpg")
+video = Video.create(title: 'South Park', description: "South Park description here", small_cover_url: "/tmp/south_park.jpg", large_cover_url: "/tmp/south_park.jpg")
+video = Video.create(title: 'Monk', description: "Monk description here", small_cover_url: "/tmp/monk.jpg", large_cover_url: "/tmp/monk_large.jpg")
