3f19def603767788245495974463e795e59785d2
Zac Clay <zacclay@gmail.com>
2014-03-11 14:24:24 -0400

    First spec

---
+++ spec/models/video_spec.rb
@@ -0,0 +1,9 @@
+require "spec_helper"
+
+describe Video do 
+  it "saves itself" do
+    video = Video.new(title: "monk", description: "a great video")
+    video.save
+    expect(Video.first).to eq(video)
+  end
+end
