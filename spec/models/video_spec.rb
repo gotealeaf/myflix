bc875453f7a649bf49adfdd4699913d184e5b175
Zac Clay <zacclay@gmail.com>
2014-03-11 15:29:57 -0400

    Added more specs

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -13,4 +13,14 @@ describe Video do
     video.categories << [cat1, cat2]
     expect(video.categories).to include(cat1, cat2) 
   end
+
+  it "does not save a video without a title" do
+    video = Video.create(description: "Some video")
+    expect(Video.count).to eq(0)
+  end
+
+  it "does not save a video without a description" do
+    video = Video.create(title: "Some other video")
+    expect(Video.count).to eq(0)
+  end
 end
