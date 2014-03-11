3171edda4be78dba47dc0cc5717434bc7f9aecdd
Michael Huang <hyc1124@gmail.com>
2014-03-12 00:27:15 +0800

    Feature test for registration with credit card

---
+++ spec/features/user_registers_spec.rb
@@ -0,0 +1,79 @@
+require 'spec_helper'
+
+feature 'User registers', { js: true, vcr: true } do
+	background do
+		visit register_path
+	end
+	scenario "with valid user info and valid card" do
+		fill_in_valid_user_info
+		fill_in_valid_card
+		click_button "Sign Up"
+		expect(page).to have_content("Thank you for registering with MyFlix. Please sign in now.")
+	end
+
+	scenario "with valid user info and invalid card" do
+		fill_in_valid_user_info
+		fill_in_invalid_card
+		click_button "Sign Up"
+		expect(page).to have_content("This card number looks invalid")
+	end
+
+	scenario "with valid user info and declined card" do
+		fill_in_valid_user_info
+		fill_in_declined_card
+		click_button "Sign Up"
+		expect(page).to have_content("Your card was declined.")
+	end
+
+	scenario "with invalid user info and valid card" do
+		fill_in_invalid_user_info
+		fill_in_valid_card
+		click_button "Sign Up"
+		expect(page).to have_content("Invalid user information. Please check the error below.")
+	end
+
+	scenario "with invalid user info and invalid card" do
+		fill_in_invalid_user_info
+		fill_in_invalid_card
+		click_button "Sign Up"
+		expect(page).to have_content("This card number looks invalid")
+	end
+
+	scenario "with invalid user info and declined card" do
+		fill_in_invalid_user_info
+		fill_in_declined_card
+		click_button "Sign Up"
+		expect(page).to have_content("Invalid user information. Please check the error below.")
+	end
+
+	def fill_in_valid_user_info
+		fill_in "Email Address", with: "alice@example.com"
+		fill_in "Password", with: "password"
+		fill_in "Full Name", with: "Alice Gordon"
+	end
+
+	def fill_in_invalid_user_info
+		fill_in "Email Address", with: "alice@example.com"
+	end
+
+	def fill_in_valid_card
+		fill_in "Credit Card Number", with: "4242424242424242"
+		fill_in "Security Code", with: "123"
+		select "7 - July", from: "date_month"
+		select "2015", from: "date_year"
+	end
+
+	def fill_in_invalid_card
+		fill_in "Credit Card Number", with: "123"
+		fill_in "Security Code", with: "123"
+		select "7 - July", from: "date_month"
+		select "2015", from: "date_year"
+	end
+
+	def fill_in_declined_card
+		fill_in "Credit Card Number", with: "4000000000000002"
+		fill_in "Security Code", with: "123"
+		select "7 - July", from: "date_month"
+		select "2015", from: "date_year"
+	end
+end
