86580b5f232610859b6d509dc60124dbed5b95d7
Zac Clay <zacclay@gmail.com>
2014-03-11 19:55:14 -0400

    added search_by_title to Video model

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -7,16 +7,33 @@ describe Video do
 
   describe "search_by_title" do
     it "returns an empty array if there is no match" do
-
+      futurama = Video.create(title: "Futurama", description: "Space Travel!")
+      back_to_future = Video.create(title: "Back To The Future", description: "Time Travel!")
+      expect(Video.search_by_title("hello")).to eq([])
     end    
+
     it "returns an array of one video for an exact match" do
-      
+      futurama = Video.create(title: "Futurama", description: "Space Travel!")
+      back_to_future = Video.create(title: "Back To The Future", description: "Time Travel!")
+      expect(Video.search_by_title("Futurama")).to eq([futurama])
     end
+
     it "returns an array of one video for a partial match" do
-      
-    end
+      futurama = Video.create(title: "Futurama", description: "Space Travel!")
+      back_to_future = Video.create(title: "Back To The Future", description: "Time Travel!")
+      expect(Video.search_by_title("urama")).to eq([futurama])
+    end 
+
     it "returns an array of all matched ordered by created_at" do
-      
+      futurama = Video.create(title: "Futurama", description: "Space Travel!", created_at: 1.day.ago)
+      back_to_future = Video.create(title: "Back To The Future", description: "Time Travel!")
+      expect(Video.search_by_title("Futur")).to eq([back_to_future, futurama])
+    end
+
+    it "returns an empty array for a search with an empty string " do
+      futurama = Video.create(title: "Futurama", description: "Space Travel!")
+      back_to_future = Video.create(title: "Back To The Future", description: "Time Travel!")
+      expect(Video.search_by_title("")).to eq([])
     end
   end
 end
