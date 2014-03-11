75576b80ffab44657c10123381ab4f140751f180
Jose Casanova <josescasanova@gmail.com>
2014-03-11 10:51:16 -0400

    added search + tests

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -5,4 +5,37 @@ describe Video do
   it { should have_many(:categories).through(:video_categories) }
   it { should validate_presence_of(:title) }
   it { should validate_presence_of(:description) }
+
+  describe "search_by_title" do
+    it "returns an array if there is no match" do
+      futurama = Video.create(title: "Futurama", description: "Space travel show")
+      back_to_future = Video.create(title: "Back To The Future", description: "Time travel movie")
+      expect(Video.search_by_title("hello")).to eq([])
+    end
+
+    it "returns an array of one video for an exact match" do
+      futurama = Video.create(title: "Futurama", description: "Space travel show")
+      back_to_future = Video.create(title: "Back To The Future", description: "Time travel movie")
+      expect(Video.search_by_title("Futurama")).to eq([futurama])
+    end
+
+    it "returns an array of one video for a partial match" do
+      futurama = Video.create(title: "Futurama", description: "Space travel show")
+      back_to_future = Video.create(title: "Back To The Future", description: "Time travel movie")
+      expect(Video.search_by_title("urama")).to eq([futurama])
+    end
+
+    it "returns an array of all matches ordered by created_at" do
+      futurama = Video.create(title: "Futurama", description: "Space travel show", created_at: 1.day.ago)
+      back_to_future = Video.create(title: "Back To The Future", description: "Time travel movie")
+      expect(Video.search_by_title("Futur")).to eq([back_to_future, futurama])
+    end
+
+    it "returns an empty array for a search with an empty string" do
+      futurama = Video.create(title: "Futurama", description: "Space travel show", created_at: 1.day.ago)
+      back_to_future = Video.create(title: "Back To The Future", description: "Time travel movie")
+      expect(Video.search_by_title("")).to eq([])
+    
+    end
+  end
 end
