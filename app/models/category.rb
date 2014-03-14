abdf76ceeb2b3eedf6cb59162b9ebe25d1f2a09d
crokobit <crokobit@gmail.com>
2014-03-14 10:11:59 +0800

    Assignment: Show Only Recent Videos

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,8 @@
 class Category < ActiveRecord::Base
   has_many :video_categories
   has_many :videos, through: :video_categories
+
+  def recent_videos
+    videos.order("created_at DESC").first(6)
+  end
 end
