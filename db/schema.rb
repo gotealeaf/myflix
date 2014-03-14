b5238740a2a668cb81fbc08048a6d910b71d6f95
Rob Jewell <jewell.robertp@gmail.com>
2014-03-14 16:10:55 -0400

    building add que item to my que

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140313212503) do
+ActiveRecord::Schema.define(version: 20140314175406) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -20,11 +20,13 @@ ActiveRecord::Schema.define(version: 20140313212503) do
   end
 
   create_table "ques", force: true do |t|
-    t.integer "list_order"
-    t.string  "video_title"
-    t.string  "genre"
-    t.integer "user_id"
-    t.integer "video_id"
+    t.integer  "list_order"
+    t.string   "video_title"
+    t.string   "genre"
+    t.integer  "user_id"
+    t.integer  "video_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "reviews", force: true do |t|
