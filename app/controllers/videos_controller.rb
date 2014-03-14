287e081c44c17b95faac9ea66117db9051934354
Zac Clay <zacclay@gmail.com>
2014-03-14 16:09:06 -0400

    Added search to UI

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -8,4 +8,9 @@ class VideosController < ApplicationController
     @video = Video.find(params[:id])
   end
 
+  def search
+    @results = Video.search_by_title(params[:search_term])
+  end
+
 end
+ 
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Myflix::Application.routes.draw do
   get 'ui(/:action)', controller: 'ui'
   get 'home', to: 'videos#index'
 
-  resources :videos, except: [:destroy]
+  resources :videos, except: [:destroy] do
+    collection do
+      get :search, to: "videos#search"
+    end
+  end
   resources :categories, except: [:destroy]
 end
