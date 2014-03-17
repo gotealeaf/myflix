d62705670d6bb5c99ab83b3e4fc69c511aa2c272
Rob Jewell <jewell.robertp@gmail.com>
2014-03-17 09:51:44 -0400

    refactoring code from review

--- app/controllers/que_items_controller.rb
+++ app/controllers/que_items_controller.rb
@@ -6,8 +6,9 @@ class QueItemsController < ApplicationController
   end
 
   def create
+    
     @video = Video.find_by(params[:video_id])
-    QueItem.create(que_item_params) unless QueItem.where(video_id: @video).first == @video
+    QueItem.create(que_item_params) #unless QueItem.where(video_id: @video).first == @video
     redirect_to que_items_path
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,8 @@ Myflix::Application.routes.draw do
   get 'sign_in', to: 'sessions#new'
   post 'log_in', to: 'sessions#create'
   get 'register', to: 'users#new'
-  get 'log_out', to: 'sessions#destroy', as: 'log_out'
-  get 'que_items', to: 'que_items#index', as: 'que_items'
-  post 'que_item', to: 'que_items#create', as: 'video_que_item'
+  get 'log_out', to: 'sessions#destroy'
+  get 'que_items', to: 'que_items#index'
   root to: 'pages#index' 
 
   resources :videos, only: [:index, :show] do
@@ -14,6 +13,7 @@ Myflix::Application.routes.draw do
     resources :reviews, only: [:create]
   end
 
+  resources :que_items, only: [:create]
   resources :categories, only: [:show] 
   resources :users, only: [:create]
   get 'ui(/:action)', controller: 'ui'
