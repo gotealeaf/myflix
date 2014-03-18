990167d5776149de0a2e4df8747bc4a95c84b9e3
Rob Jewell <jewell.robertp@gmail.com>
2014-03-18 10:05:24 -0400

    all tests pass. howeverqueue_item page only contains one queue_item after numerous attempts to add more

--- app/controllers/queue_items_controller.rb
+++ app/controllers/queue_items_controller.rb
@@ -6,8 +6,8 @@ class QueueItemsController < ApplicationController
   end
 
   def create
-    video = Video.find_by(params[:video_id])
-    QueueItem.create(video: video, user: current_user) unless current_user.queue_items.map(&:video).include?(video)
+    @video = Video.find_by(params[:video_id])
+    QueueItem.create(video: @video, user: current_user, position: current_user.queue_items.count + 1) unless current_user.queue_items.map { |n| n.video }.include?(@video)
     redirect_to queue_items_path
   end
 
