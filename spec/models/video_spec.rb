848e2dc124b8d97f7f1e157e2a478aed7a04c601
Charles Quirin <charles@cr-q.com>
2014-03-17 21:51:45 -0400

    Added test for Video.search_by_title method. TODO: Create method usign TDD.

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -4,4 +4,42 @@ describe Video do
   it { should validate_presence_of(:title) }
   it { should validate_presence_of(:description) }
   it { should belong_to(:category) }
+
+  describe "#search_by_title" do
+    it "returns an empty array if there is no match" do
+      Video.create(title: "Monk", description: "A funny man.")
+      search_rslt = Video.search_by_title("not")
+
+      expect(search_rslt).to eq([])
+    end
+    
+    it "returns an array of a matched object" do
+      monk = Video.create(title: "Monk", description: "A funny man.")
+      search_rslt = Video.search_by_title("Monk")
+
+      expect(search_rslt).to eq([monk])      
+    end
+
+    it "returns an array of a matched object in a different case" do
+      monk = Video.create(title: "Monk", description: "A funny man.")
+      search_rslt = Video.search_by_title("mONK")
+
+      expect(search_rslt).to eq([monk])
+    end
+
+    it "returns an array of a partially matched object" do
+      familyguy = Video.create(title: "Family Guy", description: "A funny family.")
+      search_rslt = Video.search_by_title("Family")
+
+      expect(search_rslt).to eq([familyguy])
+    end
+
+    it "returns an array of multiple matched obejcts" do
+      familymatters = Video.create(title: "Family Matters", description: "A Chicago family.")
+      familyguy = Video.create(title: "Family Guy", description: "A funny family.")
+      search_rslt = Video.search_by_title("Family")
+
+      expect(search_rslt).to eq([familymatters, familyguy])
+    end
+  end
 end
