9740076ae742c7cb48326c90cbb542ff47427ab1
Charles Quirin <charles@cr-q.com>
2014-03-18 19:17:55 -0400

    Added search action and appropriate route. Added search view. Replaced dummy search in header.

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -3,4 +3,7 @@ class VideosController < ApplicationController
 
   def home; end
 
+  def search
+    @videos = Video.search_by_title(params[:search])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Myflix::Application.routes.draw do
   get 'ui(/:action)', controller: 'ui'
 
   get 'home', to: 'videos#home'
+  get 'search', to: 'videos#search'
 
   resources :categories, only: :show
 end
