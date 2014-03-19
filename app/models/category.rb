730e1cdeaa4f45d8a5f35b880b5bc2910ea0d26c
Charles Quirin <charles@cr-q.com>
2014-03-19 00:43:03 -0400

    Category.recent_videos passes first two tests.

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,7 @@
 class Category < ActiveRecord::Base
   has_many :videos, order: :title
+
+  def recent_videos
+    videos = Video.where(category: self)
+  end
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -2,4 +2,22 @@ require 'spec_helper'
 
 describe Category do
   it { should have_many(:videos) }
+
+  describe "#recent_videos" do
+    it "returns an empty array if there are no videos" do
+      comedy = Category.create(name: "Comedy")
+      expect(comedy.recent_videos).to eq([])
+    end
+
+    it "returns an array of all videos if there are fewer then 6" do
+      comedy = Category.create(name: "Comedy")
+      family_guy = Video.create(title: "Family Guy", description: "A loud drunk father.", category: comedy)
+      futurama = Video.create(title: "Futurama", description: "An pizza delievery boy gets frozen.", category: comedy)
+
+      expect(comedy.recent_videos).to eq([family_guy, futurama])
+    end
+    
+    it "returns an array the 6 most recently modified videos if there are more then 6"
+    it "retuns an array of videos in chronological order"
+  end
 end
