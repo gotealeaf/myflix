6d7a1a55007e4346fa623cb7aa4d46c1e76966a5
Charles Quirin <charles@cr-q.com>
2014-03-18 23:18:47 -0400

    Small syntax change in routes.

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Myflix::Application.routes.draw do
 
   get 'home', to: 'videos#home'
 
-  resources :categories, only: :show
-  resources :videos, only: :show do
+  resources :categories, only: [:show]
+  resources :videos, only: [:show] do
     collection do
       get 'search', to: 'videos#search'
     end
