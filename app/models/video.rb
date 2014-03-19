431eb3b34576e2701ba39e3594967a3858d9e1c9
Jake Gavin <jakegavin@gmail.com>
2014-03-18 18:04:05 -0700

    Add title and description validations

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,4 +1,6 @@
 class Video < ActiveRecord::Base
   has_many :video_categories
-  has_many :categories, through: :video_categories
+  has_many :categories, through: :video_categories, order: :name
+
+  validates_presence_of :title, :description
 end
