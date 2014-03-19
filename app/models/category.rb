a9981db0fab0cdef88318a0d5fcd9c2d3aa8b7cc
Jake Gavin <jakegavin@gmail.com>
2014-03-19 13:10:15 -0700

    Error with order

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ActiveRecord::Base
   has_many :video_categories
-  has_many :videos, through: :video_categories, order: :title
+  has_many :videos, through: :video_categories, -> { order :title }, class_name: "Video"
 
   validates :name, presence: true, uniqueness: true
 end
--- app/models/video.rb
+++ app/models/video.rb
@@ -1,6 +1,6 @@
 class Video < ActiveRecord::Base
   has_many :video_categories
-  has_many :categories, through: :video_categories, order: :name
+  has_many :categories, through: :video_categories, -> { order: name }
 
   validates_presence_of :title, :description
 
