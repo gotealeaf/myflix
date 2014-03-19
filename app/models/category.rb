06ebf2b7c76ead490ee5a11ed78878ffe0952bda
Jake Gavin <jakegavin@gmail.com>
2014-03-18 18:04:24 -0700

    Add name validations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,6 @@
 class Category < ActiveRecord::Base
   has_many :video_categories
-  has_many :videos, through: :video_categories
+  has_many :videos, through: :video_categories, order: :title
+
+  validates :name, presence: true, uniqueness: true
 end
