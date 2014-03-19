59a68a0e1a6f49b14817776612d52c41395da480
Charles Quirin <charles@cr-q.com>
2014-03-18 23:20:02 -0400

    Removed "get" route to "videos#home"

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Myflix::Application.routes.draw do
   get 'ui(/:action)', controller: 'ui'
   root to: 'videos#home'
-
-  get 'home', to: 'videos#home'
-
+  
   resources :categories, only: [:show]
   resources :videos, only: [:show] do
     collection do
