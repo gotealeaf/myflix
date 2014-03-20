d003b74986364b0b88e2f957605d8701aa1053df
Jake Gavin <jakegavin@gmail.com>
2014-03-20 10:58:03 -0700

    Add users and authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    !!current_user
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Myflix::Application.routes.draw do
-  root 'videos#index'
+  root 'static_pages#front'
 
   get 'ui(/:action)', controller: 'ui'
 
+  get 'register', to: 'users#new'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  get 'logout', to: 'sessions#destroy'
+
   get 'home', to: 'videos#index'
 
+  resources :users, only: [:create]
   resources :categories, only: [:show]
   resources :videos, only: [:show] do
     collection do 
