bb777770a0871d63e17de9dd78ee6836acd78595
Jake Gavin <jakegavin@gmail.com>
2014-03-19 19:49:11 -0700

    Add recent_videos method to Category

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,4 +3,9 @@ class Category < ActiveRecord::Base
   has_many :videos, -> { order :title }, through: :video_categories
 
   validates :name, presence: true, uniqueness: true
+
+  def recent_videos
+    return [] if videos.empty?
+    videos.sort_by { |video| video.created_at }.reverse.take(6)
+  end
 end
