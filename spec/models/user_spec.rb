16cac659b52dac6d832a939ef370a6700c2e8749
Charles Quirin <charles@cr-q.com>
2014-03-21 19:52:17 -0400

    Added custom test for "validate_uniqueness_of \:email" to fix issue with password_confirmation"

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,4 +5,9 @@ describe User do
   it { should validate_presence_of(:full_name) }
   it { should validate_presence_of(:password).on(:create) }
   it { should have_secure_password }
+
+  it "should validate uniqueness of email" do
+    user = User.create(email: "smaug@lonelymountain.com", full_name: "Smaug the Magnificent", password: "gold", password_confirmation: "gold")
+    expect(user).to validate_uniqueness_of :email
+  end
 end
