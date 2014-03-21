2616a8c05284c4e195504f828572410b15882cb7
Jake Gavin <jakegavin@gmail.com>
2014-03-21 12:18:26 -0700

    Add video controller spec

---
+++ spec/controllers/video_controller_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+describe VideosController do
+  describe "GET #show" do 
+    let (:video) { Fabricate(:video) }  
+    it "sets the @video variable for authenticated users" do
+      session[:user_id] = Fabricate(:user).id
+      get :show, id: video
+      expect(assigns(:video)).to eq video
+    end
+    
+    it "redirects to the root path for unauthenticated users" do
+      get :show, id: video
+      expect(response).to redirect_to root_path
+    end
+  end
+
+  describe "GET #search" do
+    let (:video) { Fabricate(:video) }  
+    it "sets the @videos variable for authenticated users" do 
+      session[:user_id] = Fabricate(:user).id
+      get :search, search_term: video.title
+      expect(assigns(:videos)).to include(video)
+    end
+    let (:video) { Fabricate(:video) }      
+    it "redirects to root path with unathenticated user" do
+      get :search, search_term: video.title
+      expect(response).to redirect_to root_path
+    end
+  end
+end
