e8920c5e0a3a8d32b95feecb3102eab972b5e24c
Jake Gavin <jakegavin@gmail.com>
2014-03-22 01:21:13 -0700

    Restrict access based on authentication status

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,7 @@
 class SessionsController < ApplicationController
+  before_action :require_user, only: :destroy
+  before_action :require_no_user, only: [:new, :create]
+  
   def new
     redirect_to home_path if logged_in?
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :require_no_user, only: [:new, :create]
+
   def new
     @user = User.new
   end
