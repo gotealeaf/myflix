69ff39c333dcaab9be2151b4e91cf9bdd3222758
Rick Cid <rick@hispand.com>
2014-03-22 20:59:00 -0700

    Added additional files up to mod9

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,6 +19,7 @@ Myflix::Application.configure do
   config.active_support.deprecation = :notify
 
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: 'myflixrc.herokuapp.com' }
   
   config.action_mailer.smtp_settings = {
     :port                 => ENV['MAILGUN_SMTP_PORT'],    
