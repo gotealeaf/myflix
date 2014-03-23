879c346af7e80da35806bf9ec4bd4f6edebb8652
Jake Gavin <jakegavin@gmail.com>
2014-03-23 01:18:22 -0700

    Add spec for review variable

--- spec/controllers/video_controller_spec.rb
+++ spec/controllers/video_controller_spec.rb
@@ -8,7 +8,12 @@ describe VideosController do
       get :show, id: video
       expect(assigns(:video)).to eq video
     end
-    
+    it "sets the @review variable for authenticated users" do 
+      session[:user_id] = Fabricate(:user).id
+      get :show, id: video
+      expect(assigns(:review)).to be_new_record
+      expect(assigns(:review)).to be_instance_of(Review)
+    end
     it "redirects to the root path for unauthenticated users" do
       get :show, id: video
       expect(response).to redirect_to root_path
