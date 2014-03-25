8e14cfc53b8b0ce88583d880643516fdd66b6060
Jake Gavin <jakegavin@gmail.com>
2014-03-24 17:08:06 -0700

    Reorganize routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,24 @@
 Myflix::Application.routes.draw do
-  root 'static_pages#front'
 
-  get 'ui(/:action)', controller: 'ui'
-
-  get 'home', to: 'videos#index'
-  get 'register', to: 'users#new'
-  get 'login', to: 'sessions#new'
-  post 'login', to: 'sessions#create'
-  get 'logout', to: 'sessions#destroy'
-
-  
-  get 'queue', to: 'queue_items#index'
-  
-  resources :users, only: [:create]
-  resources :categories, only: [:show]
-  
   resources :videos, only: [:show] do
     resources :reviews, only: [:create]
     collection do 
       get 'search', to: 'videos#search'
     end
-  end
+  end  
+  get 'home', to: 'videos#index'
+
+  resources :categories, only: [:show]
+
+  resources :users, only: [:create]
+  get 'register', to: 'users#new'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  get 'logout', to: 'sessions#destroy'
+
+  get 'queue', to: 'queue_items#index'
+  
+  root 'static_pages#front'
+
+  get 'ui(/:action)', controller: 'ui'
 end
