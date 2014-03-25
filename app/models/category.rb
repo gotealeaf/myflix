143f41cbbed3a8fa5a2071295d0beb6a5ba17a4d
Jake Gavin <jakegavin@gmail.com>
2014-03-24 17:23:06 -0700

    Correct sorting

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,10 +1,10 @@
 class Category < ActiveRecord::Base
   has_many :video_categories
-  has_many :videos, -> { order :title }, through: :video_categories
+  has_many :videos, -> { order "created_at DESC" }, through: :video_categories
 
   validates :name, presence: true, uniqueness: true
 
   def recent_videos
-    videos.sort_by { |video| video.created_at }.reverse.take(6)
+    videos.take(6)
   end
 end
