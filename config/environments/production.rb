3e3aa36b09fdac157dd289d7058c0c4bcaa7b8c0
Desmonddai583 <desmonddai583@gmail.com>
2014-03-31 01:15:01 +0800

    add mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,4 +18,16 @@ Myflix::Application.configure do
 
   config.active_support.deprecation = :notify
 
+  ActionMailer::Base.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'sleepy-river-6688.herokuapp.com',
+    :authentication => :plain,
+  }
+  ActionMailer::Base.delivery_method = :smtp
+
+
+
 end
