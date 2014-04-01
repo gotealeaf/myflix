9a5e6fb0e9c918ba1b47d0dcf851b6ad7029399a
Scotty Runyan <runyans7@gmail.com>
2014-04-01 17:23:06 -0400

    Initial project setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Myflix::Application.routes.draw do
   get 'ui(/:action)', controller: 'ui'
+
+  get '/home', to: 'videos#index'
+
+  resources :videos, only: [:show]
+  resources :categories, only: [:show]
 end
