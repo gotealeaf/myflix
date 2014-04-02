984860774a62f37e58cf85582e45a0558cb50ba6
Desmonddai583 <desmonddai583@gmail.com>
2014-04-02 20:13:11 +0800

    add bucket

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -5,7 +5,7 @@ CarrierWave.configure do |config|
       :aws_access_key_id      => ENV['S3_KEY'],                        # required
       :aws_secret_access_key  => ENV['S3_SECRET'],                        # required
     }
-    config.fog_directory  = 'name_of_directory'                     # required
+    config.fog_directory  = 'myflix-assets'                     # required
   else
     config.storage = :file
     config.enable_processing = Rails.env.development?
