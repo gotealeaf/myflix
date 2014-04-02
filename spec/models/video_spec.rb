4a72a58942f6f4a5cc070b330e2a2723864262ff
Scotty Runyan <runyans7@gmail.com>
2014-04-02 17:16:28 -0400

    Video belongs to category

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -6,5 +6,11 @@ describe Video do
     video.save
     expect(Video.first).to eq(video)
   end
+
+  it 'should belong to a category' do
+    category = Category.new(name: 'Comedies')
+    video = Video.new(title: 'South Park', description: 'Funny stuff', category: category)
+    expect(video.category).to eq(category)
+  end
 end
 
