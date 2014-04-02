1e1220eb8b22ea7ce2374005b9bd394580a8d96f
Desmonddai583 <desmonddai583@gmail.com>
2014-04-02 21:43:06 +0800

    modify uploader

--- app/uploaders/small_cover_uploader.rb
+++ app/uploaders/small_cover_uploader.rb
@@ -2,4 +2,12 @@ class SmallCoverUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   process :resize_to_fill => [166, 236]
+
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
 end
