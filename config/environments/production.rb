51a487f67ff2135f8cfb70a778c7f27d566b6344
Rob Jewell <jewell.robertp@gmail.com>
2014-04-04 18:23:37 -0400

    refactors config/production code

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,6 +18,14 @@ Myflix::Application.configure do
 
   config.active_support.deprecation = :notify
   
-  config.action_mailer.delivery_method = :mailgun
-  config.action_mailer.mailgun_settings = {domain: 'http://myflix-rjewell2200.herokuapp.com/'}
+  config.action_mailer.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'http://myflix-rjewell2200.herokuapp.com/',
+    :authentication => :plain,
+  }
+  ActionMailer::Base.delivery_method = :smtp
 end
