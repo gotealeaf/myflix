cce68b94080b3456ff0ddb0798612128fdf45474
clintonjnelson <clintonjnelson@live.com>
2014-04-09 14:44:31 -0700

    updated configs for domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,7 +21,7 @@ Myflix::Application.configure do
   :address        => ENV['MAILGUN_SMTP_SERVER'],
   :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
   :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-  :domain         => 'yourapp.heroku.com',
+  :domain         => 'arcane-stream-2628.heroku.com',
   :authentication => :plain }
   #ActionMailer::Base.delivery_method = :smtp
 
