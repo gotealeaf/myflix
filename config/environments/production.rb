13367e781c8f6b791405c33b1e3b52917e261d44
clintonjnelson <clintonjnelson@live.com>
2014-04-09 14:29:18 -0700

    udpated for mailgun smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,16 +15,25 @@ Myflix::Application.configure do
   config.assets.digest = true
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings   = {
-      address:              'smtp.gmail.com',
-      port:                 587,
-      domain:               'example.com',
-      user_name:            ENV["GMAIL_USERNAME"],
-      password:             ENV["GMAIL_PASSWORD"],
-      authentication:       'plain',
-      enable_starttls_auto: true  }
+  ActionMailer::Base.smtp_settings = {
+  :port           => ENV['MAILGUN_SMTP_PORT'],
+  :address        => ENV['MAILGUN_SMTP_SERVER'],
+  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+  :domain         => 'yourapp.heroku.com',
+  :authentication => :plain }
+  #ActionMailer::Base.delivery_method = :smtp
 
   config.i18n.fallbacks = true
 
   config.active_support.deprecation = :notify
 end
+# GMAIL CONFIG
+# config.action_mailer.smtp_settings   = {
+#       address:              'smtp.gmail.com',
+#       port:                 587,
+#       domain:               'example.com',
+#       user_name:            ENV["GMAIL_USERNAME"],
+#       password:             ENV["GMAIL_PASSWORD"],
+#       authentication:       'plain',
+#       enable_starttls_auto: true  }
