344ee40fb67ff675530192eb7f017637a4c641d0
Rob Jewell <jewell.robertp@gmail.com>
2014-04-10 10:57:44 -0400

    comments out config.action_mailer.delivery_method = :smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,7 +18,7 @@ Myflix::Application.configure do
 
   config.active_support.deprecation = :notify
   
-  config.action_mailer.delivery_method = :smtp
+  #config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: "myflix-rjewell2200.herokuapp.com/" }
   ActionMailer::Base.smtp_settings = {
     :port           => ENV['MAILGUN_SMTP_PORT'],
