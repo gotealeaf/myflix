ebfe1c40fb53e68e38d6a09bb47bd449a21297b1
Rob Jewell <jewell.robertp@gmail.com>
2014-04-10 10:54:43 -0400

    changes config settings for heroku mail gun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,7 +19,7 @@ Myflix::Application.configure do
   config.active_support.deprecation = :notify
   
   config.action_mailer.delivery_method = :smtp
-  #config.action_mailer.default_url_options = { host: "myflix-rjewell2200.herokuapp.com/" }
+  config.action_mailer.default_url_options = { host: "myflix-rjewell2200.herokuapp.com/" }
   ActionMailer::Base.smtp_settings = {
     :port           => ENV['MAILGUN_SMTP_PORT'],
     :address        => ENV['MAILGUN_SMTP_SERVER'],
