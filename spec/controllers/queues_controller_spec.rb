512198319f15adf9f4df7275fdaa8ba0304fce86
crokobit <crokobit@gmail.com>
2014-04-11 10:30:10 +0800

    DRY controller_specs using let and let!

--- spec/controllers/queues_controller_spec.rb
+++ spec/controllers/queues_controller_spec.rb
@@ -1,40 +1,39 @@
 require 'spec_helper'
 
 describe QueuesController do
+
+  let(:user)  { Fabricate(:user) }
+  let(:video) { Fabricate(:video) }
+  let!(:queue_target) { QueueItem.where(user: user).count + 1 }
+
   describe "queues#create" do
     context "logged in" do
       before(:each) do
-        @user = Fabricate(:user)
-        @video = Fabricate(:video)
-        @queue_target = QueueItem.count + 1
-        session[:user_id] = @user.id
-        post :create, user_id: @user.id, video_id: @video.id
+        session[:user_id] = user.id
+        post :create, user_id: user.id, video_id: video.id
       end
       it "sets user relationship" do
-        expect(assigns(:queue_item).user).to eq @user
+        expect(assigns(:queue_item).user).to eq user
       end
       it "sets video relationship" do
-        expect(assigns(:queue_item).video).to eq @video
+        expect(assigns(:queue_item).video).to eq video
       end
       it "sets position as last item" do
-        expect(assigns(:queue_item).position).to eq @queue_target 
+        expect(assigns(:queue_item).position).to eq queue_target 
       end
       it "redirects to my_queue_path" do
         expect(response).to redirect_to my_queue_path
       end
       it "creates nothing if already created" do
         expect {
-          post :create, user_id: @user.id, video_id: @video.id
+          post :create, user_id: user.id, video_id: video.id
         }.to change(QueueItem, :count).by(0)
       end
     end
 
     it "redirects to login page if no logging in" do
-      @user = Fabricate(:user)
-      @video = Fabricate(:video)
-      @queue_target = QueueItem.where(user: @user).count + 1
       session[:user_id] = nil
-      post :create, user_id: @user.id, video_id: @video.id
+      post :create, user_id: user.id, video_id: video.id
       expect(response).to redirect_to root_path      
     end
   end
@@ -42,16 +41,13 @@ describe QueuesController do
   describe "queues#index" do
     context "logged in" do
       before do
-        @user = Fabricate(:user)
-        @video = Fabricate(:video)
-        session[:user_id] = @user.id
-        @queue_target = QueueItem.where(user: @user).count + 1
-        @queue_item = QueueItem.create(
-          video: @video,
-          user: @user,
-          position: @queue_target
+        session[:user_id] = user.id
+        queue_item = QueueItem.create(
+          video: video,
+          user: user,
+          position: queue_target
         ) 
-        delete :destroy, id: @queue_item
+        delete :destroy, id: queue_item
       end
       it "deletes requested queue_item" do
         expect(QueueItem.count).to eq 0
@@ -62,31 +58,25 @@ describe QueuesController do
       end
     end
     it "redirects to root_path if no logging in" do
-      @user = Fabricate(:user)
-      @video = Fabricate(:video)
       session[:user_id] = nil
-      @queue_target = QueueItem.where(user: @user).count + 1
-      @queue_item = QueueItem.create(
-        video: @video,
-        user: @user,
-        position: @queue_target
+      queue_item = QueueItem.create(
+        video: video,
+        user: user,
+        position: queue_target
       ) 
-      delete :destroy, id: @queue_item
+      delete :destroy, id: queue_item
       expect(response).to redirect_to root_path 
     end
     it "do nothing if it is not current user's queue" do
-      @user1 = Fabricate(:user)
-      @user2 = Fabricate(:user)
-      @video = Fabricate(:video)
-      session[:user_id] = @user1.id
-      @queue_target = QueueItem.where(user: @user2).count + 1
-      @queue_item = QueueItem.create(
-        video: @video,
-        user: @user2,
-        position: @queue_target
+      user2 = Fabricate(:user)
+      session[:user_id] = user.id
+      queue_item = QueueItem.create(
+        video: video,
+        user: user2,
+        position: queue_target
       ) 
       expect{
-        delete :destroy, id: @queue_item
+        delete :destroy, id: queue_item
       }.to change(QueueItem, :count).by(0)
     end
   end
--- spec/controllers/videos_controller_spec.rb
+++ spec/controllers/videos_controller_spec.rb
@@ -2,34 +2,32 @@ require 'spec_helper'
 
 describe VideosController do
 
+
+  let(:user)  { Fabricate(:user) }
+  let(:video) { Fabricate(:video) }
+
   describe "videos#show" do
-    before(:each) do
-      @video = Fabricate(:video)
-    end
-    it "assigns specific video to @video by id when logon" do
+    it "assigns specific video to video by id when logon" do
       session[:user_id] = Fabricate(:user).id
-      get :show, id: @video.id
-      expect(assigns(:video)).to eq @video
+      get :show, id: video.id
+      expect(assigns(:video)).to eq video
     end
     it "redirects to root when not logging in" do
       session[:user_id] = nil
-      get :show, id: @video.id
+      get :show, id: video.id
       expect(response).to redirect_to root_path
     end
   end  
 
   describe "videos#search" do
-    before(:each)  do
-      @videos = Fabricate(:video)
-    end
     it "assigns specific videos to @videos by search_item" do
       session[:user_id] = Fabricate(:user).id
-      get :search, search_item: @videos.title 
-      expect(assigns(:videos)).to eq [@videos]
+      get :search, search_item: video.title 
+      expect(assigns(:videos)).to eq [video]
     end
     it "redirects to root when not logging in" do
       session[:user_id] = nil
-      get :search, search_item: @videos.title 
+      get :search, search_item: video.title 
       expect(response).to redirect_to root_path
     end 
   end
@@ -38,15 +36,13 @@ describe VideosController do
   describe "videos#create_review" do
     context "logged in" do
       before(:each) do
-        @user = Fabricate(:user)
-        session[:user_id] = @user.id
+        session[:user_id] = user.id
       end
 
       context "data pass validate" do
         before(:each) do
-          @video = Fabricate(:video)
           @review = Fabricate.build(:review)
-          post :create_review, id: @video.id, rating: @review.rating, review_description: @review.review_description
+          post :create_review, id: video.id, rating: @review.rating, review_description: @review.review_description
         end
         it "assigns rating to @review" do
           expect(assigns(:review).rating).to eq @review.rating
@@ -55,10 +51,10 @@ describe VideosController do
           expect(assigns(:review).review_description).to eq @review.review_description
         end
         it "assigns video to @review" do
-          expect(assigns(:review).video).to eq @video
+          expect(assigns(:review).video).to eq video
         end
         it "assigns current user to @review" do
-          expect(assigns(:review).user).to eq @user
+          expect(assigns(:review).user).to eq user
         end
         it "save reviews to db when validation success" do
           expect(Review.count).to eq 1
@@ -70,8 +66,7 @@ describe VideosController do
       
       context "data not pass validate" do
         before(:each) do
-          @video = Fabricate(:video)
-          post :create_review, id: @video.id, rating: nil, review_description: ""
+          post :create_review, id: video.id, rating: nil, review_description: ""
         end
         it "does not save review to db" do
           expect(Review.count).to eq 0
@@ -81,9 +76,8 @@ describe VideosController do
 
     context "not logged in" do
       before(:each) do
-        @video = Fabricate(:video)
         @review = Fabricate.build(:review)
-        post :create_review, id: @video.id, rating: @review.rating, review_description: @review.review_description
+        post :create_review, id: video.id, rating: @review.rating, review_description: @review.review_description
       end
 
       it "does not save rview to db" do
