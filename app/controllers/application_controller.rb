ad0ea4faccb67f3c0599f3b6eeffe44655f75f01
unai-trecet <unai.trecet@gmail.com>
2014-04-12 12:43:28 +0200

    Current_user improvement added: @current_user ||= User.find(session[:user_id]) if session[:user_id]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   end
 
   def current_user
-  	User.find(session[:user_id]) if session[:user_id]
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
   helper_method :current_user
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,12 +1,9 @@
 class Category < ActiveRecord::Base 
-  # has_many :video_categories
-  # has_many :videos, -> { order(:title) }, through: :video_categories
   has_many :videos
 
   validates_presence_of :name
 
   def self.recent_videos category
-  	videos = Category.find(category).videos.sort{ |x, y| y.created_at <=> x.created_at }.take(6)
+  	Category.find(category).videos.order('created_at DESC').take(6)
   end
-
 end
