b68384a1032968bf270b1142705b6c6ea74dd680
Charles Quirin <charles@cr-q.com>
2014-04-13 14:42:34 -0400

    Moved some logic from QueueItemsController to User model.

--- app/controllers/queue_items_controller.rb
+++ app/controllers/queue_items_controller.rb
@@ -21,16 +21,16 @@ class QueueItemsController < ApplicationController
   def destroy
     queue_item = QueueItem.find(params[:id])
     queue_item.destroy if current_user == queue_item.user
-    normalize_queue_items_positions
+    current_user.normalize_queue_items_positions
     redirect_to my_queue_path
   end
 
   def update_queue
-    params_array = params[:queue_items]
+    queue_items_data = params[:queue_items]
 
     begin
-      update_queue_items
-      normalize_queue_items_positions
+      current_user.update_queue_items(queue_items_data)
+      current_user.normalize_queue_items_positions
     rescue ActiveRecord::RecordInvalid
       flash[:warning] = "Invalid position entry."
     end
@@ -42,20 +42,5 @@ class QueueItemsController < ApplicationController
 
   def user_next_queue_position
     current_user.queue_items.count + 1
-  end
-
-  def update_queue_items
-    ActiveRecord::Base.transaction do
-      params[:queue_items].each do |queue_items_data|
-        queue_item = QueueItem.find(queue_items_data[:id].to_i)
-        queue_item.update!(position: queue_items_data[:position]) if queue_item.user == current_user
-      end
-    end
-  end
-
-  def normalize_queue_items_positions
-    current_user.queue_items.each_with_index do |queue_item, index|
-      queue_item.update(position: index + 1)
-    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,19 @@ class User < ActiveRecord::Base
   has_many :videos, through: :queue_items
 
   has_secure_password
+
+  def update_queue_items(queue_items_data)
+    ActiveRecord::Base.transaction do
+      queue_items_data.each do |queue_item_data|
+        queue_item = QueueItem.find(queue_item_data[:id].to_i)
+        queue_item.update!(position: queue_item_data[:position]) if queue_item.user == self
+      end
+    end
+  end
+
+  def normalize_queue_items_positions
+    queue_items.each_with_index do |queue_item, index|
+      queue_item.update(position: index + 1)
+    end
+  end
 end
