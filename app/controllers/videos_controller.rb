449ebb77fec5e9732e68ad464c08130d4b69b184
Sue Anne Teo <sueanneteo@gmail.com>
2014-04-15 15:06:19 +0000

    added video controller and model

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,3 +1,31 @@
 class VideosController < ApplicationController
+  before_action :video_params, only: [:create, :edit, :show, :update]
+  before_action :require_video, only: [:edit, :update, :show]
+    
+  def index
+    @videos = Video.all
+  end
   
+  def new
+    @video = Video.new
+  end
+  
+  def create
+    @video = Video.new(video_params)
+  end
+  
+  def edit
+  end
+  
+  def update
+  end
+  
+  def show
+  end
+  
+  private
+  
+  def video_params
+    params.require(:video).permit(:title, :description)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Videos
+
+video1 = Video.create(title: "Futurama", description: "Pizza boy Philip J. Fry awakens in the 31st century after 1,000 years of cryogenic preservation in this animated series.")
+video2 = Video.create(title: "Family Guy", description: "The show follows the adventures of an endearingly ignorant dad, PETER GRIFFIN (Seth MacFarlane), and his hilariously odd family of middle-class New Englanders.")
+video3 = Video.create(title: "Monk", description: "Monk is an American comedy-drama detective mystery television series created by Andy Breckman and starring Tony Shalhoub as the eponymous character.")
+video4 = 
