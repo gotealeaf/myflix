c19741203f63eae917275cf23b2f6b2f3529a252
Sue Anne Teo <sueanneteo@gmail.com>
2014-04-16 08:56:41 +0000

    Tidied up video controller

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,22 +1,7 @@
 class VideosController < ApplicationController
-  before_action :video_params, only: [:create, :edit, :update]
     
   def index
     @videos = Video.all
-  end
-  
-  def new
-    @video = Video.new
-  end
-  
-  def create
-    @video = Video.new(video_params)
-  end
-  
-  def edit
-  end
-  
-  def update
   end
   
   def show
