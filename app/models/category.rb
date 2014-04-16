67df0e3828bf9f4991296c4dd332e606b6e8ffe7
Sue Anne Teo <sueanneteo@gmail.com>
2014-04-16 08:44:21 +0000

    added categories show page

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,7 @@
 class Category < ActiveRecord::Base
-  has_many :videos
+  has_many :video_categories
+  has_many :videos, through: :video_categories
+  
+  validates_presence_of :name
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Myflix::Application.routes.draw do
   get 'ui(/:action)', controller: 'ui'
   
-  resources :videos, only: [:index, :show]
   get '/home', to: 'videos#index'
+  
+  resources :videos, only: [:index, :show]
+  resources :categories, only: [:show]
+
 
 end
