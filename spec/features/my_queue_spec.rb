62becae2b60baf6ece444da28a3acd3dbca4aa2d
Charles Quirin <charles@cr-q.com>
2014-04-16 23:22:04 -0400

    User reorders videos feature test.

--- spec/features/my_queue_spec.rb
+++ spec/features/my_queue_spec.rb
@@ -20,7 +20,51 @@ feature 'my queue' do
     expect(page).to have_content 'List Order'
     expect(page).to have_content video.title
     click_link video.title
+    expect(page).to have_content video.title
     expect(page).to have_content 'Watch Now'
     expect(page).to_not have_content '+ My Queue'
+  end
+
+  scenario "user reorders videos in queue" do
+    video_2 = Fabricate(:video)
+    video_3 = Fabricate(:video)
+    visit_and_add_to_queue(video)
+    visit_and_add_to_queue(video_2)
+    visit_and_add_to_queue(video_3)
+    
+    within(:xpath, ".//tbody//tr[1]") do
+      expect(page).to have_content video.title
+      expect(page).to have_xpath(".//input[@value='#{QueueItem.where(user: user, video: video).first.position}']")
+      fill_in( 'queue_items__position', with: 2 )
+    end
+
+    within(:xpath, ".//tbody//tr[2]") do
+      expect(page).to have_content video_2.title
+      expect(page).to have_xpath(".//input[@value='#{QueueItem.where(user: user, video: video_2).first.position}']")
+      fill_in( 'queue_items__position', with: 3 )
+    end
+
+    within(:xpath, ".//tbody//tr[3]") do
+      expect(page).to have_content video_3.title
+      expect(page).to have_xpath(".//input[@value='#{QueueItem.where(user: user, video: video_3).first.position}']")
+      fill_in( 'queue_items__position', with: 1 )
+    end
+
+    click_button 'Update Instant Queue'
+
+    within(:xpath, ".//tbody//tr[1]") do
+      expect(page).to have_content video_3.title
+      expect(page).to have_xpath(".//input[@value='#{QueueItem.where(user: user, video: video_3).first.position}']")
+    end
+
+    within(:xpath, ".//tbody//tr[2]") do
+      expect(page).to have_content video.title
+      expect(page).to have_xpath(".//input[@value='#{QueueItem.where(user: user, video: video).first.position}']")
+    end
+
+    within(:xpath, ".//tbody//tr[3]") do
+      expect(page).to have_content video_2.title
+      expect(page).to have_xpath(".//input[@value='#{QueueItem.where(user: user, video: video_2).first.position}']")
+    end 
   end
 end
--- spec/support/macros.rb
+++ spec/support/macros.rb
@@ -10,3 +10,9 @@ end
 def clear_current_user
   session[:user_id] = nil
 end
+
+def visit_and_add_to_queue(video)
+  visit home_path
+  find(:xpath, ".//a[@href='/videos/#{video.id}']" ).click
+  click_link '+ My Queue'
+end
