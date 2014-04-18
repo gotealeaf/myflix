f0097e35782ae4555e6cbfa15ea3ac5baea5f37a
Sue Anne Teo <sueanneteo@gmail.com>
2014-04-18 07:59:52 +0000

    added search by title to video model and its tests

--- app/models/video.rb
+++ app/models/video.rb
@@ -4,4 +4,9 @@ class Video < ActiveRecord::Base
   
   validates_presence_of :title, :description
   
+  def self.search_by_title(title)
+    return [] if title.blank?
+    where("title LIKE ?", "%#{title}%").order("created_at DESC") 
+  end
+  
 end
