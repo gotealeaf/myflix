fedf1195003c6312b050b7ed64563e45007db815
mattdvhope <mattagape@me.com>
2014-04-19 15:30:01 +0700

    Created test for video model

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -4,16 +4,16 @@ describe Video do
   it "saves itself" do
     video = Video.new(title: "Road Runner", description: "Chased constantly!", small_cover_url: "/tmp/road_runner.jpg", large_cover_url: "/tmp/road_runner_large.jpg", category_id: 1)
     video.save
-    Video.first.title.should           == "Road Runner"
-    Video.first.description.should     == "Chased constantly!"
-    Video.first.small_cover_url.should == "/tmp/road_runner.jpg"
-    Video.first.large_cover_url.should == "/tmp/road_runner_large.jpg"
-    Video.first.category_id.should     == 1
+    expect(Video.first).to eq(video)
+    # Video.first.should == video
+    # Video.first.should eq(video)
   end
+
+  it "belongs to category" do
+    chases = Category.create(name: "Chase movies")
+    video = Video.create(title: "Road Runner", description: "Chased constantly!", category: chases)
+    expect(video.category).to eq(chases)
+  end
+
 end
 
-# Deprecation Warnings:
-# Using `should` from rspec-expectations' old `:should` syntax without explicitly enabling the syntax is deprecated. Use the new `:expect` syntax or explicitly enable `:should` instead. Called from /Users/matthewmalone/Sites/myflix/spec/models/video_spec.rb:7:in `block (2 levels) in <top (required)>'.
-# If you need more of the backtrace for any of these deprecations to identify where to make the necessary changes, you can configure `config.raise_errors_for_deprecations!`, and it will turn the deprecation warnings into errors, giving you the full backtrace.
-# 1 deprecation warning total
-# Randomized with seed 35627
