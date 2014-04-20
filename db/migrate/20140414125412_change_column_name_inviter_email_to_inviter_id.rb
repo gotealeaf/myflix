1c9651f9537f4df8c2013b9f6758e46358ec181b
Rob Jewell <jewell.robertp@gmail.com>
2014-04-20 16:31:53 -0400

    removes two migration files

--- db/migrate/20140414125412_change_column_name_inviter_email_to_inviter_id.rb
+++
@@ -1,6 +0,0 @@
-class ChangeColumnNameInviterEmailToInviterId < ActiveRecord::Migration
-  def change
-    rename_column :invitations, :inviter_email, :inviter_id
-    change_column :invitations, :inviter_id, :integer
-  end
-end
--- db/migrate/20140418194407_drop_column_inviter_id.rb
+++
@@ -1,5 +0,0 @@
-class DropColumnInviterId < ActiveRecord::Migration
-  def change
-    remove_column :invitations, :inviter_id
-  end
-end
