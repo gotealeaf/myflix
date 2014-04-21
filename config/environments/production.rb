2a28db6ba1f8d9804b682153b1ff99b0cb1d454f
Aleksey Chaikovsky <aleksey@dot.net>
2014-04-21 01:39:01 +0000

    Added action mailer settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,10 +21,12 @@ Myflix::Application.configure do
   config.action_mailer.default_url_options = { host: 'aleksey-myflix.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.com',
-    port: 587,
-    user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"],
-    authentication: 'plain',
-    enable_starttls_auto: true }
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'aleksey-myflix.herokuapp.com',
+    :authentication => :plain
+  }
+  
 end
