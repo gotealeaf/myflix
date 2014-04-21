57e15a075ac4fe6ed9f204de25056f5c7603785a
Aleksey Chaikovsky <aleksey@dot.net>
2014-04-21 01:06:50 +0000

    Introduced some modules, fixed form bugs

--- spec/support/shared_examples.rb
+++ spec/support/shared_examples.rb
@@ -5,3 +5,9 @@ shared_examples "require sign in" do
     expect(response).to redirect_to sign_in_path
   end
 end
+
+shared_examples "tokenable" do
+  it "generates a random token when the user is created" do
+    expect(object.token).to be_present
+  end
+end
