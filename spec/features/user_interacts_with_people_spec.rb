90841911d0785f9efc186109758e34709264fafa
John Morgan <jtmorgan61@gmail.com>
2014-04-21 14:40:12 -0700

    minor refactor to feature spec from solution

--- spec/features/user_interacts_with_people_spec.rb
+++ spec/features/user_interacts_with_people_spec.rb
@@ -1,34 +1,32 @@
 require 'spec_helper'
 
-feature "user interacts with the people page" do 
+feature "User following" do 
   scenario "user follows a person and then unfollows them" do
     alice = Fabricate(:user)
     bob = Fabricate(:user)
-    category1 = Fabricate(:category)
-    video = Fabricate(:video, title: "Video 1", category_id: category1.id)
+    category = Fabricate(:category)
+    video = Fabricate(:video, title: "Video 1", category_id: category.id)
     review = Fabricate(:review, user_id: bob.id, video_id: video.id)
 
     sign_in_user(alice)
-    visit_video_page(video)
-    click_user_name(bob)
+    click_on_video(video)
+    click_on_user_name(bob)
     expect_user_to_be_followable(bob)
     
     follow_user(bob)
     expect_user_to_be_followed(bob)
     
-    click_user_name(bob)
+    click_on_user_name(bob)
     expect_user_not_to_be_followable(bob)
 
     visit people_path
-    delete_leader(bob)
+    unfollow_leader(bob)
     expect_user_not_to_be_followed(bob)
     
-    visit_video_page(video)
-    click_user_name(bob)
+    visit home_path
+    click_on_video(video)
+    click_on_user_name(bob)
     expect_user_to_be_followable(bob)
-    #save_and_open_page
-    #find(:xpath, "//tr[contains(.,'#{bob.full_name}')]//td//a[data-method='delete']").click
-    #find("#delete_1").click
   end
 end
 
@@ -44,8 +42,7 @@ def follow_user(user)
   click_link "Follow"
 end
 
-#user not relevant yet... waiting for xpath refactor from solution
-def delete_leader(user)
+def unfollow_leader(user)
   find("a[data-method='delete']").click
 end
 
@@ -59,11 +56,10 @@ def expect_user_not_to_be_followed(user)
   expect(page).not_to have_content("#{user.full_name}")
 end
 
-def visit_video_page(video)
-  visit home_path
+def click_on_video(video)
   find("a[href='/videos/#{video.id}']").click
 end
 
-def click_user_name(user)
+def click_on_user_name(user)
   click_link "#{user.full_name}"
 end
