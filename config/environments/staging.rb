a364655d78372d17db7b1a43e89ae3b13c677960
Rob Jewell <jewell.robertp@gmail.com>
2014-04-22 14:00:20 -0400

    tweaks staging.rb file to fix css problem in staging environment

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -5,7 +5,7 @@ Myflix::Application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   config.assets.compress = true
   config.assets.js_compressor = :uglifier
