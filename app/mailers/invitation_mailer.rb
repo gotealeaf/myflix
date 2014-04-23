8ebbc3fe03f313b923d3b902b7534412346ca539
Matthew Barram <matthewbarram@gmail.com>
2014-04-23 20:49:57 +1000

    Added an env variable to only send real emails during production

--- app/mailers/invitation_mailer.rb
+++ app/mailers/invitation_mailer.rb
@@ -3,6 +3,10 @@ class InvitationMailer < ActionMailer::Base
 
   def invite_user_email(invitee_details)
     @invitee_details = invitee_details
-    mail(to: invitee_details.email, subject: "You have been invited to sign up for Myflix.")
+    if Rails.env.production?
+      mail(to: "invitee_details.email", subject: "You have been invited to sign up for Myflix.")
+    else
+      mail(to: "matthewbarram@gmail.com", subject: "You have been invited to sign up for Myflix.")
+    end
   end
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,11 +3,19 @@ class UserMailer < ActionMailer::Base
 
   def welcome_email(user)
     @user = user
-    mail(to: user.email, subject: "Thank you for signing up for MyFlix.")
+    if Rails.env.production?
+      mail(to: user.email, subject: "Thank you for signing up for MyFlix.")
+    else
+      mail(to: "matthewbarram@gmail.com", subject: "Thank you for signing up for MyFlix.")
+    end
   end
 
   def password_reset_email(user)
     @user = user
-    mail(to: user.email, subject: "Password Reset for MyFlix.")
+    if Rails.env.production?
+      mail(to: user.email, subject: "Password Reset for MyFlix.")
+    else
+      mail(to: "matthewbarram@gmail.com", subject: "Password Reset for MyFlix.")
+    end
   end
 end
