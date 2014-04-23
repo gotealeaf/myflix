ecc6020f37443322753d4f3228629fbbac98c2d9
John Morgan <jtmorgan61@gmail.com>
2014-04-23 16:01:44 -0700

    Workaround for Heroku to migrate past a table I later dropped

---
+++ app/models/create_token_column_for_user.rb
@@ -0,0 +1,5 @@
+class CreateTokenColumnForUser
+  # empty file needed to make Heroku play nice with a migration where I 
+  # mistakenly created this table
+  # workarounds to avoid this without deleting the migrations are most welcome!
+end
