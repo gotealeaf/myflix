0f4b68addd5063441102da51385d1f18c453cb39
John Morgan <jtmorgan61@gmail.com>
2014-04-23 16:11:07 -0700

    one more db fix

--- db/migrate/20140422231522_generate_tokens_for_existing_users.rb
+++ db/migrate/20140422231522_generate_tokens_for_existing_users.rb
@@ -1,5 +1,6 @@
 class GenerateTokensForExistingUsers < ActiveRecord::Migration
   def change
+    add_column :users, :token, :string
     User.all.each do |user|
       user.update_column(:token, SecureRandom.urlsafe_base64)
     end
