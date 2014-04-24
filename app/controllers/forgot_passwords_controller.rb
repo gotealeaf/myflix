9f8c5cdb8b5a45606a7fb1298d9efefc5134599f
John Morgan <jtmorgan61@gmail.com>
2014-04-24 11:30:00 -0700

    feature spec for user login via email (pre-solution)

--- app/controllers/forgot_passwords_controller.rb
+++ app/controllers/forgot_passwords_controller.rb
@@ -1,16 +1,12 @@
 class ForgotPasswordsController < ApplicationController
   def create
-    # user = User.find_by(email: params[:email])
-    # if user
-    #   AppMailer.forgot_password_email(user).deliver
-    #   redirect_to forgot_password_confirmation_path
-    # else
-    #   flash[:danger] = params[:email].blank? ? "Email cannot be blank." : "Sorry, that email does not exist in our records."
-    #   redirect_to forgot_password_path
-    # end
-
-    # ## I am using this to brute force the actual controller (above)
-    # ## So that I can be 100% sure it's not a submission error
-    redirect_to forgot_password_confirmation_path
+    user = User.find_by(email: params[:email])
+    if user
+      AppMailer.forgot_password_email(user).deliver
+      redirect_to forgot_password_confirmation_path
+    else
+      flash[:danger] = params[:email].blank? ? "Email cannot be blank." : "Sorry, that email does not exist in our records."
+      redirect_to forgot_password_path
+    end
   end
 end
--- spec/features/user_resets_password_spec.rb
+++ spec/features/user_resets_password_spec.rb
@@ -1,38 +1,44 @@
 require 'spec_helper'
 
 feature "User resets password" do
-  scenario "user forgets password and resets it via email", :js => true do 
-    alice = Fabricate(:user, password: 'old_password', email: 'test@example.com')
-
-    visit login_path
-    click_link "Forgot Password"
-    expect(page).to have_content("Forgot Your Password?")
-
-    ## Either of these works in Selenium. Neither works in pure Capybara.
-    click_button "Send Email"
-    # find(".btn.btn-default").click
-
-    ## Note that I disabled the if/else in the controller, so input doesn't 
-    ## matter. All it has to do is click the button to get here!
-    expect(page).to have_content("We have sent an email with instructions to reset your password.")
-
-    #########################################
-    # Code I will reactivate later below here
-    #########################################
-
-    ## Filling in the field
-    # expect(page).to have_field("Email Address")
-    # expect(find_field('email').value).to eq("")
-    # fill_in "Email Address", with: 'test@example.com'
-    # find('#email').set(alice.email)
-    # expect(find_field('Email Address').value).to eq('test@example.com')
-    # expect(find_field('email').value).to eq('test@example.com')
-
-    ## If it actually worked...
-    # open_email('test@example.com')
-    # current_email.save_and_open
-    # expect(page).to have_content("Click on this link to reset it:")
-    # current_email.click_link("Reset Password")
-    # expect(page).to have_content("Reset Your Password")
+  scenario "user forgets password and resets it via email" do 
+    alice = Fabricate(:user, password: 'old_password')
+    visit_forgot_password_page
+    submit_email(alice.email)
+    open_email(alice.email)
+    visit_reset_password_page_via_link_in_email(current_email)
+    submit_new_password('new_password')
+    log_in_user_with_new_password(alice, 'new_password')
   end
+end
+
+def visit_forgot_password_page
+  visit login_path
+  click_link "Forgot Password"
+  expect(page).to have_content("Forgot Your Password?")
+end
+
+def submit_email(email)
+  find('#email').set(email)
+  click_button "Send Email"
+  expect(page).to have_content("We have sent an email with instructions to reset your password.")
+end
+
+def visit_reset_password_page_via_link_in_email(current_email)
+  expect(current_email).to have_content("Click on this link to reset it:")
+  current_email.click_link("Reset Password")
+  expect(page).to have_content("Reset Your Password")
+end
+
+def submit_new_password(password)
+  fill_in "New Password", with: password
+  click_button "Reset Password"
+  expect(page).to have_content("You reset your password. Please log in.")
+end
+
+def log_in_user_with_new_password(user, password)
+  fill_in "Email Address", with: user.email
+  fill_in "Password", with: 'new_password'
+  click_button "Sign in"
+  expect(page).to have_content("Welcome, #{user.full_name}!")
 end
