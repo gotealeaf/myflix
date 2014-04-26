e2bf05aa12e8525a05687a5bf23aac5484ab264a
unai-trecet <unai.trecet@gmail.com>
2014-04-26 18:44:12 +0200

    Social interaction feature test added.

--- spec/features/user_social_interaction.rb
+++ spec/features/user_social_interaction.rb
@@ -2,6 +2,29 @@ require 'spec_helper'
 
 feature "User interacts with the another people" do
   scenario "user follows another user and delete their relationship" do
-    
+    mafia = Fabricate :category
+    the_wire = Fabricate :video, title: "The Wire", category: mafia
+    ana = Fabricate :user
+    review = Fabricate :review, creator: ana, video: the_wire
+
+    sign_in
+
+    click_on_video_on_home_page the_wire
+
+    page.should have_content the_wire.title
+    click_link ana.full_name
+
+    page.should have_content "#{ana.full_name}'s video collections"
+    click_link "Follow"
+
+    page.should have_content "People I Follow"
+    page.should have_content ana.full_name
+
+    unfollow ana
+    page.should_not have_content ana.full_name
   end
+end
+
+def unfollow user
+  find("a[data-method='delete']").click
 end
