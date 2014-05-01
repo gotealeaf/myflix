21483605700155d85e066cf63bf643d1aeb32918
Charles Quirin <charles@cr-q.com>
2014-04-30 20:04:02 -0400

    Routes cleanup

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,19 @@ Myflix::Application.routes.draw do
   get 'ui(/:action)', controller: 'ui'
   root to: 'pages#front'
 
-  get 'home', to: 'videos#index'
+  resources :users, only: [:create, :new, :show] do
+    post 'follow', to: 'users#follow'
+  end
   get 'register', to: 'users#new'
-  get 'sign_in', to: 'sessions#new'
-  get 'sign_out', to: 'sessions#destroy'
-  get 'my_queue', to: 'queue_items#index'
-  patch 'update_queue', to: 'queue_items#update_queue'
-  get 'people', to: 'user_relationships#index'
   get 'forgot_password', to: 'users#forgot_password'
   post 'reset_email', to: 'users#reset_email'
+
+  resources :sessions, only: [:create]
+  get 'sign_in', to: 'sessions#new'
+  get 'sign_out', to: 'sessions#destroy'
+
+  resources :user_relationships, only: [:create, :destroy]
+  get 'people', to: 'user_relationships#index'
 
   resources :videos, only: [:show] do
     collection do
@@ -18,13 +22,11 @@ Myflix::Application.routes.draw do
     end
     resources :reviews, only: [:create]
   end
+  get 'home', to: 'videos#index'
 
-  resources :users, only: [:create, :new, :show] do
-    post 'follow', to: 'users#follow'
-  end
-
-  resources :user_relationships, only: [:create, :destroy]
   resources :queue_items, only: [:create, :destroy]
+  get 'my_queue', to: 'queue_items#index'
+  patch 'update_queue', to: 'queue_items#update_queue'
+
   resources :categories, only: [:show]  
-  resources :sessions, only: [:create]
 end
