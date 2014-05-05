81db3ec27a4c543cb907964745515d3f283475a9
unai-trecet <unai.trecet@gmail.com>
2014-05-05 19:59:00 +0200

    Changing timestamps to timestamp in categories alter migration.

--- db/migrate/20140421173138_alter_columns_created_at_video_and_categories.rb
+++ db/migrate/20140421173138_alter_columns_created_at_video_and_categories.rb
@@ -1,6 +1,6 @@
 class AlterColumnsCreatedAtVideoAndCategories < ActiveRecord::Migration
   def change
-    change_column :categories, :created_at, :timestamps
-    change_column :videos, :created_at, :timestamps
+    change_column :categories, :created_at, :timestamp
+    change_column :videos, :created_at, :timestamp
   end
 end
