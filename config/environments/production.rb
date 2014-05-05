73d21e0c9df9b3c3a1c9bfb57ac723b911f5101b
unai-trecet <unai.trecet@gmail.com>
2014-05-05 20:22:31 +0200

    Producction enviroment email conf added.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,4 +17,14 @@ Myflix::Application.configure do
   config.i18n.fallbacks = true
 
   config.active_support.deprecation = :notify
+
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'example.com',
+    user_name:            ENV["GMAIL_USERNAME"],
+    password:             ENV["GMAIL_PASSWORD"],
+    authentication:       'plain',
+    enable_starttls_auto: true  
+  }
 end
