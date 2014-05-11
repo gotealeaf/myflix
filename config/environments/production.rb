de35affffa719d385e1d776f57198a9701bc6124
Charles Quirin <charles@cr-q.com>
2014-05-10 23:08:34 -0400

    Production configuration changes for mailgun.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,13 +21,13 @@ Myflix::Application.configure do
   config.action_mailer.delivery_method = :smtp
   
   config.action_mailer.smtp_settings = {
-    address:                'smtp.gmail.com',
-    port:                   587,
-    domain:                 'gmail.com',
-    user_name:              ENV['gmail_username'],
-    password:               ENV['gmail_password'],
-    authentication:         'plain',
-    enable_starttls_auto:   true }
+    address:                ENV['MAILGUN_SMTP_SERVER'],
+    port:                   ENV['MAILGUN_SMTP_PORT'],
+    domain:                 'myflix-crq.heroku.com',
+    user_name:              ENV['MAILGUN_SMTP_LOGIN'],
+    password:               ENV['MAILGUN_SMTP_PASSWORD'],
+    authentication:         :plain,
+  }
 
   config.action_mailer.default_url_options = { host: 'myflix-crq.heroku.com' }
 end
