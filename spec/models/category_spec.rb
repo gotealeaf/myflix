23052ac4c222479536cc96d75ea359575152c1f1
Mark Johnson <mjohnson@mellowcoder.com>
2014-05-14 16:26:00 -0400

    Replaced existing tests with shoulda-matchers

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,18 +1,7 @@
 require 'spec_helper'
 
 describe Category do
-  it "saves itself" do
-    category = Category.new(name: 'Drama')
-    category.save
-    expect(Category.first).to eq(category)
-  end
-  
-  it "can have many videos" do
-    drama = Category.new(name: 'Drama')
-    monk = Video.create(title: 'Monk', description: 'He is not realy a monk', category: drama)
-    cheers = Video.create(title: 'Cheers', description: 'There was drama in this sometimes', category: drama)
-    expect(drama.videos).to eq([cheers, monk])
-  end
+  it {should have_many(:videos)}
   
 end
   
