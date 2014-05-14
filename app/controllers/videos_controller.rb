cf586f07084c2a25bd1d9f62405b3fccdbfacae3
Mark Johnson <mjohnson@mellowcoder.com>
2014-05-13 20:48:43 -0400

    Added Controller and views to display videos

---
+++ app/controllers/videos_controller.rb
@@ -0,0 +1,22 @@
+class VideosController < ApplicationController
+  before_action :set_video, only: [:show, :edit, :update, :destroy]
+  
+  def index
+    @videos = Video.all
+  end
+  
+  def show
+  end
+  
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_video
+      @video = Video.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def video_params
+      params.require(:video).permit(:title, :description, :small_cover_url, :large_cover_url)
+    end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Myflix::Application.routes.draw do
   get 'ui(/:action)', controller: 'ui'
+  
+  resources :videos
+  
+  get 'home', to: 'videos#index'
 end
