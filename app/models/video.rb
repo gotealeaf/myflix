024661f6470b756558a270fb7b2adad0a3dd4bb7
Mark Johnson <mjohnson@mellowcoder.com>
2014-05-14 18:49:59 -0400

    Added search by title to Video model

--- app/models/video.rb
+++ app/models/video.rb
@@ -2,4 +2,9 @@ class Video < ActiveRecord::Base
   belongs_to :category
   
   validates_presence_of :title, :description
+  
+  def self.search_by_title(title)
+    Video.where('title like ?', "%#{title}%").order(created_at: :desc)
+  end
+  
 end
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -6,5 +6,29 @@ describe Video do
   it {should validate_presence_of(:title)}
   it {should validate_presence_of(:description)}
   
+  describe "search_by_title" do
+    it "returns an empty array if there are is no match" do
+      video = Video.create(title: 'The Family Guy', description: 'A comedy about a family and their dog.')
+      expect(Video.search_by_title('Monk')).to eq([])
+    end
+    
+    it "returns an array of a single item if there is an exact match on one record" do
+      video = Video.create(title: 'The Family Guy', description: 'A comedy about a family and their dog.')
+      expect(Video.search_by_title('The Family Guy')).to eq([video])
+      
+    end
+ 
+    it "returns an array of a single item if there is an partial match on one record" do
+      video = Video.create(title: 'The Family Guy', description: 'A comedy about a family and their dog.')
+      expect(Video.search_by_title('Family')).to eq([video])
+    end
+
+    it "returns an array of a multiple items ordered by created_at if there is a match on multiple records" do
+      video = Video.create(title: 'The Family Guy', description: 'A comedy about a family and their dog.', created_at: 1.day.ago)
+      video2 = Video.create(title: 'The Family Guy', description: 'A comedy about a family and their dog.')
+      expect(Video.search_by_title('Family')).to eq([video2, video])
+    end
+  end
+  
 end
   
