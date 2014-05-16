d821ef94ebab2fb0a8388613ce51e8d1bd059366
Sue Anne Teo <sueanneteo@gmail.com>
2014-05-16 12:04:31 +0000

    Added feature specs for user social networking tools

--- spec/features/user_social_networking_tools_spec.rb
+++ spec/features/user_social_networking_tools_spec.rb
@@ -2,19 +2,38 @@ require 'spec_helper'
 
 feature "user social networking tools" do
   scenario "user following and unfollowing another user" do
+    
     comedy = Category.create(name: "comedy")
     #comedies = Fabricate(:category) # because on the home page, videos are ordered by category
     monk = Fabricate(:video, title: "Monk")
     monk.categories << comedy
     
     jane = Fabricate(:user)
+    bob = Fabricate(:user)
+    review = Fabricate(:review, video: monk, user: bob)
+    
     sign_in_user(jane)
+    go_to_video_page(monk)
+    find_user_to_follow(bob)  
+    unfollow_user(bob) 
     
+  end
+  
+  def go_to_video_page(video)
     visit home_path
-    find("a[href='/videos/#{monk.id}']").click # find the video by ID position
-    find #to be continued!!
-    click_link "+ My Queue"
-    
-    
+    find("a[href='/videos/#{video.id}']").click # find the video by ID position
+    page.should have_content(video.title)
+  end
+
+  def find_user_to_follow(user)
+    click_link user.full_name
+    page.should have_content("video collections")
+    click_link "Follow"
+    page.should have_content(user.full_name)
+  end
+
+  def unfollow_user(user)
+    find("a[data-method='delete']").click #to find the exact delete link
+    page.should_not have_content(user.full_name)
   end
 end
