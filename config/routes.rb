b95f492271185424d59e5610a34bdfb508d9156c
Reed Nelson-Saunders <reedns@hotmail.com>
2014-05-16 14:03:33 -0700

    added recent_video method with specs to category model and implemented it in  the video index template

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Myflix::Application.routes.draw do
   get 'ui(/:action)', controller: 'ui'
 
   root to: "videos#index"
+  get "home", to: "videos#index"
 
   resources :videos, only: [:index, :show] do
     collection do
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -2,4 +2,44 @@ require "spec_helper"
 
 describe Category do
   it { should have_many(:videos) }
+
+  describe "#recent_videos" do
+    it "returns videos in reverse chronological order" do |variable|
+      category = Category.create(name: "Dramas", id: 1)
+      rudy = Video.create(title: "Rudy", description: "Heartwarming drama", category: category, created_at: 1.day.ago)
+      lost = Video.create(title: "Lost", description: "Mystery drama", category: category, created_at: 2.days.ago)
+      expect(category.recent_videos).to eq([rudy, lost])
+    end
+
+    it "returns all videos if there are less than 6" do
+      category = Category.create(name: "Dramas", id: 1)
+      rudy = Video.create(title: "Rudy", description: "Heartwarming drama", category: category)
+      lost = Video.create(title: "Lost", description: "Mystery drama", category: category)
+      bsg = Video.create(title: "Battle Star Galactica", description: "Sci-Fi drama", category: category)
+      expect(category.recent_videos.count).to eq(31)
+    end
+
+    it "returns only 6 videos if there are more than 6" do
+      category = Category.create(name: "Dramas", id: 1)
+      7.times { Video.create(title: "Rudy", description: "Heartwarming drama", category: category) }
+      expect(category.recent_videos.count).to eq(6)
+    end
+
+    it "returns 6 videos ordered by most recent" do
+      category = Category.create(name: "Dramas", id: 1)
+      rudy = Video.create(title: "Rudy", description: "Heartwarming drama", category: category, created_at: 4.day.ago)
+      lost = Video.create(title: "Lost", description: "Mystery drama", category: category, created_at: 5.days.ago)
+      bsg = Video.create(title: "Battle Star Galactica", description: "Sci-Fi drama", category: category, created_at: 6.days.ago)
+      rudy2 = Video.create(title: "Rudy", description: "Heartwarming drama", category: category, created_at: 1.days.ago)
+      lost2 = Video.create(title: "Lost", description: "Mystery drama", category: category, created_at: 2.days.ago)
+      bsg2 = Video.create(title: "Battle Star Galactica", description: "Sci-Fi drama", category: category, created_at: 3.days.ago)
+      rudy3 = Video.create(title: "Rudy", description: "Heartwarming drama", category: category, created_at: 7.days.ago)
+      expect(category.recent_videos).to eq([rudy2, lost2, bsg2, rudy, lost, bsg])
+    end
+
+    it "returns an empty array if there are no videos" do
+      category = Category.create(name: "Dramas", id: 1)
+      expect(category.recent_videos).to eq([])
+    end
+  end
 end
