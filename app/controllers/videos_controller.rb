663c04ffae8a9f8cb0533015b8f71c57e45dcf04
Quinn Avila <hkdnow@yahoo.com>
2014-05-19 13:49:00 -0500

    video and categories

---
+++ app/controllers/videos_controller.rb
@@ -0,0 +1,8 @@
+class VideosController < ApplicationController
+
+  def index
+    @videos = Video.all
+
+  end
+
+end
---
+++ app/models/video.rb
@@ -0,0 +1,4 @@
+class Video < ActiveRecord::Base
+  belongs_to :category
+
+end
