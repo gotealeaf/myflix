c1b90578261fb4c8baf4608feaed0a7cdeb56ddb
Quinn Avila <hkdnow@yahoo.com>
2014-05-19 14:59:02 -0500

    categoryid in video

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140519181338) do
+ActiveRecord::Schema.define(version: 20140519190153) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20140519181338) do
     t.string   "large_cover_url"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "category_id"
   end
 
 end
