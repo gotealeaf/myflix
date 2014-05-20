c9cfcd49e5f1fef1f165610cbc6c4bdf4209b8c8
AntonKolmakov <konfonx@gmail.com>
2014-05-21 01:08:23 +0600

    Use shoulda_matchers

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,16 +1,5 @@
 require 'spec_helper'
 
 describe Category do
-  it "saves itself" do
-    category = Category.new(name: "Comedy")
-    category.save
-    expect(Category.first).to eq(category)
-  end
-
-  it "has many videos" do
-    comedy = Category.create(name: "Comedy")
-    futurama = Video.create(title: "Futurama", description: "Space travel!", category: comedy)
-    south_park = Video.create(title: "South Park", description: "Funny video!", category: comedy)
-    expect(comedy.videos).to include(south_park, futurama)   
-  end
+  it { should have_many(:videos) }
 end
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -1,23 +1,7 @@
 require 'spec_helper'
 
 describe Video do
-  it "saves itself" do
-    video = Video.create(title: "Futurama", description: "Pizza boy Philip J. Fry...")
-    video.save
-    expect(Video.first).to eq(video)
-  end
-
-  it "belongs to category" do
-    action = Category.create(name: "Action")
-    monk = Video.create(title: "Monk", description: "a great video!", category_id: action.id)
-    expect(monk.category).to eq(action)
-  end
-
-  it"is invalid without a title"do
-    expect(Video.new(title: nil, description: "a great video!")).to have(1).errors_on(:title)
-  end
-
-  it"is invalid without a description"do
-    expect(Video.new(description: nil, title: "Monk")).to have(1).errors_on(:description)
-  end
+  it { should belong_to(:category) }
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:description) }
 end
