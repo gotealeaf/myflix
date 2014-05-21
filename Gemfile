a3feeb5af0ab460df09978991e88cdc21d8f7ad8
Quinn Avila <hkdnow@yahoo.com>
2014-05-20 20:58:41 -0500

    working

--- Gemfile
+++ Gemfile
@@ -16,6 +16,7 @@ group :development do
   gem 'thin'
   gem "better_errors"
   gem "binding_of_caller"
+  gem 'rspec-rails'
 end
 
 group :production do
--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -6,6 +6,6 @@ class CategoriesController < ApplicationController
 
   def show
     @category = Category.find(params[:id])
-    #@categories = Category.all
+    
   end
 end
