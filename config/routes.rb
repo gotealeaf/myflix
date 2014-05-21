0b4022b7051d63c540656e9ad9c71d35eac05db1
AntonKolmakov <konfonx@gmail.com>
2014-05-21 18:14:48 +0600

    Recent Videos

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Myflix::Application.routes.draw do
   root "ui#index"
+
+  resources :categories, only: [:show]
   resources :videos, only: [:show] do
     collection do
       get :search, to: "videos#search"
     end
   end
-  resources :categories, only: :show
+
   get 'home', to: 'videos#index'
   get 'ui(/:action)', controller: 'ui'
 end
