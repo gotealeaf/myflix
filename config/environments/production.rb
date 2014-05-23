64bb5ebfcf0281fdec0c49e16ab8d0311c9d4f27
Sue Anne Teo <sueanneteo@gmail.com>
2014-05-23 05:10:06 +0000

    Added Mailgun production environment code

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,13 +19,13 @@ Myflix::Application.configure do
   config.active_support.deprecation = :notify
   
   #note: change this to mailgun settings
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-  address:              'smtp.gmail.com',
-  port:                 587,
-  domain:               'example.com',
-  user_name:            '<username>',
-  password:             '<password>',
-  authentication:       'plain',
-  enable_starttls_auto: true  }
+  ActionMailer::Base.smtp_settings = {
+  :port           => ENV['MAILGUN_SMTP_PORT'],
+  :address        => ENV['MAILGUN_SMTP_SERVER'],
+  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+  :domain         => 'yourapp.heroku.com',
+  :authentication => :plain,
+  }
+  ActionMailer::Base.delivery_method = :smtp
 end
