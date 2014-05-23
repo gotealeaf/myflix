5baf06458c7526f5cc1725d787e30aaf7deaf287
Sue Anne Teo <sueanneteo@gmail.com>
2014-05-23 08:30:00 +0000

    Amended production environment code for mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,13 +18,15 @@ Myflix::Application.configure do
 
   config.active_support.deprecation = :notify
   
-  #note: change this to mailgun settings
+  config.action_mailer.default_url_options = { host: 'teoanne-myflix.herokuapp.com' }
+  
+  # mailgun settings
   ActionMailer::Base.smtp_settings = {
   :port           => ENV['MAILGUN_SMTP_PORT'],
   :address        => ENV['MAILGUN_SMTP_SERVER'],
   :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
   :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-  :domain         => 'yourapp.heroku.com',
+    :domain         => 'teoanne-myflix.herokuapp.com',
   :authentication => :plain,
   }
   ActionMailer::Base.delivery_method = :smtp
