5792098678a47ed248a36c0a2e4e0b23a6a57c2d
AntonKolmakov <kolmakoff@icloud.com>
2014-05-25 02:20:03 +0600

    User Sees the Queue

---
+++ spec/models/queue_item_spec.rb
@@ -0,0 +1,48 @@
+require 'spec_helper'
+
+describe QueueItem do
+  it { should belong_to(:user) }
+  it { should belong_to(:video) }
+
+  describe "#video_title" do
+    it "reterned the title of the associeted video" do
+      video = Fabricate(:video, title: "Monk")
+      queue_item = Fabricate(:queue_item, video: video)
+      expect(queue_item.video_title).to eq("Monk")
+    end
+  end
+
+  describe "#rating" do
+    it "returns the rating from review when the review is presents" do
+      video = Fabricate(:video)
+      user = Fabricate(:user)
+      review = Fabricate(:review, user: user, video: video, rating: 5)
+      queue_item = Fabricate(:queue_item, video: video, user: user)
+      expect(queue_item.rating).to eq(5)
+    end
+    it "returns nil when the review is not presents" do
+      video = Fabricate(:video)
+      user = Fabricate(:user)
+      queue_item = Fabricate(:queue_item, video: video, user: user)
+      expect(queue_item.rating).to eq(nil)
+    end
+  end
+
+  describe "#category_name" do
+    it "returns the category's name of the video" do
+      category = Fabricate(:category, name: "Comedy")
+      video = Fabricate(:video, category: category)
+      queue_item = Fabricate(:queue_item, video: video)
+      expect(queue_item.category_name).to eq("Comedy")
+    end
+  end
+
+  describe "#category" do
+    it "returns the category of the video" do
+      category = Fabricate(:category, name: "Comedy")
+      video = Fabricate(:video, category: category)
+      queue_item = Fabricate(:queue_item, video: video)
+      expect(queue_item.category).to eq(category)
+    end
+  end
+end
