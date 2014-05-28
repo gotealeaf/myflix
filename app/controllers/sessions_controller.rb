4b3fc9b306b1ac905dcb7493fc44f2b08a6f4d0c
Quinn Avila <hkdnow@yahoo.com>
2014-05-28 11:58:14 -0500

    working

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,17 +5,19 @@ class SessionsController < ApplicationController
 
   def create
     user = User.where(email: params[:email]).first
-    if user && user.authentication(params[:password])
+    if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to home_path, notice: 'You are signed in, enjoy'
+      flash[:success] = 'You are signed in, enjoy'
+      redirect_to home_path
     else
-      flash[:error] = "Invalid email or password."
+      flash[:danger] = "Invalid email or password."
       redirect_to sign_in_path
     end
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_path, notice: "You are signed out."
+    flash[:danger] = "You are signed out."
+    redirect_to root_path 
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Myflix::Application.routes.draw do
   get 'ui(/:action)', controller: 'ui'  
   get 'register', to: 'users#new'
   get 'sign_in', to: 'sessions#new'
-  get 'sign_out' to: 'sessions#destroy'
+  get 'sign_out', to: 'sessions#destroy'
   
   resources :users, only: [:create]
   resources :sessions, only: [:create]
