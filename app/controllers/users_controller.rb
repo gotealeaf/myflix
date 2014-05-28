c1438cacd9b2e20d4e201a4926470b0904dd0126
Quinn Avila <hkdnow@yahoo.com>
2014-05-28 08:24:23 -0500

    added require for strong params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,17 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(params[:user])
+    @user = User.new(category_params)
     if @user.save
       redirect_to sign_in_path
     else
       render :new
     end
+  end
+
+  private
+
+  def category_params
+    params.require(:user).permit(:full_name, :email, :password)
   end
 end
