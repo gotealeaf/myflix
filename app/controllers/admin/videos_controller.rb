edb78f9305dbf34aa613ff154b0d653fc6089ea6
Sue Anne Teo <sueanneteo@gmail.com>
2014-05-30 11:57:55 +0000

    Added tests for admin add video create action

--- app/controllers/admin/videos_controller.rb
+++ app/controllers/admin/videos_controller.rb
@@ -1,11 +1,24 @@
 class Admin::VideosController < AdminsController
 
-  
   def new
       @video = Video.new
   end
   
   def create
+    @video = Video.new(video_params)
+    if @video.save
+      flash[:success] = "You have successfully added a video."
+      redirect_to new_admin_video_path
+    else
+      flash[:danger] = "There was an error. Your video could not be added."
+      render :new
+    end
+  end
+  
+  private
+  
+  def video_params
+    params.require(:video).permit(:title, :description, :categories)
   end
   
 end
--- spec/controllers/admin/videos_controller_spec.rb
+++ spec/controllers/admin/videos_controller_spec.rb
@@ -10,10 +10,8 @@ describe Admin::VideosController do
       expect(assigns(:video)).to be_new_record
     end
     
-    it "redirects to home path if user is not authorised to access the page" do
-      set_current_user
-      get :new
-      expect(response).to redirect_to home_path
+    it_behaves_like 'require admin' do
+      let(:action) { get :new }
     end
     
     it "displays a flash message" do
@@ -25,5 +23,61 @@ describe Admin::VideosController do
     it_behaves_like "require sign in" do
       let(:action) {get :new}
     end
-  end # ends the GET new test  
+  end # ends the GET new test
+  
+  describe "POST create" do
+    
+    it_behaves_like "require sign in" do
+      let(:action) { post :create }
+    end
+    
+    it_behaves_like "require admin" do
+      let(:action) { post :create }
+    end
+    
+    context "valid input" do
+      before do
+        set_admin
+        category1 = Fabricate(:category, name: "Drama")
+        category2 = Fabricate(:category, name: "Thriller")
+        post :create, video: {description: "Funny as hell!", title: "Monk 2", categories: [category1, category2]}
+      end
+      it 'adds a video' do
+        expect(Video.count).to eq(1)
+      end
+      
+      it 'redirects to the add video path' do
+        expect(response).to redirect_to new_admin_video_path
+      end
+      
+      it 'displays a success flash message' do
+        expect(flash[:success]).not_to be_blank
+      end
+      
+    end #ends valid input context
+    
+    context "invalid input" do
+      before do
+        set_admin
+        post :create, video: {description: "Funny as hell!"}
+      end
+      
+      it 'does not add a video' do
+        expect(Video.count).to eq(0)
+      end
+      
+      it 'displays a flash error message' do
+        expect(flash[:danger]).not_to be_blank
+      end
+      
+      it 'sets the @video instance variable' do
+        expect(assigns(:video)).to be_instance_of(Video)
+      end
+      
+      it 'renders the new template' do
+        expect(response).to render_template :new
+      end
+    end #ends invalid input context
+    
+  end #ends POST create test
 end
