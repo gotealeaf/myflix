b1a90b284ec1e6d2c40a1e040e112a9004868264
Tsubasa Furukawa <tfurukawa824@gmail.com>
2014-06-01 18:06:02 -0700

    Finished precourse: Added Video and Category model and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Myflix::Application.routes.draw do
+  root to: 'videos#index'
+
   get 'ui(/:action)', controller: 'ui'
+  get '/home', to: 'videos#index'
+  get '/genre/:id', to: 'categories#show', as: 'genre'
+
+  resources :videos, only: [:show]
 end
---
+++ db/migrate/20140602001306_create_category.rb
@@ -0,0 +1,8 @@
+class CreateCategory < ActiveRecord::Migration
+  def change
+    create_table :categories do |t|
+      t.string :name
+      t.timestamps
+    end
+  end
+end
