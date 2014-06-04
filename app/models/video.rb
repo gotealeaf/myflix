8cc44e896f35802a3754573b0745ec329d137026
Quinn Avila <hkdnow@yahoo.com>
2014-06-04 15:57:01 -0500

    all passed

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,6 +1,7 @@
 class Video < ActiveRecord::Base
   belongs_to :category
   has_many :reviews, order: "created_at DESC"
+  #has_many :reviews, -> { order: ("created_at DESC") }
   #validates :title, presence: true
   #validates :description, presence: true
   validates_presence_of :title, :description
