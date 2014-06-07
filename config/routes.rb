197bdeb38bc2896e61e99315113df5d51a399280
aj <ajjonez@icloud.com>
2014-06-07 18:36:11 -0500

    Pre-Course Work

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Myflix::Application.routes.draw do
+  root 'videos#index'
+
   get 'ui(/:action)', controller: 'ui'
+  get '/home', to: 'videos#index'
+
+  resources :videos, only: [:show]
+  resources :categories, only: [:show]
 end
