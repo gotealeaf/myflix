586970d27455b7ebb62ba80737ede790a9f5056a
Josh Leeman <joshleeman@gmail.com>
2014-06-08 17:23:42 -0400

    validates category name

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,5 @@
 class Category < ActiveRecord::Base
   has_many :videos, -> { order 'title asc' }
+
+  validates_presence_of :name
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -2,4 +2,5 @@ require 'rails_helper.rb'
 
 describe Category do
   it { should have_many(:videos)}
+  it { should validate_presence_of(:name)}
 end
