0063346535ae7b07898c8bd945ee4602dbf26275
AntonKolmakov <kolmakoff@icloud.com>
2014-06-09 16:56:58 +0600

    feature spec for invite friend

--- spec/support/macros.rb
+++ spec/support/macros.rb
@@ -10,6 +10,10 @@ def sign_in(a_user=nil)
   click_button "Sign in"
 end
 
+def sign_out
+  visit sign_out_path
+end
+
 def click_on_video_on_home_page(video)
   find("a[href='/videos/#{video.id}']").click
 end
