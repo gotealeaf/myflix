12defeb347d743f4917b96ffa106f5779923bd8a
AntonKolmakov <kolmakoff@icloud.com>
2014-06-10 13:53:30 +0600

    Configured for deployment to Heroku

--- config/application.rb
+++ config/application.rb
@@ -4,17 +4,24 @@ require 'rails/all'
 
 Bundler.require(:default, Rails.env)
 
+config = YAML.load(File.read(File.expand_path('../application.yml', __FILE__)))
+config.merge! config.fetch(Rails.env, {})
+config.each do |key, value|
+  ENV[key] = value unless value.kind_of? Hash
+end
+
 module Myflix
   class Application < Rails::Application
     config.encoding = "utf-8"
     config.filter_parameters += [:password]
     config.active_support.escape_html_entities_in_json = true
+    config.autoload_paths << "#{Rails.root}/lib"
+    config.action_mailer.default_url_options = { host: ENV['MAILER_HOST'] }
 
     config.assets.enabled = true
     config.generators do |g|
       g.orm :active_record
       g.template_engine :haml
     end
-    config.autoload_paths << "#{Rails.root}/lib"
   end
 end
