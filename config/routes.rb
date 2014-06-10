630bad7d60057eb99a25e75ff92b6c05f3069e39
Josh Leeman <joshleeman@gmail.com>
2014-06-09 23:46:41 -0400

    nav search results working

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Myflix::Application.routes.draw do
   root to: 'videos#index'
-
   get 'ui(/:action)', controller: 'ui'
-
   resources :categories, only: [:show]
-  resources :videos, only: [:index, :show]
+  resources :videos, only: [:index, :show] do
+    collection do
+      get :search, to: "videos#search"
+    end
+  end
+
 end
