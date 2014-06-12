e1d28d747e62111578dda9c043ec5befaf1a4540
Sue Anne Teo <sueanneteo@gmail.com>
2014-06-12 06:41:35 +0000

    Finished admin payments in controller and view

---
+++ app/controllers/admin/payments_controller.rb
@@ -0,0 +1,6 @@
+class Admin::PaymentsController < AdminsController
+  
+  def index
+    @payments = Payment.all
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,7 @@ Myflix::Application.routes.draw do
   
   namespace :admin do
     resources :videos, only: [:new, :create]
+    resources :payments, only: [:index]
   end
   
   resources :users, except: [:delete]
