d2d1825e707f0e07fc50cc93de585740f83cfe06
Cullen Jett <cullenjett@gmail.com>
2014-06-13 12:04:16 -0600

    Create Category show page, update home page w/categories

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,6 +1,7 @@
 class VideosController < ApplicationController
   def index
     @videos = Video.all
+    @categories = Category.all
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Myflix::Application.routes.draw do
   get '/home', to: 'videos#index'
 
   resources :videos, only: [:show]
+  resources :categories, only: [:show]
 end
