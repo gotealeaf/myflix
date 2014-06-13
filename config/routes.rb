16624a61077cda0ff6bfc94d4e05798470f59560
Dito <dhartoto@yahoo.com>
2014-06-13 11:38:03 +1000

    added resources for video

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Myflix::Application.routes.draw do
 
-  get '/home', to: 'videos#index', as: 'home'
+  get '/home', to: 'videos#index', as: :home
+
+  resources :videos, except: :index
 
   get 'ui(/:action)', controller: 'ui'
 end
