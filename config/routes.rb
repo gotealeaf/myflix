3e82ff33d4773c9128593f618f54102daa6c5975
Dito <dhartoto@yahoo.com>
2014-06-13 15:31:41 +1000

    added genre resources and root

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Myflix::Application.routes.draw do
-
+  root to: 'videos#index'
   get '/home', to: 'videos#index', as: :home
 
   resources :videos, except: :index
+  resources :genres, except: :index
 
   get 'ui(/:action)', controller: 'ui'
 end
