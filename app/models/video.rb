3d1191b6556ea958ea67fd023fb82dbebdc05190
Cullen Jett <cullenjett@gmail.com>
2014-06-14 15:23:04 -0600

    Add tests for Video validations

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,3 +1,9 @@
 class Video < ActiveRecord::Base
   belongs_to :category
+
+  validates :title, presence: :true
+  validates :description, presence: :true
+
+  # if you're only validating 'presence' you can use this syntax instead:
+  # validates_presence_of :title, :description
 end
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -12,4 +12,14 @@ describe Video do
     monk = Video.create(title: "Monk", description: "Not my cup of tea, but to each his own.", category: drama)
     expect(monk.category).to eq(drama)
   end
+
+  it "doesn't save a video without a title" do
+    video = Video.create(description: "I don't have a title!")
+    expect(video.errors.any?).to eq(true)
+  end
+
+  it "doesn't save a video without a description" do
+    video = Video.create(title: "I don't have a description!")
+    expect(Video.count).to eq(0)
+  end
 end
