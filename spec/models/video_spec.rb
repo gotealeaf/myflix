af542289ff4d86073fc2d684ed2f9cf2a34b4285
Dito <dhartoto@yahoo.com>
2014-06-16 15:25:23 +1000

    Add video test

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -10,6 +10,6 @@ describe Video do
                  description: "It's often said that the true character of a man is only revealed in times of dire crisis, and for likable, lovelorn loser Shaun (Simon Pegg), that moment of reckoning came when the dead rose from their slumber to feast on the flesh of the living. A hapless electronics store employee who spends most of his spare time downing pints at the local pub with his roommate, Ed (Nick Frost), Shaun's life seems to fall apart when he is dumped by his girlfriend, Liz (Kate Ashfield), and his obnoxious stepfather, Philip (Bill Nighy), shows up to berate him for not being more attentive to his caring mother Barbara (Penelope Wilton) -- especially since he forgot to send flowers for her birthday. Things take a turn for the worse when the dead return to stake their claim on the Earth, and though the chaos that follows threatens to swallow up all of England, it's up to Shaun to keep his cool and prove himself once and for all by successfully rescuing Liz and his mother. With his trusty roommate by his side, nothing -- not even the living dead -- can stand between Shaun and the two most important women in his life. ~ Jason Buchanan, Rovi")
     soth.save
 
-    expect(Video.first.name).to eq(soth.name)
+    expect(Video.first).to eq(soth)
   end
 end
