bcfc1b6f288165f90840fc0f097c790dd374bc0e
Quinn Avila <hkdnow@yahoo.com>
2014-06-16 19:44:38 -0500

    user can change video rating

--- spec/models/queue_item_spec.rb
+++ spec/models/queue_item_spec.rb
@@ -29,6 +29,34 @@ describe QueueItem do
     end
   end
 
+  describe "#rating=" do
+    it "changes the rating of the review if the review is present" do
+      video = Fabricate(:video)
+      user = Fabricate(:user)
+      review = Fabricate(:review, user: user, video: video, rating: 2)
+      queue_item = Fabricate(:queue_item, user: user, video: video)
+      queue_item.rating = 4
+      expect(Review.first.rating).to eq(4)
+    end
+
+    it "clears the rating of the review if the review if present" do
+      video = Fabricate(:video)
+      user = Fabricate(:user)
+      review = Fabricate(:review, user: user, video: video, rating: 2)
+      queue_item = Fabricate(:queue_item, user: user, video: video)
+      queue_item.rating = nil
+      expect(Review.first.rating).to be_nil
+    end
+
+    it "creates a review with the rating if the review is not present" do
+      video = Fabricate(:video)
+      user = Fabricate(:user)
+      queue_item = Fabricate(:queue_item, user: user, video: video)
+      queue_item.rating = 3
+      expect(Review.first.rating).to eq(3)
+    end
+  end
+
   describe "#category_name" do
     it "returns the category's name of the video" do
       category = Fabricate(:category, name: "comedies")
