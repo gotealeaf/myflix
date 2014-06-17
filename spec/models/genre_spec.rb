c87e71779fe3a3d51c547546ca169bc07ef4a965
Dito <dhartoto@yahoo.com>
2014-06-17 21:35:14 +1000

    Add presence and uniqueness test for name

--- spec/models/genre_spec.rb
+++ spec/models/genre_spec.rb
@@ -1,18 +1,7 @@
 require 'rails_helper'
 
 describe Genre do
-  it "saves to database" do
-    genre = Genre.new(name: 'comedy', slug: 'comedy')
-    genre.save
-
-    expect(Genre.first).to eq(genre)
-  end
-
-  it "has many videos" do
-    genre = Genre.create(name: 'action')
-    video_1 = Video.create(name: 'gladiator', description: 'About gladiators', genre: genre)
-    video_2 = Video.create(name: 'terminator', description: 'About robots', genre: genre)
-    expect(Video.where(genre: genre).count).to eq(2)
-    expect(genre.videos).to eq([video_1, video_2])
-  end
+  it { should have_many(:videos).order(:name) }
+  it { should validate_presence_of :name }
+  it { should validate_uniqueness_of :name}
 end
