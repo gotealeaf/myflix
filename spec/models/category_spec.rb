77ea293cf228da868965d767b6b88859a995c859
aj <ajjonez@icloud.com>
2014-06-16 20:06:34 -0500

    Week 1 work

---
+++ spec/models/category_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe Category do
+  it "saves itself" do 
+    category = Category.new(name: "comedies")
+    category.save
+    expect(Category.first).to eq(category)
+  end
+
+  it "has many videos" do
+    comedies = Category.create(name: "comedies")
+    south_park = Video.create(title: "South Park", description: "Funny video", category: comedies)
+    futurama = Video.create(title: "Futurama", description: "Space Travel", category: comedies)
+    expect(comedies.videos).to eq([futurama, south_park])
+  end
+end
