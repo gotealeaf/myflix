84887bf5c9b0d316494d944b15521f91d0b3e150
Dito <dhartoto@yahoo.com>
2014-06-17 18:32:03 +1000

    ordered video by name

--- app/models/video.rb
+++ app/models/video.rb
@@ -2,7 +2,8 @@ class Video < ActiveRecord::Base
   belongs_to :genre
 
   validates :name, presence: true, uniqueness: true
-  validates :slug, uniqueness: true
+  validates :description, presence: true
+  #validates :slug, uniqueness: true
 
   def to_param
     self.slug
