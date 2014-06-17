2132b8a63db2dd96ddb27ebd2e121dbc2ad9fe46
Cullen Jett <cullenjett@gmail.com>
2014-06-17 11:07:22 -0600

    Add simple User tests

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe User do
+  it { should validate_presence_of (:name) }
+  it { should validate_presence_of (:email) }
+  it { should validate_presence_of (:password) }
+  it { should validate_uniqueness_of (:email) }
+
+  it 'has a password that is at least 3 characters long' do
+    user = User.create(name: "Test User", password: "12", email: "test@email.com")
+    expect(User.count).to eq(0)
+  end 
+end
