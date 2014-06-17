5fd1641f67dfb39aa41c681437dbee7c17f67821
aj <ajjonez@icloud.com>
2014-06-16 20:54:06 -0500

    Week 1 work: category model and associations

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -4,6 +4,14 @@ describe Video do
   it "saves itself" do
     video = Video.new(title: "monk", description: "I like this video")
     video.save
-    expect(Video.first).to eq(video)
+    expect(Video.first).to eq(video) 
   end
+
+  it "belongs to category" do
+    dramas = Category.create(name: "dramas")
+    monk = Video.create(title: "monk", description: "I like this video", category: dramas)
+    expect(monk.category).to eq(dramas)
+
+  end
+
 end
