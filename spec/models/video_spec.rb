4d7a23ce70c8545786183af311b8c14a813dab5d
aj <ajjonez@icloud.com>
2014-06-16 21:36:41 -0500

    Week 1 work: Test Validations

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -11,7 +11,14 @@ describe Video do
     dramas = Category.create(name: "dramas")
     monk = Video.create(title: "monk", description: "I like this video", category: dramas)
     expect(monk.category).to eq(dramas)
-
   end
 
+  it "does not save a vidoe without a title" do
+    video = Video.create(description: "a video description")
+    expect(Video.count).to eq(0)
+  end
+  it "does not save a video without a description" do
+    video = Video.create(title: "monk")
+    expect(Video.count).to eq(0)
+  end
 end
