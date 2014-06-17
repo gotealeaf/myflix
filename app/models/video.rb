8c0382ba86a36f52175d05efbfd438f3db317b13
aj <ajjonez@icloud.com>
2014-06-16 23:09:40 -0500

    Week 1 work: Shoulda-Matchers

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,5 +1,6 @@
 class Video < ActiveRecord::Base
   belongs_to :category
 
-  validates_presence_of :title, :description
+  validates_presence_of :title
+  validates_presence_of :description
 end
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -1,24 +1,7 @@
 require 'rails_helper'
 
 describe Video do
-  it "saves itself" do
-    video = Video.new(title: "monk", description: "I like this video")
-    video.save
-    expect(Video.first).to eq(video) 
-  end
-
-  it "belongs to category" do
-    dramas = Category.create(name: "dramas")
-    monk = Video.create(title: "monk", description: "I like this video", category: dramas)
-    expect(monk.category).to eq(dramas)
-  end
-
-  it "does not save a vidoe without a title" do
-    video = Video.create(description: "a video description")
-    expect(Video.count).to eq(0)
-  end
-  it "does not save a video without a description" do
-    video = Video.create(title: "monk")
-    expect(Video.count).to eq(0)
-  end
+  it { should belongs_to(:category) }
+  it { should validates_presence_of(:title) }
+  it { should validates_presence_of(:description) }
 end
