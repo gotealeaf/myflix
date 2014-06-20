ae561e27b8d3a6afc39c2d24a3943a44abe389fe
Dito <dhartoto@yahoo.com>
2014-06-20 10:24:18 +1000

    Add tests for search_by_name

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -5,4 +5,27 @@ describe Video do
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:description) }
   it { should validate_uniqueness_of :name }
+
+  describe '#search_by_name' do
+    it 'should return an empty array if no videos are found' do
+      expect(Video.search_by_name('terminator')).to eq([])
+    end
+    it 'should return and array of one video object if there is one in the database' do
+      video = Video.create(name: 'terminator', description: 'robots and armegaddon')
+      expect(Video.search_by_name('terminator')).to eq([video])
+    end
+    it 'should return an array of one video for a case insensitive match' do
+      video = Video.create(name: 'terminator', description: 'robots and armegaddon')
+      expect(Video.search_by_name('Terminator')).to eq([video])
+    end
+    it 'should return an array of one video object for a partial match' do
+      video = Video.create(name: 'terminator', description: 'robots and armegaddon')
+      expect(Video.search_by_name('term')).to eq([video])
+    end
+    it 'should return an array of video objects if there are multiple matches' do
+      video_1 = Video.create(name: 'terminator', description: 'robots and armegaddon')
+      video_2 = Video.create(name: 'terminator 2', description: 'more robots and armegaddon')
+      expect(Video.search_by_name('term')).to eq([video_1, video_2])
+    end
+  end
 end
