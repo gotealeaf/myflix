0fdc3a7e79aaa21affd43363d540ad7b0e0a805b
Dito <dhartoto@yahoo.com>
2014-06-20 17:47:12 +1000

    Add search collection to videos

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ Myflix::Application.routes.draw do
   root to: 'videos#index'
   get '/home', to: 'videos#index', as: :home
 
-  resources :videos, except: :index
+  resources :videos, except: :index do
+    collection do
+      get :search, to: 'videos#search'
+    end
+  end
+
   resources :genres, except: :index
 
   get 'ui(/:action)', controller: 'ui'
