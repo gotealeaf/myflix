bbf8bc6b49370d586d24648481d078e377356a67
Dito <dhartoto@yahoo.com>
2014-06-21 14:17:33 +1000

    fixed #recent_videos order scope

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,10 +1,10 @@
 class Genre < ActiveRecord::Base
-  has_many :videos, ->{order(:name)}
+  has_many :videos, -> {order(:name)}
   validates :name, presence: true, uniqueness: true
   #validates :slug, uniqueness: true
 
   def recent_videos
-    videos.order(created_at: :desc).limit(6)
+    videos.reorder(created_at: :desc).limit(6)
   end
 
   def to_param
