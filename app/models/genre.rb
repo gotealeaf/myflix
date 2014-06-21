1c07fa4dcd6f527dfaf0cd631ccdee9ea118a25d
Dito <dhartoto@yahoo.com>
2014-06-21 13:44:10 +1000

    Add #recent_videos

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,9 +1,11 @@
 class Genre < ActiveRecord::Base
   has_many :videos, ->{order(:name)}
-
   validates :name, presence: true, uniqueness: true
-
   #validates :slug, uniqueness: true
+
+  def recent_videos
+    videos.order(created_at: :desc).limit(6)
+  end
 
   def to_param
     self.slug
