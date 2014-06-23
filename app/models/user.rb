4db89d406c9b63cfe6a740babcbf074f03868af3
Dito <dhartoto@yahoo.com>
2014-06-23 17:06:34 +1000

    Add password confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password validations: false
-  validates_presence_of :full_name, :email, :password
-  validates_uniqueness_of :email, :slug
-  validates :password, on: :create, length: { minimum: 8 }
+  validates :password, on: :create, length: { minimum: 8 }, confirmation: true
+  validates_presence_of :full_name, :email, :password, :password_confirmation
+  validates_uniqueness_of :email
+
 end
