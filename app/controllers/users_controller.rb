7b316ac195c2ec03e68bbdeea28f3fedd7358a38
Dito <dhartoto@yahoo.com>
2014-06-23 17:10:17 +1000

    Added session to create and password_confirmation to strong params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,16 +7,26 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:notice] = "Welcome #{@user}!"
-      redirect_to :root
+      flash[:success] = "Welcome #{@user.full_name}!"
+      session[:user] = @user.username
+      redirect_to videos_path
     else
+      flash.now[:danger] = @user.errors.full_messages
       render :new
     end
+  end
+
+  def edit
+
+  end
+
+  def update
+
   end
 
   private
 
   def user_params
-    require(:user).permit(:full_name, :email, :password, :slug)
+    params.require(:user).permit(:full_name, :username, :email, :password, :password_confirmation)
   end
 end
