99f1d8d59900ab2031926eeec27bd2492fafeed0
Dito <dhartoto@yahoo.com>
2014-06-23 15:44:04 +1000

    Add sign_in & sign_out routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Myflix::Application.routes.draw do
   root to: 'videos#index'
-  get '/home', to: 'videos#index', as: :home
-
   resources :videos, except: :index do
-    collection do
-      get :search, to: 'videos#search'
-    end
+    get :search, to: 'videos#search', on: :collection
   end
 
   resources :genres, except: :index
-  resources :users, except: [:index, :destroy]
+  resources :users, except: [:new, :index, :destroy]
+
+  get '/home', to: 'videos#index', as: :home
+  get '/sign_in', to: 'sessions#new'
+  post '/sign_in', to: 'sessions#create'
+  get '/sign_out', to: 'sessions#destroy', as: :sign_out
+  get '/register', to: 'users#new', as: :register
   get 'ui(/:action)', controller: 'ui'
 end
