e3baf367098c3be31b670f814000c788728570e0
Dito <dhartoto@yahoo.com>
2014-06-23 17:16:32 +1000

    Add #current_user #logged_in?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  helper_method :current_user, :logged_in?, :require_user
+
+  def current_user # make sure this is the same as Controller
+    @current_user ||= User.find_by(username: session[:username]) if session[:username]
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def require_user
+    # To do
+  end
 end
