3ab4a2494a8e8199b0c435184a92eea595ee0ebb
Dito <dhartoto@yahoo.com>
2014-06-23 10:39:34 +1000

    remove slug, add password_confirmation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,7 +5,7 @@ describe User do
   it { should validate_presence_of :full_name }
   it { should validate_presence_of :email }
   it { should validate_presence_of :password }
+  it { should validate_presence_of :password_confirmation}
   it { should ensure_length_of(:password).is_at_least(8)}
   it { should validate_uniqueness_of :email }
-  it { should validate_uniqueness_of :slug }
 end
