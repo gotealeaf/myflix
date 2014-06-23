f76fdd5ee81492f4b40eb9e8d0807a00af094eae
Dito <dhartoto@yahoo.com>
2014-06-23 17:14:35 +1000

    First commit sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,29 @@
+class SessionsController < ApplicationController
+
+  def index
+
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.find_by(email: params[:email])
+    if @user && @user.authenticate(params[:password])
+      session[:username] = @user.username
+      binding.pry
+      flash[:success] = "Welcome #{@user.username}"
+      redirect_to videos_path
+    else
+      flash.now[:danger] = "Incorrect email or password. Please try again."
+      render :new
+    end
+  end
+
+  def destroy
+    session[:username] = nil
+    redirect_to :root
+  end
+
+end
