6ae0b786c694eb2e4c0953964dae7052d3ce3f9a
Dito <dhartoto@yahoo.com>
2014-06-24 17:39:22 +1000

    Add validation of username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password validations: false
   validates :password, on: :create, length: { minimum: 8 }, confirmation: true
-  validates_presence_of :full_name, :email, :password, :password_confirmation
-  validates_uniqueness_of :email
+  validates_presence_of :username, :full_name, :email, :password, :password_confirmation
+  validates_uniqueness_of :email, :username
 
 end
