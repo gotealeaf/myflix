c443f8917475a1a11908f1c49e4159e3475cf755
Dito <dhartoto@yahoo.com>
2014-06-24 20:21:41 +1000

    Add validation to require username

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 describe User do
   it { should have_secure_password }
+  it { should validate_presence_of :username }
   it { should validate_presence_of :full_name }
   it { should validate_presence_of :email }
   it { should validate_presence_of :password }
