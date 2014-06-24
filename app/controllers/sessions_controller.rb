b63f1b4b52367ff686c2896e867ef962c76e2130
Dito <dhartoto@yahoo.com>
2014-06-24 17:38:21 +1000

    Update log in session to solutions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,19 +1,16 @@
 class SessionsController < ApplicationController
 
-  def index
-
-  end
-
   def new
-    @user = User.new
+    #redirect_to home_path if logged_in?
+    #@user = User.new
   end
 
   def create
-    @user = User.find_by(email: params[:email])
-    if @user && @user.authenticate(params[:password])
-      session[:username] = @user.username
-      flash[:success] = "Welcome #{@user.username}"
-      redirect_to videos_path
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:username] = user.username
+      flash[:success] = "Welcome #{user.username}"
+      redirect_to home_path
     else
       flash.now[:danger] = "Incorrect email or password. Please try again."
       render :new
@@ -22,6 +19,7 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:username] = nil
+    flash[:success] = "You have signed out."
     redirect_to :root
   end
 
