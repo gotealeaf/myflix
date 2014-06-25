12b68a4532f614d9b28fe27230d1fcbc46fdf6e1
Quinn Avila <hkdnow@yahoo.com>
2014-06-25 13:24:24 -0500

    finished more complicated feature spec

--- spec/features/user_interacts_with_queue_spec.rb
+++ spec/features/user_interacts_with_queue_spec.rb
@@ -9,30 +9,42 @@ feature "User interacts with the queue" do
     futurama = Fabricate(:video, title: "Futurama", category: comedies)
 
     sign_in
-    find("a[href='/videos/#{monk.id}']").click
-    page.should have_content(monk.title)
 
-    click_link "+ My Queue"
+    add_video_to_queue(monk)
     page.should have_content(monk.title)
 
     visit video_path(monk)
     page.should_not have_content "+ My Queue"
 
-    visit home_path
-    find("a[href='/videos/#{south_park.id}']").click
-    click_link "+ My Queue"
-    visit home_path
-    find("a[href='/videos/#{futurama.id}']").click
-    click_link "+ My Queue"
+    add_video_to_queue(south_park)
+    add_video_to_queue(futurama)
 
-    fill_in "video_#{monk.id}", with: 3
-    fill_in "video_#{south_park.id}", with: 1
-    fill_in "video_#{futurama.id}", with: 2
 
+    set_video_position(monk, 3)
+    set_video_position(south_park, 1)
+    set_video_position(futurama, 2)
+    
     click_button "Update Instant Queue"
+    
+    expect_video_position(south_park, 1)
+    expect_video_position(futurama, 2)
+    expect_video_position(monk, 3)
+  
+  end
 
-    expect(find("#video_#{south_park.id}").value).to eq("1")
-    expect(find("#video_#{futurama.id}").value).to eq("2")
-    expect(find("#video_#{monk.id}").value).to eq("3")
+  def add_video_to_queue(video)
+    visit home_path
+    find("a[href='/videos/#{video.id}']").click
+    click_link "+ My Queue"
+  end
+
+  def set_video_position(video, position)
+    within(:xpath, "//tr[contains(.,'#{video.title}')]") do
+      fill_in "queue_items[][position]", with: position
+    end
+  end
+
+  def expect_video_position(video, position)
+    expect(find(:xpath, "//tr[contains(.,'#{video.title}')]//input[@type='text']").value).to eq(position.to_s)
   end
 end
