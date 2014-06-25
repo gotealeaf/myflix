4d11d65aa665b94d6935d8a7b4d1e07699ff2de6
Quinn Avila <hkdnow@yahoo.com>
2014-06-25 13:32:33 -0500

    finished more complicated feature spec

--- spec/features/user_interacts_with_queue_spec.rb
+++ spec/features/user_interacts_with_queue_spec.rb
@@ -11,25 +11,36 @@ feature "User interacts with the queue" do
     sign_in
 
     add_video_to_queue(monk)
-    page.should have_content(monk.title)
+    expect_video_to_be_in_queue(monk)
 
     visit video_path(monk)
-    page.should_not have_content "+ My Queue"
-
+    expect_link_not_to_be_seen("+ My Queue")
+    
     add_video_to_queue(south_park)
     add_video_to_queue(futurama)
-
 
     set_video_position(monk, 3)
     set_video_position(south_park, 1)
     set_video_position(futurama, 2)
     
-    click_button "Update Instant Queue"
+    update_queue
     
     expect_video_position(south_park, 1)
     expect_video_position(futurama, 2)
     expect_video_position(monk, 3)
   
+  end
+
+  def expect_video_to_be_in_queue(video)
+    page.should have_content(video.title)
+  end
+
+  def expect_link_not_to_be_seen(link_text)
+    page.should_not have_content(link_text)
+  end
+
+  def update_queue
+    click_button "Update Instant Queue"
   end
 
   def add_video_to_queue(video)
