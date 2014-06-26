08cce2069443df8c4fea67cc6703fe7d133abce2
aj <ajjonez@icloud.com>
2014-06-25 23:02:01 -0500

    Week 1 work: Recent Videos

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,8 @@
 class Category < ActiveRecord::Base
-  has_many :videos, -> { order "title" }
+  has_many :videos, -> { order "created_at DESC" }
+
+  def recent_videos
+    videos.first(6)
+  end
+
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -2,4 +2,35 @@ require 'rails_helper'
 
 describe Category do
   it { should have_many(:videos)}
+
+  describe "#recent_videos" do
+    it "returns the videos in the reverse chronical order by created at" do
+      comedies = Category.create(name: "comedies")
+      futurama = Video.create(title: "Futurama", description: "space travel", category: comedies, created_at: 1.day.ago)
+      south_park = Video.create(title: "South Park", description: "crazy kids", category: comedies)
+      expect(comedies.recent_videos).to eq([south_park, futurama])
+    end
+
+    it "returns all the videos if there are less than 6 videos" do
+      comedies = Category.create(name: "comedies")
+      futurama = Video.create(title: "Futurama", description: "space travel", category: comedies, created_at: 1.day.ago)
+      south_park = Video.create(title: "South Park", description: "crazy kids", category: comedies)
+      expect(comedies.recent_videos.count).to eq(2)
+    end
+    it "returns 6 videos if there are more than 6 videos" do
+      comedies = Category.create(name: "comedies")
+      7.times { Video.create(title: "foo", description: "bar", category: comedies)}
+      expect(comedies.recent_videos.count).to eq(6)
+    end
+    it "returns the most recent 6 videos" do
+      comedies = Category.create(name: "comedies")
+      6.times { Video.create(title: "foo", description: "bar", category: comedies)}
+        tonight_show = Video.create(title: "Tonight Show", description: "Late night tv", category: comedies, created_at: 1.day.ago)
+      expect(comedies.recent_videos).not_to include(tonight_show)
+    end
+    it "returns anempty array if the category does not have any videos" do
+      comedies = Category.create(name: "comedies")
+      expect(comedies.recent_videos).to eq([])
+    end
+  end
 end
