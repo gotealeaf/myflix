5b3340ebd5d71caa28a360b38e221af25f1a9297
Dito <dhartoto@yahoo.com>
2014-06-29 17:08:13 +1000

    Add belong to video spec

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 describe Review do
   it { should belong_to(:user) }
+  it { should belong_to(:video) }
   it { should validate_presence_of :rating }
   it { should validate_presence_of :review }
   it { should validate_presence_of :user_id }
