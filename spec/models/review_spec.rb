881e29bea7f7b6d9ad04627dc5817a2f13b5d120
Dito <dhartoto@yahoo.com>
2014-06-29 16:29:39 +1000

    Add and validate presence of rating, review, user id and video id

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -2,4 +2,8 @@ require 'rails_helper'
 
 describe Review do
   it { should belong_to(:user) }
+  it { should validate_presence_of :rating }
+  it { should validate_presence_of :review }
+  it { should validate_presence_of :user_id }
+  it { should validate_presence_of :video_id }
 end
