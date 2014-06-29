e32688a6363dd97c07af94c10fbcabbc0486e185
Dito <dhartoto@yahoo.com>
2014-06-29 15:33:42 +1000

    Changed users#create params to use Fabricator

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -6,27 +6,23 @@ describe UsersController do
       get :new
       expect(assigns(:user)).to be_a_new(User)
     end
-    it 'renders new template' do
-      get :new
-      expect(response).to render_template(:new)
-    end
+
+    # no need to test render new because it's a rails code
   end
 
   describe 'POST create' do
     context 'if validation passes' do
-      before do
-        post :create, user: { username: 'dhartoto', full_name: 'Dito Hartoto',
-                              email: 'dhartoto@yahoo.com', password: 'password',
-                              password_confirmation: 'password'}
-      end
+      
+      before { post :create, user: Fabricate.attributes_for(:user) }
+
       it 'creates a new user' do
         expect(User.all.count).to eq(1)
       end
       it 'Flash a welcome message' do
-        expect(flash[:success]).to eq("Welcome Dito Hartoto!")
+        expect(flash[:success].blank?).to eq(false)
       end
       it 'assigns session[:user]' do
-        expect(session[:username]).to eq('dhartoto')
+        expect(session[:username].blank?).to eq(false)
       end
       it 'redirects to video_path' do
         expect(response).to redirect_to videos_path
@@ -34,9 +30,7 @@ describe UsersController do
     end
 
     context 'if validation fails' do
-      before do
-        post :create, user: { username: 'dhartoto' }
-      end
+      before { post :create, user: { username: 'test_user' } }
 
       it 'does not create a user' do
         expect(User.count).to eq(0)
