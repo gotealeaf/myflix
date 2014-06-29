fcd0b330f705dd977bef56c82018d0dc00b188cc
AZS <ambshar@gmail.com>
2014-06-29 23:10:17 +0200

    passing local_env file

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,12 @@ module Myflix
     config.active_support.escape_html_entities_in_json = true
     config.autoload_paths += %W(#{config.root}/lib)
     config.assets.enabled = true
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+      ENV[key.to_s] = value
+      end if File.exists?(env_file)
+    end
     config.generators do |g|
       g.orm :active_record
       g.template_engine :haml
