05837c9bd90fab803be85c42d4d9fce4b26745c9
Dito <dhartoto@yahoo.com>
2014-07-02 21:16:36 +1000

    Add spec for queue_videos#index

--- spec/controllers/queue_videos_controller_spec.rb
+++ spec/controllers/queue_videos_controller_spec.rb
@@ -3,8 +3,21 @@ require 'rails_helper'
 describe QueueVideosController do
   describe 'GET Index' do
     context 'when user is authenticated' do
-      it 'should assign @queue_videos variable'
-      it 'should render index template'
+      let(:current_user) { Fabricate(:user) }
+      it 'should assign @queue_videos variable' do
+        user = current_user
+        session[:username] = user.username
+        queue_video1 = Fabricate(:queue_video, user: user)
+        queue_video2 = Fabricate(:queue_video, user: user)
+        get :index
+        expect(assigns(:queue_videos)).to match_array([queue_video1, queue_video2])
+      end
+    end
+    context 'when user is unauthenticated' do
+      it 'should redirect to sign in page' do
+        get :index
+        expect(response).to redirect_to sign_in_path
+      end
     end
   end
 end
