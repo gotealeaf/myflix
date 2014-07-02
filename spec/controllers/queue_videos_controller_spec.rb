52bf362f5fb842e226bb9b229e5980b481c05af6
Dito <dhartoto@yahoo.com>
2014-07-02 20:19:59 +1000

    Add queue_video_controller spec

---
+++ spec/controllers/queue_videos_controller_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+describe QueueVideosController do
+  describe 'GET Index' do
+    context 'when user is authenticated' do
+      it 'should assign @queue_videos variable'
+      it 'should render index template'
+    end
+  end
+end
