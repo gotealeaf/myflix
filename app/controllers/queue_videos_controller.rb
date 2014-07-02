45547e10ee1da46cd33309c685c78cf2f07fbb49
Dito <dhartoto@yahoo.com>
2014-07-02 21:14:41 +1000

    Add queue_videos controller

--- app/controllers/queue_videos_controller.rb
+++ app/controllers/queue_videos_controller.rb
@@ -1,3 +1,7 @@
 class QueueVideosController < ApplicationController
-
+  before_action :require_user
+  def index
+    # QueueVideo.where(user: current_user)
+    @queue_videos = current_user.queue_videos
+  end
 end
