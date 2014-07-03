58e9a70cee8c66687e4ed0b0d3741c3c116f62a4
Cullen Jett <cullenjett@gmail.com>
2014-07-03 11:16:15 -0600

    Tests for new user email delivery

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -36,6 +36,28 @@ describe UsersController do
         expect(assigns(:user)).to be_a(User)
       end
     end
+
+    context "sending emails" do
+      after { ActionMailer::Base.deliveries.clear }
+      
+      it "sends email to new users with vaild inputs" do
+        post :create, user: { email: "joe@example.com", password: "password", name: "Joe" }
+
+        expect(ActionMailer::Base.deliveries.last.to).to eq(["joe@example.com"])
+      end
+      
+      it "sends email containing the user's name with valid inputs" do
+        post :create, user: { email: "joe@example.com", password: "password", name: "Joe" }
+
+        expect(ActionMailer::Base.deliveries.last.body).to include("Joe")
+      end
+
+      it "does not send email with invalid inputs" do
+        post :create, user: { email: "joe@example.com" }
+
+        expect(ActionMailer::Base.deliveries).to be_empty
+      end
+    end
   end # POST create
 
   describe "GET show" do
