7c7f812ecf9edd56567e6f8a5fd72ceffad52db6
James Coleman <james.k.coleman@gmail.com>
2014-07-04 15:13:02 -0400

    Finished Week 2 assignment 10

---
+++ spec/controllers/videos_controller_spec.rb
@@ -0,0 +1,32 @@
+require 'spec_helper'
+
+describe VideosController do
+  describe "GET show" do
+    it "sets @video for authenticated users" do
+      session[:user_id] = Fabricate(:user).id
+      video = Fabricate(:video)
+      get :show, id: video.id
+      expect(assigns(:video)).to eq(video)
+    end
+
+    it "redirects unauthenticated users to the sign in page" do
+      video = Fabricate(:video)
+      get :show, id: video.id
+      expect(response).to redirect_to sign_in_path
+    end
+  end
+
+  describe "POST search" do
+    it "sets @results for authenticated users" do
+      session[:user_id] = Fabricate(:user).id
+      futurama = Fabricate(:video, title: 'Futurama')
+      post :search, search_term: 'rama'
+      expect(assigns(:results)).to eq([futurama])
+    end
+    it "redirects unauthenticated users to sign in page" do
+      futurama = Fabricate(:video, title: 'Futurama')
+      post :search, search_term: 'rama'
+      expect(response).to redirect_to sign_in_path
+    end
+  end
+end
