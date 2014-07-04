d0b41c41a214c90da56f45a0b45d03a3ba3cb83c
Dito <dhartoto@yahoo.com>
2014-07-04 20:01:53 +1000

    Add create action

--- app/controllers/queue_videos_controller.rb
+++ app/controllers/queue_videos_controller.rb
@@ -1,7 +1,26 @@
 class QueueVideosController < ApplicationController
   before_action :require_user
+
   def index
-    # QueueVideo.where(user: current_user)
+    #@queue_videos = QueueVideo.where(user: current_user)
     @queue_videos = current_user.queue_videos
   end
+
+  def create
+    unless video_in_queue
+      QueueVideo.create(video_id: params[:video_id], user: current_user, position: queue_position)
+    end
+    redirect_to my_queue_path
+  end
+
+  private
+
+  def queue_position
+    QueueVideo.count + 1
+  end
+
+  def video_in_queue
+    QueueVideo.find_by(video_id: params[:video_id])
+  end
+
 end
