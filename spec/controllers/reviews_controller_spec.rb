1b9e2a8767c0cb3351609009f3b5d81c64755205
Dito <dhartoto@yahoo.com>
2014-07-14 15:34:34 +1000

    Refactor with macros and shared examples

--- spec/controllers/reviews_controller_spec.rb
+++ spec/controllers/reviews_controller_spec.rb
@@ -4,9 +4,7 @@ describe ReviewsController do
   describe 'POST create' do
     context 'with authenticated user' do
 
-      let(:current_user) {Fabricate(:user)}
-      let(:video) { Fabricate(:video)}
-      before { session[:username] = current_user.username }
+      before { set_session_user }
 
       context 'if validations pass' do
         before do
@@ -22,7 +20,7 @@ describe ReviewsController do
             expect(Review.first.video).to eq(video)
           end
           it 'creates a review associated with the user' do
-            expect(Review.first.user).to eq(current_user)
+            expect(Review.first.user).to eq(user)
           end
           it 'flashes a success message' do
             expect(flash[:success]).to eq("Thanks! Your review has been posted!")
@@ -34,7 +32,7 @@ describe ReviewsController do
         it 'should not create a review' do
           expect(Review.count).to eq(0)
         end
-        it 'should redirect to the videos/show page' do
+        it 'should render the videos/show page' do
           expect(response).to render_template 'videos/show'
         end
         it 'assigns @video variable' do
@@ -42,18 +40,15 @@ describe ReviewsController do
         end
 
         it 'assigns @review variable' do
-          reviews = Fabricate(:review, user: current_user, video: video)
+          reviews = Fabricate(:review, user: user, video: video)
           expect(assigns(:reviews)).to match_array(reviews)
         end
       end
 
     end
-    context "with unauthenticated user" do
-      it 'should redirect to the sign in path' do
-        video = Fabricate(:video)
-        post :create, review: Fabricate.attributes_for(:review), video_id: video.id
-        expect(response).to redirect_to sign_in_path
-      end
+
+    it_behaves_like 'redirect for unauthenticated user' do
+      let(:action) { post :create, review: Fabricate.attributes_for(:review), video_id: video.id }
     end
   end
 end
