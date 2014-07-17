74c3f35ae3b2edd8bb6ce925bd9fa38fd83a6431
Dito <dhartoto@yahoo.com>
2014-07-17 14:15:56 +1000

    Update sign_in method to new method name

--- spec/features/user_sign_in_spec.rb
+++ spec/features/user_sign_in_spec.rb
@@ -2,12 +2,12 @@ require 'rails_helper'
 
 feature "Signing in" do
   scenario 'with registered user' do
-    sign_in(user)
+    user_signs_in(user)
     expect(page).to have_content("Welcome #{ user.username }")
   end
 
   scenario 'with unregistered user' do
-    sign_in
+    user_signs_in
     click_button 'Sign in'
     expect(page).to have_content('Incorrect email or password. Please try again.')
   end
