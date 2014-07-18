50ebe47b8f8b6ce928a657fbab2311fdfb914220
Dito <dhartoto@yahoo.com>
2014-07-18 15:47:39 +1000

    update have_many reviews to be in descending order by created_at

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe User do
-  it { should have_many(:reviews) }
+  it { should have_many(:reviews).order(created_at: :desc) }
   it { should have_many(:queue_videos).order(:position) }
   it { should have_secure_password }
   it { should validate_presence_of :username }
