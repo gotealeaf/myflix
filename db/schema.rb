942e155bf8ac9249b7c802a7c90449133c487941
Dito <dhartoto@yahoo.com>
2014-07-21 14:28:36 +1000

    Add followings table

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140702103627) do
+ActiveRecord::Schema.define(version: 20140720053518) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "followings", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "followee_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "genres", force: true do |t|
     t.string "name"
