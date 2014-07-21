e57a953d06adefd696e53f63b4b33bf42e43e4f5
Dito <dhartoto@yahoo.com>
2014-07-21 16:54:43 +1000

    Delegate full_name and email to User, add #count_queue_videos and #count_followers

--- app/models/following.rb
+++ app/models/following.rb
@@ -1,4 +1,15 @@
 class Following < ActiveRecord::Base
   belongs_to :user
-  belongs_to :followee, class_name: "User"
+  belongs_to :followee, class_name: 'User'
+
+  delegate :full_name, to: :followee
+  delegate :email, to: :followee
+
+  def count_queue_videos
+    followee.queue_videos.count
+  end
+
+  def count_followers
+    Following.where(followee_id: followee.id).count
+  end
 end
