7d2ba130bc713dcd581db1ccc9dbace50bb24eba
James Coleman <james.k.coleman@gmail.com>
2014-07-21 16:37:32 -0400

    Changed email configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,13 +18,15 @@ Myflix::Application.configure do
 
   config.active_support.deprecation = :notify
 
+  config.action_mailer.default_url_options = { host: 'limitless-scrubland-2896.heroku.com' }
+
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :port           => ENV['MAILGUN_SMTP_PORT'],
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'yourapp.heroku.com',
+    :domain         => 'limitless-scrubland-2896.heroku.com',
     :authentication => :plain,
   }
 
