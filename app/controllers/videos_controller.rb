cd215af5a86928a21f43c1d212dd954d356c4671
davidhuff2014 <David.Huff@computer-critters.com>
2014-07-23 12:59:53 -0400

    after video table creation

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,5 +1,6 @@
 class VideosController < ApplicationController
     def index
       # this will be routed to /home/index.html.haml
+      @videos = Video.all
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Myflix::Application.routes.draw do
   get 'ui(/:action)', controller: 'ui'
+
+  # default home page
+  # get '/home', to: 'videos#index'
+  resources :videos, :path => 'home'
 end
