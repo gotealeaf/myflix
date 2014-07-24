521b98be7f210ab17584fd622e315d021a3c354d
Dito <dhartoto@yahoo.com>
2014-07-24 17:00:06 +1000

    Change domain name to heroku url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,7 @@ Myflix::Application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'gmail.com',
+    domain:               'http://myflix-rottenapple.herokuapp.com',
     user_name:            ENV["GMAIL_USERNAME"],
     password:             ENV["GMAIL_PASSWORD"],
     authentication:       'plain',
