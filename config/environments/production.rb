608d22f3560ce88310b51e1eaf73159017958a9b
Dito <dhartoto@yahoo.com>
2014-07-25 07:48:14 +1000

    Add host to action mailer default url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,10 +20,12 @@ Myflix::Application.configure do
 
   config.action_mailer.delivery_method = :smtp
 
+  config.action_mailer.default_url_options = { host: 'myflix-rottenapple.herokuapp.com' }
+
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'www.gmail.com', 
+    domain:               'www.gmail.com',
     user_name:            ENV["GMAIL_USERNAME"],
     password:             ENV["GMAIL_PASSWORD"],
     authentication:       'plain',
