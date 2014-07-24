6d4a9bfe7c0955ae04e143df85cb055b0e65dc2c
Dito <dhartoto@yahoo.com>
2014-07-24 21:49:43 +1000

    removed domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,6 @@ Myflix::Application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'seedragon.com',
     user_name:            ENV["GMAIL_USERNAME"],
     password:             ENV["GMAIL_PASSWORD"],
     authentication:       'plain',
