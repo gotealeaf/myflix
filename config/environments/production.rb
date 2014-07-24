be7e8d50f7a295177c49aeaa302eb4e5b8946de9
Reed Nelson-Saunders <reedns@hotmail.com>
2014-07-24 14:56:38 -0700

    Fixed email configs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,14 +17,17 @@ Myflix::Application.configure do
   config.i18n.fallbacks = true
 
   config.active_support.deprecation = :notify
-
-  ActionMailer::Base.smtp_settings = {
-  :address        => "smtp.gmail.com",
-  :port           => 587,
-  :domain         => "domain.com",
-  :user_name      => ENV["gmail_username"]
-  :password       => ENV["password"]
-  :authentication => :plain
+ 
+ config.action_mailer.delivery_method = :smtp
+ config.action_mailer.default_url_options = { host: "http://vast-scrubland-1489.herokuapp.com" }
+ config.action_mailer.smtp_settings = {
+  address:        "smtp.gmail.com",
+  port:           587,
+  domain:         "http://vast-scrubland-1489.herokuapp.com/",
+  user_name:      ENV["gmail_username"],
+  password:       ENV["password"],
+  authentication: "plain",
+  enable_starttls_auto: true
   }
 
 end
