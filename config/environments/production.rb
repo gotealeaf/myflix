1b75dc3e5ecf34bcafcfede3449bc96290be89fa
Reed Nelson-Saunders <reedns@hotmail.com>
2014-07-24 15:09:53 -0700

    Fixed typo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,16 +18,16 @@ Myflix::Application.configure do
 
   config.active_support.deprecation = :notify
  
- config.action_mailer.delivery_method = :smtp
- config.action_mailer.default_url_options = { host: "http://vast-scrubland-1489.herokuapp.com" }
- config.action_mailer.smtp_settings = {
-  address:        "smtp.gmail.com",
-  port:           587,
-  domain:         "http://vast-scrubland-1489.herokuapp.com/",
-  user_name:      ENV["gmail_username"],
-  password:       ENV["password"],
-  authentication: "plain",
-  enable_starttls_auto: true
-  }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: "http://vast-scrubland-1489.herokuapp.com" }
+  config.action_mailer.smtp_settings = {
+    address:        "smtp.gmail.com",
+    port:           587,
+    domain:         "http://vast-scrubland-1489.herokuapp.com/",
+    user_name:      ENV["gmail_username"],
+    password:       ENV["password"],
+    authentication: "plain",
+    enable_starttls_auto: true
+    }
 
 end
