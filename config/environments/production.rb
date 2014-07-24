21b348eef4085602eedff62dd9609beb8e63b81b
Dito <dhartoto@yahoo.com>
2014-07-25 07:44:28 +1000

    Revert domain to gmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,6 +23,7 @@ Myflix::Application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
+    domain:               'www.gmail.com', 
     user_name:            ENV["GMAIL_USERNAME"],
     password:             ENV["GMAIL_PASSWORD"],
     authentication:       'plain',
