8763f16368f35339c820e76ef88b2c6a0ed8bebb
Dito <dhartoto@yahoo.com>
2014-07-24 15:57:03 +1000

    Add environment var to mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,8 +24,8 @@ Myflix::Application.configure do
     address:              'smtp.gmail.com',
     port:                 587,
     domain:               'gmail.com',
-    user_name:            'example@gmail.com',
-    password:             'password_here',
+    user_name:            ENV[:GMAIL_USERNAME],
+    password:             ENV[:GMAIL_PASSWORD],
     authentication:       'plain',
     enable_starttls_auto: true  }
 end
