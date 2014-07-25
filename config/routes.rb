2c8e07f371526f58b3c102cdbe989a8d0061e180
Dito <dhartoto@yahoo.com>
2014-07-26 09:13:30 +1000

    Add password_resets#new and create and pages#confirm_password_reset

--- config/routes.rb
+++ config/routes.rb
@@ -10,14 +10,19 @@ Myflix::Application.routes.draw do
   resources :queue_videos, only: [:create, :destroy]
   resources :followings, only: [:create, :destroy]
 
-  get '/home', to: 'videos#index'
-  get '/sign_in', to: 'sessions#new'
-  post '/sign_in', to: 'sessions#create'
-  get '/sign_out', to: 'sessions#destroy'
-  get '/register', to: 'users#new'
-  get '/my_queue', to: 'queue_videos#index'
-  get '/people', to: 'followings#index'
-  post '/update_queue', to: 'queue_videos#update_queue'
+  get 'home', to: 'videos#index'
+  get 'sign_in', to: 'sessions#new'
+  post 'sign_in', to: 'sessions#create'
+  get 'sign_out', to: 'sessions#destroy'
+  get 'register', to: 'users#new'
+  get 'my_queue', to: 'queue_videos#index'
+  get 'people', to: 'followings#index'
+
+  get 'forgot_password', to: 'password_resets#new'
+  post'forgot_password', to: 'password_resets#create'
+  get 'confirm_password_reset', to: 'pages_controller#confirm_password_reset'
+
+  post 'update_queue', to: 'queue_videos#update_queue'
 
   get 'ui(/:action)', controller: 'ui'
 end
