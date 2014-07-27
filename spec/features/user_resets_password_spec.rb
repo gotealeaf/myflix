dce5df291accb7c6dfedbaad39da7bff23db9522
Quinn Avila <hkdnow@yahoo.com>
2014-07-27 18:24:24 -0500

    rspec working capybara spec not

--- spec/features/user_resets_password_spec.rb
+++ spec/features/user_resets_password_spec.rb
@@ -3,8 +3,10 @@ require 'spec_helper'
 feature 'User resets password' do
   scenario 'user successfully resets the password' do
     alice = Fabricate(:user, password: 'old_password')
+
     visit sign_in_path
     click_link "Forgot Password"
+
     fill_in "Email Address", with: alice.email
     click_button "Send Email"
 
--- spec/features/user_sign_in_spec.rb
+++ spec/features/user_sign_in_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 feature "user signs in" do
   scenario "with valid email and password" do
     alice = Fabricate(:user)
-    sign_in(alice)    
+    sign_in(alice)
     page.should have_content alice.full_name
   end
 end
