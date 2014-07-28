9c02e87da5947897f31a0dfd3c71dffa10d2e15c
Dito <dhartoto@yahoo.com>
2014-07-28 13:41:22 +1000

    Add condition for blank and invalid email

--- app/controllers/forgot_passwords_controller.rb
+++ app/controllers/forgot_passwords_controller.rb
@@ -4,8 +4,13 @@ class ForgotPasswordsController < ApplicationController
 
   def create
     @user = User.find_by(email: params[:email])
-    password_reset = PasswordReset.create(token: SecureRandom.urlsafe_base64, user: @user)
-    MyflixMailer.password_reset_email(password_reset).deliver
-    redirect_to confirm_password_reset_path
+    if @user
+      password_reset = PasswordReset.create(token: SecureRandom.urlsafe_base64, user: @user)
+      MyflixMailer.password_reset_email(password_reset).deliver
+      redirect_to confirm_password_reset_path
+    else
+      flash[:danger] = params[:email].blank? ? "Email can't be blank": "Email invalid"
+      redirect_to forgot_password_path
+    end
   end
 end
