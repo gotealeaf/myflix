0b12bb8ac6bd7d506e1a65055f364cbecd2da43b
James Coleman <james.k.coleman@gmail.com>
2014-07-28 08:28:49 -0400

    Changed Unicorn config

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -3,10 +3,7 @@ timeout 15
 preload_app true
 
 before_fork do |server, worker|
-   @sidekiq_pid ||= spawn("bundle exec sidekiq -c 2")
-end
-
-before_fork do |server, worker|
+  @sidekiq_pid ||= spawn("bundle exec sidekiq -c 2")
   Signal.trap 'TERM' do
     puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
     Process.kill 'QUIT', Process.pid
