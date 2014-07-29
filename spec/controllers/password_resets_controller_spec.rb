e1f31e2933db4baae65df7c0e3dc402bc330088e
Dito <dhartoto@yahoo.com>
2014-07-29 10:54:56 +1000

    Amend specs to reflect removal of session

--- spec/controllers/password_resets_controller_spec.rb
+++ spec/controllers/password_resets_controller_spec.rb
@@ -8,10 +8,10 @@ describe PasswordResetsController do
         get :show, id: password_reset.token
         expect(assigns(:user)).to eq(user)
       end
-      it 'redirects to reset password page' do
+      it 'assigns @token variable' do
         password_reset = Fabricate(:password_reset, user: user)
         get :show, id: password_reset.token
-        expect(response).to redirect_to reset_password_path
+        expect(assigns(:token)).to eq(user.password_reset.token)
       end
     end
 
@@ -25,53 +25,41 @@ describe PasswordResetsController do
 
   describe 'POST create' do
     it 'assigns @user variable with valid user' do
-      user = Fabricate(:user)
-      session[:id] = user.id
-      post :create, id: 1
+      password_reset = Fabricate(:password_reset, user: user)
+      post :create, token: password_reset.token
       expect(assigns(:user)).to eq(user)
     end
     context 'if new password is valid' do
-
-      before { session[:id] = user.id }
-
       it 'saves the new password if valid' do
         password_reset = Fabricate(:password_reset, user: user)
-        post :create, { password: 'drowssap', password_confirmation: 'drowssap' }
+        post :create, { token: password_reset.token, password: 'drowssap', password_confirmation: 'drowssap' }
         expect(User.first.authenticate('drowssap')).to eq(user)
-      end
-      it 'sets session[:id] to nil' do
-        password_reset = Fabricate(:password_reset, user: user)
-        post :create, { password: 'drowssap', password_confirmation: 'drowssap' }
-        expect(session[:id]).to be_nil
       end
       it 'should delete user associated row from password_resets table' do
         password_reset = Fabricate(:password_reset, user: user)
-        post :create, { password: 'drowssap', password_confirmation: 'drowssap' }
+        post :create, { token: password_reset.token, password: 'drowssap', password_confirmation: 'drowssap' }
         expect(PasswordReset.count).to eq(0)
       end
       it 'flashes a success message' do
         password_reset = Fabricate(:password_reset, user: user)
-        post :create, { password: 'drowssap', password_confirmation: 'drowssap' }
+        post :create, { token: password_reset.token, password: 'drowssap', password_confirmation: 'drowssap' }
         expect(flash[:success]).to eq('Your password has been reset!')
       end
       it 'redirects to sign in page' do
         password_reset = Fabricate(:password_reset, user: user)
-        post :create, { password: 'drowssap', password_confirmation: 'drowssap' }
+        post :create, { token: password_reset.token, password: 'drowssap', password_confirmation: 'drowssap' }
         expect(response).to redirect_to sign_in_path
       end
     end
     context 'if new password is not valid' do
-
-      before { session[:id] = user.id }
-
       it 'redirects to reset password page if new password not valid' do
         password_reset = Fabricate(:password_reset, user: user)
-        post :create, { password: 'drowssap', password_confirmation: 'password' }
+        post :create, { token: password_reset.token, password: 'drowssap', password_confirmation: 'password' }
         expect(response).to redirect_to reset_password_path
       end
       it 'flashes a message if new password is not valid' do
         password_reset = Fabricate(:password_reset, user: user)
-        post :create, { password: 'drowssap', password_confirmation: 'password' }
+        post :create, { token: password_reset.token, password: 'drowssap', password_confirmation: 'password' }
         expect(flash[:danger]).to include('please try again.')
       end
     end
