82638baec4be492a2b4336a2102c0ec4066fdc41
Dito <dhartoto@yahoo.com>
2014-07-29 10:51:46 +1000

    Remove session and #new action, amend  @user to find by token

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -4,21 +4,16 @@ class PasswordResetsController < ApplicationController
     @password_reset = PasswordReset.find_by(token: params[:id])
     if @password_reset
       @user = @password_reset.user
-      session[:id] = @user.id
-      redirect_to reset_password_path
+      @token = params[:id]
     else
       redirect_to invalid_token_path
     end
   end
 
-  def new
-  end
-
   def create
-    @user = User.find(session[:id])
+    @user = PasswordReset.find_by(token: params[:token]).user
     if password_confirmation_match && @user.update_attributes(password: params[:password],
                             password_confirmation: params[:password_confirmation])
-      session[:id] = nil
       @user.password_reset.delete
       flash[:success] = "Your password has been reset!"
       redirect_to sign_in_path
