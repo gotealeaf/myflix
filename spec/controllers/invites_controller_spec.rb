d6c55778959852908e980af3239d58373e72ed8d
Dito <dhartoto@yahoo.com>
2014-07-30 15:19:12 +1000

    Add spec for POST create

---
+++ spec/controllers/invites_controller_spec.rb
@@ -0,0 +1,52 @@
+require 'rails_helper'
+
+describe InvitesController do
+  describe 'POST Create' do
+    it_behaves_like 'redirect for unauthenticated user' do
+      let(:action) { post :create, id: 1 }
+    end
+    context 'when user is authenticated' do
+      before { set_session_user }
+      context 'when fields are blank' do
+        it 'redirects to invite page' do
+          post :create, invite: [{ friend_name: '', friend_email: '' }]
+          expect(response).to redirect_to invite_path
+        end
+        it 'flashes error message' do
+          post :create, invite: [{ friend_name: '', friend_email: '' }]
+          expect(flash[:danger]).to eq("Fields can't be empty")
+        end
+      end
+      context 'when email is not valid' do
+        it 'redirects to invite page' do
+          post :create, invite: [{ friend_name: 'john', friend_email: 'john.com' }]
+          expect(response).to redirect_to invite_path
+        end
+        it 'flashes error message' do
+          post :create, invite: [{ friend_name: 'john', friend_email: 'john.com' }]
+          expect(flash[:danger]).to eq('Please enter a valid email')
+        end
+      end
+      context 'when inputs are valid' do
+        before { set_session_user }
+        it 'assigns @user' do
+          post :create, invite: [{ message: 'some message', friend_name: 'Nelle', friend_email: 'nelle@example.com' }]
+          expect(assigns(:inviter)).to eq(user)
+        end
+        it 'assigns @invite' do
+          user_inputs = { "message" => 'some message', "friend_name" => 'Nelle', "friend_email" => 'nelle@example.com' }
+          post :create, invite: [user_inputs]
+          expect(assigns(:invite)).to eq(user_inputs)
+        end
+        it 'delivers email to @friend_email' do
+          post :create, invite: [{ message: 'some message', friend_name: 'Nelle', friend_email: 'nelle@example.com' }]
+          expect(ActionMailer::Base.deliveries.last.to).to eq(['nelle@example.com'])
+        end
+        it 'redirects to home page' do
+          post :create, invite: [{ message: 'some message', friend_name: 'Nelle', friend_email: 'nelle@example.com' }]
+          expect(response).to redirect_to home_path
+        end
+      end
+    end
+  end
+end
