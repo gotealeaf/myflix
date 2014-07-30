372f181c737ca9f24dc52976dbce68267397ebc9
Dito <dhartoto@yahoo.com>
2014-07-30 15:11:21 +1000

    Add #follow_inviter_if_invited

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,7 @@ class UsersController < ApplicationController
       flash[:success] = "Welcome #{@user.full_name}!"
       session[:username] = @user.username
       MyflixMailer.welcome_email(current_user).deliver
+      follow_inviter_if_invited
       redirect_to videos_path
     else
       render :new
@@ -22,9 +23,16 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
-private
+  private
 
   def user_params
     params.require(:user).permit(:full_name, :username, :email, :password, :password_confirmation)
+  end
+
+  def follow_inviter_if_invited
+    unless params[:inviter_id].blank?
+      Following.create(user_id: @user.id, followee_id: params[:inviter_id])
+      Following.create(user_id: params[:inviter_id], followee_id: @user.id)
+    end
   end
 end
