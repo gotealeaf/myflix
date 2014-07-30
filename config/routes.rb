5cd59328e600912f888ae105f60284174002e95d
davidhuff2014 <David.Huff@computer-critters.com>
2014-07-30 15:38:29 -0400

    removing .idea from git and other cleanup

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Myflix::Application.routes.draw do
   #this path was in video
   root to: "videos#index"
 
-
   # decided on this as most syntactically correct version
   # removing my version for solution version
   # get '/home' => 'videos#index', :as => :videos
@@ -10,15 +9,17 @@ Myflix::Application.routes.draw do
   # this may change as development changes
   # removing my version for solution version
   # get '/video' => 'videos#show'
-  # resources :videos, only: [:show, :index]
 
   resources :videos, only: [:show] do
     collection do
       post :search, to: 'videos#search'
     end
+
   end
-  # not shown in solution
+  # removing my version for solution version
   # get '/genre' => 'categories#show', :as => :categories
+
   resources :categories, only: [:show]
+
   get 'ui(/:action)', controller: 'ui'
 end
