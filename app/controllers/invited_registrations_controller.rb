92dbb8f8aef90c97793bc71e71f2b446f4e45129
Dito <dhartoto@yahoo.com>
2014-07-31 22:56:37 +1000

    Add condition for expired token

--- app/controllers/invited_registrations_controller.rb
+++ app/controllers/invited_registrations_controller.rb
@@ -1,10 +1,14 @@
 class InvitedRegistrationsController < ApplicationController
 
   def new
-    @user = User.new
-    @friend_name = params[:friend_name]
-    @friend_email = params[:friend_email]
-    @token = params[:token]
-    render 'users/new'
+    case !!UserToken.find_by(token: params[:token])
+    when true
+      @user = User.new
+      @friend_name = params[:friend_name]
+      @friend_email = params[:friend_email]
+      @token = params[:token]
+      render 'users/new'
+    when false then redirect_to register_path
+    end
   end
 end
