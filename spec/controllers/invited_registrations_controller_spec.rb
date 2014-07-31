630f3e9e3dccc118420910b42e4b09fc6b8a19f7
Dito <dhartoto@yahoo.com>
2014-07-31 21:13:31 +1000

    Update spec to reflect change from inviter token

--- spec/controllers/invited_registrations_controller_spec.rb
+++ spec/controllers/invited_registrations_controller_spec.rb
@@ -2,40 +2,25 @@ require 'rails_helper'
 
 describe InvitedRegistrationsController do
   describe 'GET new' do
-    context 'when user is authenticated' do
+    before { set_session_user }
 
-      before { set_session_user }
-
-      it 'should assign @friend_name' do
-        inviter = Fabricate(:user)
-        get :new, friend_name: 'Nelle', inviter_id: inviter.id
-        expect(assigns(:friend_name)).to eq('Nelle')
-      end
-      it 'should assign @friend_email' do
-        inviter = Fabricate(:user)
-        get :new, friend_email: 'nelle@example.com', inviter_id: inviter.id
-        expect(assigns(:friend_email)).to eq('nelle@example.com')
-      end
-      it 'should assign @inviter' do
-        inviter = Fabricate(:user)
-        get :new, inviter_id: inviter.id
-        expect(assigns(:inviter)).to eq(inviter)
-      end
-      it 'should assign @token' do
-        inviter = Fabricate(:user)
-        get :new, inviter_id: inviter.id
-        expect(assigns(:token)).to eq(inviter.user_tokens.first)
-      end
-
-      it 'should render user/new template' do
-        inviter = Fabricate(:user)
-        get :new, inviter_id: inviter.id
-        expect(response).to render_template 'users/new'
-      end
+    it 'should assign @friend_name' do
+      get :new, friend_name: 'Nelle'
+      expect(assigns(:friend_name)).to eq('Nelle')
+    end
+    it 'should assign @friend_email' do
+      get :new, friend_email: 'nelle@example.com'
+      expect(assigns(:friend_email)).to eq('nelle@example.com')
+    end
+    it 'should assign @token' do
+      get :new, token: 1234
+      expect(assigns(:token)).to eq("1234")
     end
 
-    it_behaves_like 'redirect for unauthenticated user' do
-      let(:action) { get :new }
+    it 'should render user/new template' do
+      inviter = Fabricate(:user)
+      get :new, token: 1234
+      expect(response).to render_template 'users/new'
     end
   end
 end
