bf2bf0aa846e28a927feee32aff056bd8b403b31
Dito <dhartoto@yahoo.com>
2014-07-31 21:06:43 +1000

    Replace @inviter with @token and remove #token_generator

--- app/controllers/invited_registrations_controller.rb
+++ app/controllers/invited_registrations_controller.rb
@@ -1,18 +1,10 @@
 class InvitedRegistrationsController < ApplicationController
-  before_action :require_user 
 
   def new
     @user = User.new
     @friend_name = params[:friend_name]
     @friend_email = params[:friend_email]
-    @inviter = User.find(params[:inviter_id])
-    @token = generate_token(@inviter)
+    @token = params[:token]
     render 'users/new'
-  end
-
-  private
-
-  def generate_token(user)
-    UserToken.create(token: SecureRandom.urlsafe_base64, user: user)
   end
 end
