2b773fe03753a45b41208c790fe83f1348b83d30
Dito <dhartoto@yahoo.com>
2014-07-31 15:56:15 +1000

    Change invite identifier from user_id to user_token

--- app/controllers/invited_registrations_controller.rb
+++ app/controllers/invited_registrations_controller.rb
@@ -1,10 +1,18 @@
 class InvitedRegistrationsController < ApplicationController
+  before_action :require_user 
 
   def new
     @user = User.new
     @friend_name = params[:friend_name]
     @friend_email = params[:friend_email]
-    @inviter_id = params[:inviter_id]
+    @inviter = User.find(params[:inviter_id])
+    @token = generate_token(@inviter)
     render 'users/new'
+  end
+
+  private
+
+  def generate_token(user)
+    UserToken.create(token: SecureRandom.urlsafe_base64, user: user)
   end
 end
