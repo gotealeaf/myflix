851d9378a1ba04bf525810c33a128c03d722f390
Dito <dhartoto@yahoo.com>
2014-08-02 17:48:01 +1000

    Push MyflixMailer to background job #delay

--- app/controllers/forgot_passwords_controller.rb
+++ app/controllers/forgot_passwords_controller.rb
@@ -1,13 +1,13 @@
 class ForgotPasswordsController < ApplicationController
   include Tokenable
-  
+
   def new; end
 
   def create
     @user = User.find_by(email: params[:email])
     if @user
       user_token = generate_token(@user)
-      MyflixMailer.password_reset_email(user_token).deliver
+      MyflixMailer.delay.password_reset_email(user_token.id)
       redirect_to confirm_password_reset_path
     else
       flash[:danger] = params[:email].blank? ? "Email can't be blank": "Email invalid"
