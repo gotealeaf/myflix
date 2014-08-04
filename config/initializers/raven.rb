0b530dcdfe77d9c5fd9831c1301bbcbf4a4171b7
Dito <dhartoto@yahoo.com>
2014-08-04 17:06:42 +1000

    Add require raven/sidekiq

--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -1,4 +1,4 @@
-require 'raven'
+require 'raven/sidekiq'
 
 Raven.configure do |config|
   config.dsn = 'https://15377fda962c46fba061feae971bfb77:fabe1982541f4d049b928bbdb073d6ca@app.getsentry.com/28403'
