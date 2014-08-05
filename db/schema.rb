735bd67c512f333630756d3c2f7a0b19f32f43ad
James Coleman <james.k.coleman@gmail.com>
2014-08-05 10:27:40 -0400

    Implemented watch video and feature test for adding video.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140804182605) do
+ActiveRecord::Schema.define(version: 20140804220527) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -71,6 +71,7 @@ ActiveRecord::Schema.define(version: 20140804182605) do
     t.integer  "category_id"
     t.string   "large_cover"
     t.string   "small_cover"
+    t.string   "video_url"
   end
 
 end
---
+++ spec/features/admin_adds_new_video_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+feature 'Admin adds new video' do
+  scenario 'Admin successfully adds a new video' do
+    admin = Fabricate(:admin)
+    drama = Fabricate(:category, name: "Dramas")
+    sign_in(admin)
+    visit new_admin_video_path
+
+    fill_in "Title", with: "Monk"
+    select "Dramas", from: "Category"
+    fill_in "Description", with: "SF detective"
+    attach_file "Large cover", "spec/support/uploads/monk_large.jpg"
+    attach_file "Small cover", "spec/support/uploads/monk.jpg"
+    fill_in "Video URL", with: "http://www.example.com/my_video.mp4"
+    click_button "Add Video"
+
+    sign_out
+    sign_in
+
+    visit video_path(Video.first)
+    expect(page).to have_selector("img[src='/uploads/monk_large.jpg']")
+    expect(page).to have_selector("a[href='http://www.example.com/my_video.mp4']")
+  end
+end
