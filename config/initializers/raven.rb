98af356fd86ed1a2f04e6c30d3e6aeb5cca387a9
Dito <dhartoto@yahoo.com>
2014-08-05 17:39:45 +1000

    Remove action_dispatch

--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -1,7 +1,6 @@
 require 'raven/sidekiq'
 
 Raven.configure do |config|
-  config.action_dispatch.show_exceptions = false
   config.dsn = ENV['SENTRY_DSN']
   config.current_environment = 'production'
 end
