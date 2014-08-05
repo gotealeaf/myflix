491f34631b1e984b20cdf66ecdd1843518964e30
Dito <dhartoto@yahoo.com>
2014-08-05 17:32:51 +1000

    Add ENV SENTRY_DSN

--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -2,6 +2,6 @@ require 'raven/sidekiq'
 
 Raven.configure do |config|
   config.action_dispatch.show_exceptions = false
-  config.dsn = 'https://15377fda962c46fba061feae971bfb77:fabe1982541f4d049b928bbdb073d6ca@app.getsentry.com/28403'
+  config.dsn = ENV['SENTRY_DSN']
   config.current_environment = 'production'
 end
