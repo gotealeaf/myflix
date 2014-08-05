fdec7c09a665c9d033b3d93e24f3acb0fbd5b45c
Dito <dhartoto@yahoo.com>
2014-08-05 21:09:50 +1000

    add raise delivery_errors false

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,7 +16,7 @@ Myflix::Application.configure do
 
   config.i18n.fallbacks = true
 
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.active_support.deprecation = :notify
 
