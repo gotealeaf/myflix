45acaf093faea526229a1fd9f5c1bde2f44afe86
Dito <dhartoto@yahoo.com>
2014-08-05 15:36:31 +1000

    Change app name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,14 +20,14 @@ Myflix::Application.configure do
 
   config.action_mailer.delivery_method = :smtp
 
-  config.action_mailer.default_url_options = { host: 'myflix-rottenapple.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'myflix-rottenapple-production.herokuapp.com' }
 
   ActionMailer::Base.smtp_settings = {
   :port           => ENV['MAILGUN_SMTP_PORT'],
   :address        => ENV['MAILGUN_SMTP_SERVER'],
   :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
   :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-  :domain         => 'myflix-rottenapple.herokuapp.com',
+  :domain         => 'myflix-rottenapple-production.herokuapp.com',
   :authentication => :plain,
   }
 end
