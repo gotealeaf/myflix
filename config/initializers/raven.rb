8124256363e033bee55d627cb1e49f8540d51231
Dito <dhartoto@yahoo.com>
2014-08-05 17:27:55 +1000

    Remove end

--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -5,4 +5,3 @@ Raven.configure do |config|
   config.dsn = 'https://15377fda962c46fba061feae971bfb77:fabe1982541f4d049b928bbdb073d6ca@app.getsentry.com/28403'
   config.current_environment = 'production'
 end
-end
