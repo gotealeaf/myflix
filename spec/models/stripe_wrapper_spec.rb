91576aa879c45735193e871d7413902631125ca6
James Coleman <james.k.coleman@gmail.com>
2014-08-06 18:16:21 -0400

    Implemented charge credit card on registration

--- spec/models/stripe_wrapper_spec.rb
+++ spec/models/stripe_wrapper_spec.rb
@@ -4,7 +4,6 @@ describe StripeWrapper do
   describe StripeWrapper::Charge do
     describe ".create" do
       it "makes a successful charge", :vcr do
-        Stripe.api_key = ENV['STRIPE_SECRET_KEY']
         token = Stripe::Token.create(
           :card => {
             :number => "4242424242424242",
@@ -20,8 +19,27 @@ describe StripeWrapper do
           description: "a valid charge"
         )
 
-        expect(response.amount).to eq(999)
-        expect(response.currency).to eq('usd')      
+        expect(response).to be_successful   
+      end
+
+      it "makes a card declined charge", :vcr do
+        token = Stripe::Token.create(
+          :card => {
+            :number => "4000000000000002",
+            :exp_month => 8,
+            :exp_year => 2018,
+            :cvc => "314"
+          }
+        ).id
+
+        response = StripeWrapper::Charge.create(
+          amount: 999,
+          card: token,
+          description: "an invalid charge"
+        )
+
+        expect(response).not_to be_successful   
+
       end
     end
   end
