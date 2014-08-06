5c2ff30316eb0c9427ae6bd04be352d0acdcafe5
Reed Nelson-Saunders <reedns@hotmail.com>
2014-08-06 11:09:38 -0700

    configured action mailer for mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,11 +21,11 @@ Myflix::Application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: "http://vast-scrubland-1489.herokuapp.com" }
   config.action_mailer.smtp_settings = {
-    address:        "smtp.gmail.com",
-    port:           587,
+    address:        ENV["MAILGUN_SMTP_SERVER"],
+    port:           ENV["MAILGUN_SMTP_PORT"],
     domain:         "http://vast-scrubland-1489.herokuapp.com/",
-    user_name:      ENV["gmail_username"],
-    password:       ENV["password"],
+    user_name:      ENV["MAILGUN_SMTP_LOGIN"],
+    password:       ENV["MAILGUN_SMTP_PASSWORD"],
     authentication: "plain",
     enable_starttls_auto: true
     }
