14065b756b5f2c7db4127c03e666cb3bf5afc457
Dito <dhartoto@yahoo.com>
2014-08-07 17:20:21 +1000

    Add admin to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140731040850) do
+ActiveRecord::Schema.define(version: 20140807065227) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 20140731040850) do
     t.string   "slug"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "admin",           default: false
   end
 
   create_table "videos", force: true do |t|
