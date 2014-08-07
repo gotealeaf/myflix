88eae58c142d1f5e70ab36d4e9a0fc43543dc326
Rick Heller <rick.heller@yahoo.com>
2014-08-07 14:25:27 -0400

    try alternative route to home

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Myflix::Application.routes.draw do
   get 'ui(/:action)', controller: 'ui'
-  get 'home', controller: 'videos', action: 'index'
+
+#  The following two routes are alternates that send the user home  
+#  get 'home', controller: 'videos', action: 'index'
+  get 'home', to: 'videos#index'
+
+#limit routes to those actions supported by controller
   resources :videos, only: [:index, :show]
   resources :categories, only: :show
 end
