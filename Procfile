d2b0c646704adee34c37c1dddde3a01c08a3aaaa
James Cheuk <jccf091@gmail.com>
2014-08-08 17:26:39 +0800

    solve problem: Sidekiq with a single heroku dyno

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 web: bundle exec unicorn -p $PORT -c ./config/unicorn.rb
-work: bundle exec sidekiq
+worker: bundle exec sidekiq -e production -C config/sidekiq.yml
--- config/unicorn.rb
+++ config/unicorn.rb
@@ -9,6 +9,8 @@ before_fork do |server, worker|
     Process.kill 'QUIT', Process.pid
   end
 
+  @sidekiq_pid ||= spawn("bundle exec sidekiq -c 2")
+
   defined?(ActiveRecord::Base) and
     ActiveRecord::Base.connection.disconnect!
 end
@@ -16,6 +18,14 @@ end
 after_fork do |server, worker|
   Signal.trap 'TERM' do
     puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
+  end
+
+  Sidekiq.configure_client do |config|
+    config.redis = { :size => 1 }
+  end
+
+  Sidekiq.configure_server do |config|
+    config.redis = { :size => 5 }
   end
 
   defined?(ActiveRecord::Base) and
