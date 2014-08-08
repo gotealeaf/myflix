8e434d024f1ed03a2799e7a429da183662c7992a
James Cheuk <jccf091@gmail.com>
2014-08-08 19:25:43 +0800

    add assest images

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -9,7 +9,7 @@ before_fork do |server, worker|
     Process.kill 'QUIT', Process.pid
   end
 
-  @sidekiq_pid ||= spawn("bundle exec sidekiq -c 2")
+  @sidekiq_pid ||= spawn("bundle exec sidekiq -c 1")
 
   defined?(ActiveRecord::Base) and
     ActiveRecord::Base.connection.disconnect!
@@ -21,11 +21,11 @@ after_fork do |server, worker|
   end
 
   Sidekiq.configure_client do |config|
-    config.redis = { :size => 1 }
+    config.redis = { :size => 2 }
   end
 
   Sidekiq.configure_server do |config|
-    config.redis = { :size => 5 }
+    config.redis = { :size => 4 }
   end
 
   defined?(ActiveRecord::Base) and
