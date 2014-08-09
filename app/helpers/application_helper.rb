390ad95cdc21cb34917ed454d6c02acf7fbb87d0
Dito <dhartoto@yahoo.com>
2014-08-10 09:17:59 +1000

    Add #genre_options

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,8 @@ module ApplicationHelper
   def review_options(selected=nil)
     options_for_select([5,4,3,2,1].map { |x| [pluralize(x, "Star"), x] }, selected)
   end
+
+  def genre_options
+    options_for_select(Genre.all.map { |x| [x.name.titleize, x.id] })
+  end
 end
