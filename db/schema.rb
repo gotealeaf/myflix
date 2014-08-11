17491b3b5d4bcfa83ef585a8b8ef0ea182d47be6
Dito <dhartoto@yahoo.com>
2014-08-11 10:34:56 +1000

    Add video_url to videos table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140809231436) do
+ActiveRecord::Schema.define(version: 20140810235525) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,6 +74,7 @@ ActiveRecord::Schema.define(version: 20140809231436) do
     t.string   "small_cover"
     t.string   "large_cover"
     t.integer  "genre_id"
+    t.string   "video_url"
   end
 
 end
