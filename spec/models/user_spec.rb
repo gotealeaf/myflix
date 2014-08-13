ae6bc609494940d650bce381476d83546422c564
Rick Heller <rick.heller@yahoo.com>
2014-08-13 16:09:35 -0400

    fixed rspec, needed to run rake db:reset

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe User do
+  it { should have_secure_password }
   it { should validate_presence_of(:email) }
   it { should validate_presence_of(:full_name) }
-  it { should validate_presence_of(:password_digest) }
 end
