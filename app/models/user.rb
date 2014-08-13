71a869a2cfdad6e72037073d5898c44e87bec681
Rick Heller <rick.heller@yahoo.com>
2014-08-13 15:57:44 -0400

    fix validation of password digest

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   validates :email, presence: true, uniqueness: true
   validates :full_name, presence: true
-  validates :password, presence: true
+  validates :password_digest, presence: true
 
   has_secure_password
 end
