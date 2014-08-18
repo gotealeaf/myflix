e18791ab5ecb336f29056632fafe489dc47b11a8
aj <ajjonez@icloud.com>
2014-08-17 21:28:19 -0500

    review error issues

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,7 @@
 class ReviewsController < ApplicationController
   def create
-    video = Video.find(params[:video_id])
-    video.reviews.create(review_params[:review].merge!(user: current_user))
+    @video = Video.find(params[:video_id])
+    @review = @video.reviews.build(review_params.merge!(user: current_user))
     redirect_to video
   end
 
