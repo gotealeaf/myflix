1a5897713cc6e1b2b828c1f3cb302fa9df509635
Dito <dhartoto@yahoo.com>
2014-08-19 08:59:50 +1000

    Add .set_stripe_sec_key

--- app/models/stripe_wrapper.rb
+++ app/models/stripe_wrapper.rb
@@ -1,12 +1,21 @@
 module StripeWrapper
   class Charge
     def self.create(options={})
+      set_stripe_sec_key
       Stripe::Charge.create(
         amount: options[:amount],
         currency: 'aud',
         card: options[:card],
         description: options[:description]
       )
+    end
+
+    def self.set_stripe_sec_key
+      if Rails.env.development? || Rails.env.test?
+        Stripe.api_key = Rails.application.secrets.stripe_sec_key
+      else
+        Stripe.api_key = ENV['STRIPE_SECRET_KEY']
+      end
     end
   end
 end
