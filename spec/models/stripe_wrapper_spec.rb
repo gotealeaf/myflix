dcea5994859c00059bc7be0f547297c0a6b75f3c
Dito <dhartoto@yahoo.com>
2014-08-19 08:56:08 +1000

    Add context for valid inputs

--- spec/models/stripe_wrapper_spec.rb
+++ spec/models/stripe_wrapper_spec.rb
@@ -3,14 +3,16 @@ require 'rails_helper'
 describe StripeWrapper do
   describe StripeWrapper::Charge do
     describe '.create' do
-      it 'creates a charge', :vcr do
-        response = StripeWrapper::Charge.create(
-          amount: 999,
-          card: stripe_valid_token.id,
-          description: "A valid charge"
-        )
-        expect(response.amount).to eq(999)
-        expect(response.currency).to eq('aud')
+      context 'if card is accepted' do
+        it 'creates a charge', :vcr do
+          response = StripeWrapper::Charge.create(
+            amount: 999,
+            card: stripe_valid_token.id,
+            description: "A valid charge"
+          )
+          expect(response.amount).to eq(999)
+          expect(response.currency).to eq('aud')
+        end
       end
     end
   end
