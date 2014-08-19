23dcf1f1cba8aee6ed7141346d59395acfb05d57
Dito <dhartoto@yahoo.com>
2014-08-19 14:24:33 +1000

    Add response and rescue

--- app/models/stripe_wrapper.rb
+++ app/models/stripe_wrapper.rb
@@ -1,13 +1,30 @@
 module StripeWrapper
   class Charge
+
+    attr_reader :response, :error_message
+
+    def initialize(options={})
+      @response = options[:response]
+      @error_message = options[:error_message]
+    end
+
     def self.create(options={})
       set_stripe_sec_key
-      Stripe::Charge.create(
-        amount: options[:amount],
-        currency: 'aud',
-        card: options[:card],
-        description: options[:description]
-      )
+      begin
+        response = Stripe::Charge.create(
+          amount: options[:amount],
+          currency: 'aud',
+          card: options[:card],
+          description: options[:description]
+        )
+        new(response: response)
+      rescue Stripe::CardError => e
+        new(error_message: e.message)
+      end
+    end
+
+    def successful?
+      response.present?
     end
 
     def self.set_stripe_sec_key
