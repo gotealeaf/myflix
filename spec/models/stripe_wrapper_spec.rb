b392c56bcd1d42fc433962067662283329bccd12
Dito <dhartoto@yahoo.com>
2014-08-19 14:25:31 +1000

    Add card validation to spec

--- spec/models/stripe_wrapper_spec.rb
+++ spec/models/stripe_wrapper_spec.rb
@@ -3,16 +3,31 @@ require 'rails_helper'
 describe StripeWrapper do
   describe StripeWrapper::Charge do
     describe '.create' do
-      context 'if card is accepted' do
-        it 'creates a charge', :vcr do
-          response = StripeWrapper::Charge.create(
-            amount: 999,
-            card: stripe_valid_token.id,
-            description: "A valid charge"
-          )
-          expect(response.amount).to eq(999)
-          expect(response.currency).to eq('aud')
-        end
+      it 'creates a charge when card is valid', :vcr do
+        response = StripeWrapper::Charge.create(
+          amount: 999,
+          card: stripe_valid_token.id,
+          description: "A valid charge"
+        )
+        expect(response.successful?).to be_truthy
+      end
+
+      it 'does not create a charge when card is not valid', :vcr do
+        response = StripeWrapper::Charge.create(
+          amount: 999,
+          card: stripe_invalid_token.id,
+          description: "An invalid charge"
+        )
+        expect(response.successful?).to be_falsey
+      end
+
+      it 'returns the error message for card error', :vcr do
+        response = StripeWrapper::Charge.create(
+          amount: 999,
+          card: stripe_invalid_token.id,
+          description: "An invalid charge"
+        )
+        expect(response.error_message).to eq("Your card was declined.")
       end
     end
   end
