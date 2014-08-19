acf3f311ab39d68a95fe5c451e70a4b39bb5c66c
Dito <dhartoto@yahoo.com>
2014-08-19 11:12:54 +1000

    Add card details

--- spec/features/invitation_spec.rb
+++ spec/features/invitation_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 feature 'User invites a friend to MyFlix' do
-  scenario 'a friend registers after an invitation from a current user' do
+  scenario 'a friend registers after an invitation from a current user', { js: true, vcr: true } do
     inviter = Fabricate(:user)
     visit sign_in_path
     user_signs_in(inviter)
@@ -18,8 +18,11 @@ feature 'User invites a friend to MyFlix' do
     fill_in 'user_username', with: 'unicorn'
     fill_in 'user_password', with: 'password'
     fill_in 'user_password_confirmation', with: 'password'
+    fill_in 'card_number', with: '4242424242424242'
+    fill_in 'card_cvc', with: '123'
+    select('2016', from: 'card-expiry-year')
     click_on 'Sign Up'
-    expect(page).to have_content("Welcome Nelle!")
+    expect(page).to have_content("Welcome, Nelle")
 
     click_on 'People'
     expect(page).to have_content(inviter.full_name)
