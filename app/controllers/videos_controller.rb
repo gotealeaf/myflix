45f1b19d00559fd87afe9af018d5fe732c8c48be
Dito <dhartoto@yahoo.com>
2014-08-20 15:46:43 +1000

    remove #set_video

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,6 +1,5 @@
 class VideosController < ApplicationController
 
-  before_action :set_video, except: [:index, :new, :search]
   before_action :require_user
 
   def index
@@ -8,17 +7,12 @@ class VideosController < ApplicationController
   end
 
   def show
+    @video = Video.find(params[:id])
     @review = Review.new
     @reviews = @video.reviews
   end
 
   def search
     @results = Video.search_by_name(params[:search_name])
-  end
-
-  private
-
-  def set_video
-    @video = Video.find(params[:id])
   end
 end
