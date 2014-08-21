86a4e38c8d633656b83d2f38c5135ee330cc6146
aj <ajjonez@icloud.com>
2014-08-20 22:08:46 -0500

    finished review spec

--- spec/controllers/reviews_controller_spec.rb
+++ spec/controllers/reviews_controller_spec.rb
@@ -2,54 +2,48 @@ require "rails_helper"
 
 describe ReviewsController do
   describe "POST create" do
+    let (:video) { Fabricate(:video) }
+
     context "with authenticated user" do
 
       let (:current_user) { Fabricate(:user) }
       before { session[:user_id] = current_user.id }
 
       context "with valid input" do
-        it "redirects to the video show page" do 
-          video = Fabricate(:video)
+        before do
           post :create, review: Fabricate.attributes_for(:review), video_id: video.id
+        end
+        it "redirects to the video show page" do 
           expect(response).to redirect_to video
         end
         it "creates a review" do
-          video = Fabricate(:video)
-          post :create, review: Fabricate.attributes_for(:review), video_id: video.id
           expect(Review.count).to eq(1)
         end
         it "it creates a review associated with video" do
-          video = Fabricate(:video)
-          post :create, review: Fabricate.attributes_for(:review), video_id: video.id
           expect(Review.first.video).to eq(video)
         end
         it "it creates a review associated with the signed in user" do
-          video = Fabricate(:video)
-          post :create, review: Fabricate.attributes_for(:review), video_id: video.id
           expect(Review.first.user).to eq(current_user)
         end
       end
       context "with invalid input"
+      
       it "does not create a review" do
-        video = Fabricate(:video)
         post :create, review: {rating: 4}, video_id: video.id
         expect(Review.count).to eq(0)
       end
 
       it "renders the videos/show template" do
-        video = Fabricate(:video)
         post :create, review: {rating: 4}, video_id: video.id
         expect(response).to render_template 'videos/show'
       end
 
       it "sets @video" do
-        video = Fabricate(:video)
         post :create, review: {rating: 4}, video_id: video.id
         expect(assigns(:video)).to eq(video)
       end
 
       it "sets @review" do
-        video = Fabricate(:video)
         review = Fabricate(:review, video: video)
         post :create, review: {rating: 4}, video_id: video.id
         expect(assigns(:reviews)).to match_array([review])
