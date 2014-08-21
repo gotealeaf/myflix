843386cacfe9c6e226208aadfb7e11e4b2c9caa4
Reed Nelson-Saunders <reedns@hotmail.com>
2014-08-21 10:50:30 -0700

    Fixed seed data and uploaders

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,18 +5,57 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
 comedy = Category.create(name: "TV Comedies")
 drama = Category.create(name: "Dramas")
 
-Video.create(title: "Monk", description: "A dective with OCD.", small_cover: "/tmp/monk.jpg", large_cover: "/tmp/monk_large.jpg", category: drama)
-Video.create(title: "Family Guy", description: "Cartoon comedy about zany family.", small_cover: "/tmp/family_guy.jpg", large_cover: "/tmp/family_guy.jpg", category: comedy)
-Video.create(title: "Futurama", description: "From the creator of the Simpsons.", small_cover: "/tmp/futurama.jpg", large_cover: "/tmp/futurama.jpg", category: comedy)
-Video.create(title: "South Park", description: "An irreverent cartoon comedy starring 4 boys.", small_cover: "/tmp/south_park.jpg", large_cover: "/tmp/south_park.jpg", category: comedy)
-Video.create(title: "Monk", description: "A dective with OCD.", small_cover: "/tmp/monk.jpg", large_cover: "/tmp/monk_large.jpg", category: drama)
-Video.create(title: "Family Guy", description: "Cartoon comedy about zany family.", small_cover: "/tmp/family_guy.jpg", large_cover: "/tmp/family_guy.jpg", category: comedy)
-Video.create(title: "Futurama", description: "From the creator of the Simpsons.", small_cover: "/tmp/futurama.jpg", large_cover: "/tmp/futurama.jpg", category: comedy)
-Video.create(title: "South Park", description: "An irreverent cartoon comedy starring 4 boys.", small_cover: "/tmp/south_park.jpg", large_cover: "/tmp/south_park.jpg", category: comedy)
-Video.create(title: "Monk", description: "A dective with OCD.", small_cover: "/tmp/monk.jpg", large_cover: "/tmp/monk_large.jpg", category: comedy)
+Video.create(title: "Monk", 
+  description: "A dective with OCD.", 
+  small_cover: File.open(File.join(Rails.root, "/public/tmp/monk.jpg")), 
+  large_cover: File.open(File.join(Rails.root, "/public/tmp/monk_large.jpg")), 
+  category_id: 2)
+
+Video.create(title: "Family Guy", 
+  description: "Cartoon comedy about zany family.", 
+  small_cover: File.open(File.join(Rails.root, "/public/tmp/family_guy.jpg")), 
+  large_cover: File.open(File.join(Rails.root, "public/tmp/family_guy.jpg")), 
+  category_id: 1)
+
+Video.create(title: "Futurama", 
+  description: "From the creator of the Simpsons.", 
+  small_cover: File.open(File.join(Rails.root, "/public/tmp/futurama.jpg")), 
+  large_cover: File.open(File.join(Rails.root, "public/tmp/futurama.jpg")), 
+  category_id: 1)
+
+Video.create(title: "South Park", 
+  description: "An irreverent cartoon comedy starring 4 boys.", 
+  small_cover: File.open(File.join(Rails.root, "/public/tmp/south_park.jpg")), 
+  large_cover: File.open(File.join(Rails.root, "public/tmp/south_park.jpg")), 
+  category_id: 1)
+Video.create(title: "Monk", 
+  description: "A dective with OCD.", 
+  small_cover: File.open(File.join(Rails.root, "/public/tmp/monk.jpg")), 
+  large_cover: File.open(File.join(Rails.root, "/public/tmp/monk_large.jpg")), 
+  category_id: 2)
+
+Video.create(title: "Family Guy", 
+  description: "Cartoon comedy about zany family.", 
+  small_cover: File.open(File.join(Rails.root, "/public/tmp/family_guy.jpg")), 
+  large_cover: File.open(File.join(Rails.root, "public/tmp/family_guy.jpg")), 
+  category_id: 1)
+
+Video.create(title: "Futurama", 
+  description: "From the creator of the Simpsons.", 
+  small_cover: File.open(File.join(Rails.root, "/public/tmp/futurama.jpg")), 
+  large_cover: File.open(File.join(Rails.root, "public/tmp/futurama.jpg")), 
+  category_id: 1)
+
+Video.create(title: "South Park", 
+  description: "An irreverent cartoon comedy starring 4 boys.", 
+  small_cover: File.open(File.join(Rails.root, "/public/tmp/south_park.jpg")), 
+  large_cover: File.open(File.join(Rails.root, "public/tmp/south_park.jpg")), 
+  category_id: 1)
+
 
 jim = User.create(fullname: "Jim Bobby", email: "cool@cool.com", password: "password", admin: true)
 jamie = User.create(fullname: "Jamie Bobber", email: "jb@jb.net", password: "password")
