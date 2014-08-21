e4896644ac9609f0aa9910cf33c38c57e55fb118
Logan Rice <loganrice72@gmail.com>
2014-08-21 08:42:08 -0700

    added video search

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,3 +1,10 @@
 class Video < ActiveRecord::Base
   belongs_to :category
+
+  validates_presence_of :title, :description
+
+  def self.search_by_title(key_word)
+    return [] if key_word.blank?
+    where("title LIKE ?", "%#{key_word}%").order("created_at DESC")
+  end
 end
