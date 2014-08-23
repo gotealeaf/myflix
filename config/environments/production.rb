53a51cbe3ba4c375b7e5f225ec32a34615799aff
Josh Hunter <thatsmegettinmail@yahoo.com>
2014-08-22 20:54:05 -0500

    added_email_settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,10 +22,10 @@ Myflix::Application.configure do
   config.action_mailer.smtp_settings = {
     :address              =>"smtp.gmail.com",
     :port                 => 587,
-    :domain               => 'baci.lindsaar.net'
-    :user_name            => ENV['gmail_username']
-    :password             => ENV['gmail_password']
-    :authentication       => 'plain'
+    :domain               => 'baci.lindsaar.net',
+    :user_name            => ENV['gmail_username'],
+    :password             => ENV['gmail_password'],
+    :authentication       => 'plain',
     :enable_starttls_auto => true }
   }
 end
