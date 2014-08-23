572fc5089667519bddd49b5fbe17a847eefcdc95
Josh Hunter <thatsmegettinmail@yahoo.com>
2014-08-22 20:13:06 -0500

    email

--- spec/support/macros.rb
+++ spec/support/macros.rb
@@ -9,3 +9,7 @@ def sign_in(a_user=nil)
   fill_in "Password", with: user.password
   click_button "Sign in"
 end
+
+def click_on_video_on_home_page(video)
+  find("a[href='/videos/#{video.id}']").click
+end
