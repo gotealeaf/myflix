60afbf65981e239805a6d3e845df6a5378990da2
Josh Hunter <thatsmegettinmail@yahoo.com>
2014-08-22 21:04:16 -0500

    added_email_settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,7 +18,7 @@ Myflix::Application.configure do
 
   config.active_support.deprecation = :notify
 
-  config.action_mailer.delivery_method = :setup
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address              =>"smtp.gmail.com",
     :port                 => 587,
@@ -27,5 +27,5 @@ Myflix::Application.configure do
     :password             => ENV['gmail_password'],
     :authentication       => 'plain',
     :enable_starttls_auto => true }
-  
+
 end
