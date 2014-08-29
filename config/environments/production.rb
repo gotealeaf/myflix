de0977d4af35128b638668c3d367d0269c36a14d
Josh Hunter <thatsmegettinmail@yahoo.com>
2014-08-29 15:04:16 -0500

    added_mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,14 +18,17 @@ Myflix::Application.configure do
 
   config.active_support.deprecation = :notify
 
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    :address              =>"smtp.gmail.com",
-    :port                 => 587,
-    :domain               => 'baci.lindsaar.net',
-    :user_name            => ENV['gmail_username'],
-    :password             => ENV['gmail_password'],
-    :authentication       => 'plain',
-    :enable_starttls_auto => true }
+  ActionMailer::Base.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => 'jhunter-myflix.heroku.com' }
+
+  ActionMailer::Base.smtp_settings = {
+      :port           => ENV['MAILGUN_SMTP_PORT'],
+      :address        => ENV['MAILGUN_SMTP_SERVER'],
+      :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+      :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+      :domain         => 'yourapp.heroku.com',
+      :authentication => :plain,
+  }
+
 
 end
