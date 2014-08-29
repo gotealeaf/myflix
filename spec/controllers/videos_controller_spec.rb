836e9b0faed5edcc5d7787f3c76642585ad75b35
Rick Heller <rick.heller@yahoo.com>
2014-08-29 13:14:30 -0400

    get rspec of show to work

--- spec/controllers/videos_controller_spec.rb
+++ spec/controllers/videos_controller_spec.rb
@@ -2,18 +2,18 @@ require 'spec_helper'
 
 describe VideosController do
 
-  describe 'GET index' do
     before do
       @cartoon = Category.create(name: 'Cartoon')
       @mystery = Category.create(name: 'Mystery')
-      monk = Video.create(title: 'Monk', description: "clever TV show", category: @mystery)
-      conk = Video.create(title: 'Conk', description: "A very conky TV show", category: @mystery)
-      fut = Video.create(title: 'Future Family', description: "Family guy in the future",  category: @cartoon)
+      @monk = Video.create(title: 'Monk', description: "clever TV show", category: @mystery)
+      @conk = Video.create(title: 'Conk', description: "A very conky TV show", category: @mystery)
+      @fut = Video.create(title: 'Future Family', description: "Family guy in the future",  category: @cartoon)
     end
   
+  describe 'GET index' do
     it "prepares the data" do
       get :index
-#puts "rick assigns  is " + @assigns.inspect
+#index only prepares the category. Videos are gotten in the view
       assigns(:categories).should == [@cartoon,@mystery]
     end
 
@@ -24,15 +24,22 @@ describe VideosController do
 
   end
 
+  describe 'GET show' do
+
+    it "prepares the data" do
+      get :show, id: 1
+puts "rick assigns  is " + @assigns.inspect
+      assigns(:video).should == @monk
+    end
+
+    it "renders the template" do
+      get :show, id: 1
+      response.should render_template :show
+    end
+
+  end
 
   describe 'GET search' do
-    before do
-      @cartoon = Category.create(name: 'Cartoon')
-      @mystery = Category.create(name: 'Mystery')
-      @monk = Video.create(title: 'Monk', description: "clever TV show", category: @mystery)
-      conk = Video.create(title: 'Conk', description: "A very conky TV show", category: @mystery)
-      fut = Video.create(title: 'Future Family', description: "Family guy in the future",  category: @cartoon)
-    end
 
     it "prepares the data" do
       get :search, search_term: "Monk"
@@ -45,5 +52,6 @@ describe VideosController do
     end
 
   end
+
 
 end
