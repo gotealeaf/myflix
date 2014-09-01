762fa320cfc9cc4c61907dd5259b5684a6121312
Dito <dhartoto@yahoo.com>
2014-09-01 19:48:04 +0700

    Add status to users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140829045825) do
+ActiveRecord::Schema.define(version: 20140901111420) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,6 +68,7 @@ ActiveRecord::Schema.define(version: 20140829045825) do
     t.datetime "updated_at"
     t.boolean  "admin",           default: false
     t.string   "stripe_id"
+    t.string   "status",          default: "active"
   end
 
   create_table "videos", force: true do |t|
