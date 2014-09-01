53d31cc97a3e5c5e192c3131fc22cd77ef373fc1
Dito <dhartoto@yahoo.com>
2014-09-01 19:52:06 +0700

    Changed context to describe

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,7 +13,7 @@ describe User do
   it { should ensure_length_of(:password).is_at_least(8)}
   it { should validate_uniqueness_of :email }
 
-  context '#video_in_queue?' do
+  describe '#video_in_queue?' do
     it 'returns true if the video is in users queue_videos' do
       Fabricate(:queue_video, video: video, user: user)
       expect(user.video_in_queue?(video)).to be true
@@ -26,7 +26,7 @@ describe User do
     end
   end
 
-  context '#count_queue_videos' do
+  describe '#count_queue_videos' do
     it 'returns 0 if there are no videos in the users queue' do
       expect(user.count_queue_videos).to eq(0)
     end
@@ -37,7 +37,7 @@ describe User do
     end
   end
 
-  context '#count_reviews' do
+  describe '#count_reviews' do
     it 'returns 0 if user has not posted any reviews' do
       expect(user.count_reviews).to eq(0)
     end
@@ -47,6 +47,14 @@ describe User do
       Fabricate(:review, user: user, video: video1)
       Fabricate(:review, user: user, video: video2)
       expect(user.count_reviews).to eq(2)
+    end
+  end
+
+  describe '#deactivate!' do
+    it 'sets the user status to inactive' do
+      nelle = Fabricate(:user)
+      nelle.deactivate!
+      expect(nelle).not_to be_active
     end
   end
 end
