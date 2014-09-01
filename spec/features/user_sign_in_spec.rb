e9022f0416bf00a82f5fa13c071793b4f6905ba4
Dito <dhartoto@yahoo.com>
2014-09-01 19:51:25 +0700

    Add user suspended sign in

--- spec/features/user_sign_in_spec.rb
+++ spec/features/user_sign_in_spec.rb
@@ -8,7 +8,13 @@ feature "Signing in" do
 
   scenario 'with unregistered user' do
     user_signs_in
-    click_button 'Sign in'
     expect(page).to have_content('Incorrect email or password. Please try again.')
+  end
+
+  scenario 'with deactivated user' do
+    user = Fabricate(:user, status: 'inactive')
+    user_signs_in(user)
+    expect(page).not_to have_content("Welcome #{ user.username }")
+    expect(page).to have_content('Your account has been suspended.')
   end
 end
