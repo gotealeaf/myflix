14ec014746fc9c76465e2439d7de8f64da7e3043
Rick Heller <rick.heller@yahoo.com>
2014-09-02 14:39:34 -0400

    small changes to tests to conform to solutions video

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -5,37 +5,46 @@ describe UsersController do
   describe 'GET new' do
         it "generates a new record" do
           get :new
-          assigns(:user).should be_new_record
+          assigns(:user).should be_instance_of(User)
         end
   end
 
   context "the user sign up is valid" do
+ 
+    before do
+      post :create, user: Fabricate.attributes_for(:user)
+    end
 
       describe 'POST create' do
         it "generates a user from valid data" do
-          post :create, user: {email: "rick.heller@yahoo.com", password: "password1", full_name: "Rick Heller"}
-          assigns(:user).should be_valid
+          User.count.should == 1
         end
 
-        it "renders redirect to sign_in" do
-          post :create, user: {email: "rick.heller@yahoo.com", password: "password1", full_name: "Rick Heller"}
+        it "redirects to sign_in" do
           response.should redirect_to sign_in_path
         end
       end
   end
 
   context "the user sign up is INVALID" do
-      describe 'POST create' do
+
+    before do
+      post :create, user: {email: "", password: "", full_name: ""}
+    end
+
+    describe 'POST create' do
         it "generates a user from valid data" do
-          post :create, user: {email: "", password: "", full_name: ""}
-          assigns(:user).should be_new_record
+          User.count.should == 0
         end
 
         it "renders redirect to sign_in" do
-          post :create, user: {email: "", password: "", full_name: ""}
           response.should render_template :new
         end
-      end
+
+        it "regenerates a user record for another try" do
+          assigns(:user).should be_instance_of(User)
+        end
+    end
   end
 
 end
