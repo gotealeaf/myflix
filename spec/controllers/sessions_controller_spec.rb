74c90356f14781eb4e923609df15cf416aee3ad4
Rick Heller <rick.heller@yahoo.com>
2014-09-02 13:06:20 -0400

    add tests for users and sessions controller

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -2,4 +2,38 @@ require 'spec_helper'
 
 describe SessionsController do
 
+  context "sign in is valid" do
+
+    before do
+      @user = Fabricate(:user)
+    end
+
+    describe 'POST create' do
+      it "signs in a user from valid data" do
+        post :create, email: @user.email, password: @user.password
+        session[:user_id].should == @user.id
+      end
+
+      it "redirects to home" do
+        post :create, email: @user.email, password: @user.password
+        response.should redirect_to home_path
+      end
+    end
+  end
+
+  context "sign in is INVALID" do
+
+    describe 'POST create' do
+      it "generates NO session from INVALID data" do
+        post :create, email: "", password: ""
+        session[:user_id].should be_blank
+      end
+
+      it "redirects to home" do
+        post :create, email: "", password: ""
+        response.should redirect_to sign_in_path
+      end
+    end
+  end
+
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,4 +2,33 @@ require 'spec_helper'
 
 describe UsersController do
 
+  context "the user sign up is valid" do
+
+      describe 'POST create' do
+        it "generates a user from valid data" do
+          post :create, user: {email: "rick.heller@yahoo.com", password: "password1", full_name: "Rick Heller"}
+          assigns(:user).should be_valid
+        end
+
+        it "renders redirect to sign_in" do
+          post :create, user: {email: "rick.heller@yahoo.com", password: "password1", full_name: "Rick Heller"}
+          response.should redirect_to sign_in_path
+        end
+      end
+  end
+
+  context "the user sign up is INVALID" do
+      describe 'POST create' do
+        it "generates a user from valid data" do
+          post :create, user: {email: "", password: "", full_name: ""}
+          assigns(:user).should be_new_record
+        end
+
+        it "renders redirect to sign_in" do
+          post :create, user: {email: "", password: "", full_name: ""}
+          response.should render_template :new
+        end
+      end
+  end
+
 end
