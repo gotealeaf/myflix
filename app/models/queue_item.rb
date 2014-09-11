d8ce7548f67e54b0e712a50651eec425dc465baf
Rick Heller <rick.heller@yahoo.com>
2014-09-11 16:44:47 -0400

    add reviews from queue without validation

--- app/models/queue_item.rb
+++ app/models/queue_item.rb
@@ -4,22 +4,25 @@ class QueueItem < ActiveRecord::Base
    validates_numericality_of :position, :greater_than => 0, :only_integer => :true
 
   def rating
-    reviews = Review.where(user_id: user_id, video_id: video_id)
-    reviews.count > 0 ? reviews.first.rating : nil
+    review.present? ? review.rating : nil
   end
 
 def rating= new_rating
-    review = Review.where(user_id: user_id, video_id: video_id).first
+    
     if review.present?
       #bypass validation
        review.update_column(:rating, new_rating)
     else
-       review = Review.create(user_id: user_id, video_id: video_id, rating:new_rating)
-       review.save(validate: false)
+       new_review = Review.new(user_id: user_id, video_id: video_id, rating:new_rating)
+       new_review.save(validate: false)
     end
 
   end
 
+  def review
+    #instance variable to prevent going to db again
+    @review ||= Review.where(user_id: user_id, video_id: video_id).first
+  end
 
 
 end
