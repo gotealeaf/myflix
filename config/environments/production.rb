607c52c96de77749507762de3facf2d3f05e9e9d
Logan Rice <loganrice72@gmail.com>
2014-09-11 10:51:39 -0700

    set up send grid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,11 +21,11 @@ Myflix::Application.configure do
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
+    address:              'smtp.sendgrid.net',
     port:                 587,
-    domain:               'example.com',
-    user_name:            ENV['gmail_username'],
-    password:             ENV['gmail_password'],
     authentication:       'plain',
+    user_name:            ENV['SENDGRID_USERNAME'],
+    password:             ENV['SENDGRID_PASSWORD'],
+    domain:               'heroku.com',
     enable_starttls_auto: true  }
 end
