edb535c195b4972bcbc98de8d2e4ae6d55da1e7e
Logan Rice <loganrice72@gmail.com>
2014-09-17 14:43:39 -0700

    forgot email w feature test

--- spec/features/user_signs_in_spec.rb
+++ spec/features/user_signs_in_spec.rb
@@ -3,10 +3,8 @@ require 'spec_helper'
 feature "User signs in" do 
   
   scenario "with valid email and password" do
-    require 'pry';
     karen = Fabricate(:user)
     sign_in(karen)
-   
     page.should have_content karen.full_name
   end
 end
