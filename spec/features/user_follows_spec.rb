e192d8dff32feb0f7c588f00f8fec1a1354f8cb7
Rick Heller <rick.heller@yahoo.com>
2014-09-19 17:31:02 -0400

    added feature spec for following a user

--- spec/features/user_follows_spec.rb
+++ spec/features/user_follows_spec.rb
@@ -1,25 +1,36 @@
 require 'spec_helper'
 
 
-feature 'user follows another user' do
+feature 'user follows and unfollows another user' do
 
-  background do
-    @hank = Fabricate(:user)
-    @joe = Fabricate(:user)
-  end
+
 
   scenario 'user follows another user' do
-    sign_in(@hank)
-    visit "users/#{@joe.id}"
+    joe = Fabricate(:user)
+    hank = Fabricate(:user)
+    mystery = Fabricate(:category)
+    video1 = Fabricate(:video, category: mystery)
+    review1 = Fabricate(:review, video: video1, user: joe)
+    sign_in(hank)
+    click_on_video_on_home_page(video1)
+    page.should have_content video1.title
+
+    click_link(joe.full_name)
+ 
     page.should have_content 'Follow'
 
     click_link('Follow')
     page.should have_content "People I Follow"
+    page.should have_content joe.full_name
 
-
-    visit "users/#{@joe.id}"
-    page.should_not have_content 'Follow'
+    unfollow_user
+    expect(page).to_not have_content joe.full_name
 
   end
+
+def unfollow_user
+    find("a[data-method='delete']").click
+end
+
 
 end
