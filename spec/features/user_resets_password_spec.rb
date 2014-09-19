42f5e8b137721aeafdd00dd60c688181e6451058
Logan Rice <loganrice72@gmail.com>
2014-09-19 08:35:08 -0700

    reset password feature spec

--- spec/features/user_resets_password_spec.rb
+++ spec/features/user_resets_password_spec.rb
@@ -2,28 +2,30 @@ require 'spec_helper'
 
 feature "user resets password" do
   scenario "current user resets password, gets email, and logs in with new password" do
-    clear_emails
-    karen = Fabricate(:user)
+    karen = Fabricate(:user, password: "old_password")
     visit sign_in_path
-    page.should have_content("Sign in")
-    click_link "Forgot Password?"
-    fill_in "Email Address", with: karen.email
-    click_button "Send Email"
-    page.should have_content("We have sent an email with instructions to reset your password.")
-    open_email(karen.email)
-    current_email.click_link 'Reset My Password'
+    clear_emails
 
+    reset_password(karen)
     set_new_password("new_password")
 
     fill_in "Email Address", with: karen.email
     fill_in "Password", with: "new_password"
     click_button "Sign in"
     page.should have_content(karen.full_name)
+  end
 
+  def reset_password(user)
+    click_link "Forgot Password?"
+    fill_in "Email Address", with: user.email
+    click_button "Send Email"
+    open_email(user.email)
+    current_email.click_link 'Reset My Password'
   end
 
   def set_new_password(password)
     fill_in "New Password", with: password
     click_button "Reset Password"
   end
+
 end
