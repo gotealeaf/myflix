50babefd1a9014aa2e028eed1bbfffc01406bbd3
Logan Rice <loganrice72@gmail.com>
2014-09-22 17:18:37 -0700

    using mail gun instead of send grid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,11 +21,10 @@ Myflix::Application.configure do
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
-    address:              'smtp.sendgrid.net',
-    port:                 587,
-    authentication:       'plain',
-    user_name:            ENV['SENDGRID_USERNAME'],
-    password:             ENV['SENDGRID_PASSWORD'],
-    domain:               'heroku.com',
-    enable_starttls_auto: true  }
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'vast-savannah-3870.herokuapp.com',
+    :authentication => :plain }
 end
