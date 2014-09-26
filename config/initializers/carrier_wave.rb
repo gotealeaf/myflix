c58abc22eab69fa59a37aa6d6d8046e3aa723960
Logan Rice <loganrice72@gmail.com>
2014-09-25 20:03:01 -0700

    set ENV vars for S3

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -2,8 +2,8 @@ CarrierWave.configure do |config|
   if Rails.env.staging? || Rails.env.production?
     config.fog_credentials = {
       :provider               => 'AWS',                        # required
-      :aws_access_key_id      => 'secrectkey',                        # required
-      :aws_secret_access_key  => 'secrectkey',                        # required
+      :aws_access_key_id      => ENV['aws_access_key_id'],                        # required
+      :aws_secret_access_key  => ENV['aws_secret_access_key'],                        # required
     }
     config.fog_directory  = 'name_of_directory'                     # required
   else
