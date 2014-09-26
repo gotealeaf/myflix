21ac9d48ce8b8308fecceeada09b58dbe509c05f
Rick Heller <rick.heller@yahoo.com>
2014-09-26 14:13:53 -0400

    refactor

--- spec/features/user_invites_friend_spec.rb
+++ spec/features/user_invites_friend_spec.rb
@@ -10,8 +10,26 @@ feature 'user invites friend' do
 
   scenario 'user enters a valid email address' do
     joe = Fabricate(:user)
+
     sign_in(joe)
-    click_link "Welcome, " + joe.full_name
+    invite_a_friend(joe)
+    sign_out
+
+    friend_accepts_invite
+    friend_signs_in    
+    click_link('People')
+    expect(page).to have_content joe.full_name
+    sign_out
+
+    sign_in(joe)
+    click_link('People')
+    expect(page).to have_content "Jamie Smil"
+    
+    clear_email
+  end
+#########################################
+  def invite_a_friend inviter
+    click_link "Welcome, " + inviter.full_name
     click_link('Invite A Friend')
     expect(page).to have_content "Friend's Name"
 
@@ -19,38 +37,23 @@ feature 'user invites friend' do
     fill_in "Friend's Email Address", with: "seeingtheroses@gmail.com"
     fill_in "Invitation Message", with: "Hi dere"
     click_button "Send Invitation"
-    sign_out
+  end
 
+  def friend_accepts_invite
     open_email('rick.heller@yahoo.com')
     current_email.click_link 'Register'
-
     expect(page).to have_content 'Password'
     fill_in "Password", with: 'gonzo'
     fill_in "Full Name", with: "Jamie Smil"
     click_button "Sign Up"
     expect(page).to have_content 'Sign In'
-    
+  end
+
+  def friend_signs_in
     fill_in "email", with: 'seeingtheroses@gmail.com'
     fill_in "password", with: 'gonzo'
     click_button "Sign In"
     expect(page).to have_content "Welcome, " + "Jamie Smil"
-
-    click_link('People')
-    expect(page).to have_content joe.full_name
-    sign_out
-
-    click_link('Sign In')
-
-    fill_in "email", with: joe.email
-    fill_in "password", with: joe.password
-    click_button "Sign In"
-    expect(page).to have_content "Welcome, " + joe.full_name
-
-    click_link('People')
-    expect(page).to have_content "Jamie Smil"
-    
-    clear_email
   end
-
 
 end
