796c198f62a697d2c9d599fbbf8fc3ece63212db
Rick Heller <rick.heller@yahoo.com>
2014-09-29 17:14:51 -0400

    changed prod to use mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,15 +20,19 @@ Myflix::Application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: "gmail.com",
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"]
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'thawing-mountain-9297.herokuapp.com',
+    :authentication => :plain,
+    # address: "smtp.gmail.com",
+    # port: 587,
+    # domain: "gmail.com",
+    # authentication: "plain",
+    # enable_starttls_auto: true,
+    # user_name: ENV["GMAIL_USERNAME"],
+    # password: ENV["GMAIL_PASSWORD"]
   }
-
-
 
 end
