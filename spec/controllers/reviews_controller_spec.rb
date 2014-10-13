723a5524a7eaf510f010a0dbc7648e7eb4aeb5ce
Darren Conley <dcprime@gmail.com>
2014-10-13 16:02:58 +0000

    added tests to Reviews Controller Spec

--- spec/controllers/reviews_controller_spec.rb
+++ spec/controllers/reviews_controller_spec.rb
@@ -2,19 +2,22 @@ require 'spec_helper'
 
 describe ReviewsController do
   describe "POST create" do
-    it "creates a new review object"
-    it "associates the review with the video"
-    it "associates the review with the current user"
-    context "the data is valid" do
-      it "saves the review"
-      it "redirects to the 'video/show' page"
+    context "athenticated users" do
+      it "creates a new review object"
+      it "associates the review with the video"
+      it "associates the review with the current user"
+      context "the data is valid" do
+        it "saves the review"
+        it "redirects to the 'video/show' page"
+      end
+      context "the data is not valid" do
+        it "does not save the review"
+        it "renders the 'video/show' page"
+        it "shows the errors"
+      end
     end
-    context "the data is not valid" do
-      it "does not save the review"
-      it "renders the 'video/show' page"
-      it "shows the errors"
-    end
-    
-    
+    context "unauthenticated users" do
+      it "redirects to the sign-in page"
+    end    
   end
 end
