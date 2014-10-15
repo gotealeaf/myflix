7a37b9db2ad959d901dd14a242008b1bdff05a6c
Darren Conley <dcprime@gmail.com>
2014-10-15 01:22:10 +0000

    added destroy queue_item function using TDD

--- app/controllers/queue_items_controller.rb
+++ app/controllers/queue_items_controller.rb
@@ -12,6 +12,12 @@ class QueueItemsController < ApplicationController
     redirect_to my_queue_path
   end
   
+  def destroy
+    queue_item = QueueItem.find(params[:id])
+    queue_item.destroy if queue_item.user_id == current_user.id
+    redirect_to my_queue_path
+  end
+  
   private
   
   def queue_video(video)
--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,6 @@ Myflix::Application.routes.draw do
   resources :users, only: [:create, :edit, :show]  
   resources :categories, only: [:show]
   resources :sessions, only: [:create]
-  resources :queue_items, only: [:create]
+  resources :queue_items, only: [:create, :destroy]
   
 end
