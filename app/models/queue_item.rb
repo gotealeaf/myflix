ad6140b8d8a7c2052d93e85245f54f8fe4e5afc5
Darren Conley <dcprime@gmail.com>
2014-10-17 12:18:00 +0000

    small refactors

--- app/models/queue_item.rb
+++ app/models/queue_item.rb
@@ -20,4 +20,31 @@ class QueueItem < ActiveRecord::Base
     video.categories.first
   end
   
+  def queue_video(video)
+    QueueItem.create(video: video, user: current_user, position: new_queue_item_position) unless current_user_queued_video?(video)
+  end
+  
+  def new_queue_item_position
+    current_user.queue_items.count + 1
+  end
+  
+  def current_user_queued_video?(video)
+    current_user.queue_items.map(&:video).include?(video)
+  end
+  
+  def update_queue_items
+    ActiveRecord::Base.transaction do
+      params[:queue_items].each do |queue_item_data|
+        queue_item = QueueItem.find(queue_item_data[:id])
+        queue_item.update_attributes!(position: queue_item_data[:position]) if queue_item.user == current_user
+      end
+    end
+  end
+  
+  def normalize_queue_item_positions
+    current_user.queue_items.each_with_index do |queue_item, index|
+      queue_item.update_attributes(position: index + 1)
+    end
+  end
+  
 end
