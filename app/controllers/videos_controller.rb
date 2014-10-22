060e7c9547341c16c32819ed60bef8bfee584fe2
Arjun Rajkumar <arjunrajkumars@gmail.com>
2014-10-22 13:47:01 +0530

    pre-curse work finished

---
+++ app/controllers/videos_controller.rb
@@ -0,0 +1,10 @@
+class VideosController < ApplicationController
+  def index
+    @videos = Video.all
+    @categories = Category.all
+  end
+
+  def show
+    @video = Video.find(params[:id])
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+comedy = Category.create(name: "Comedy")
+drama = Category.create(name: "Drama")
+
+
+
+Video.create(
+            title: "Family Guy", 
+            description: "Follow the adventures of an endearingly ignorant dad, PETER GRIFFIN, and his hilariously odd family.",
+            small_cover_url: "/tmp/family_guy.jpg",
+            large_cover_url: "/tmp/family_guy.jpg",
+            category: comedy
+            )
+Video.create(
+            title: "Monk", 
+            description: "Monk is an American comedy-drama detective mystery television series created by Andy Breckman and starring Tony Shalhou.",
+            small_cover_url: "/tmp/monk.jpg",
+            large_cover_url: "/tmp/monk_large.jpg",
+            category: drama
+            )
+Video.create(
+            title: "South Park", 
+            description: "South Park is an American adult animated sitcom created by Trey Parker and Matt Stone for the Comedy Central television network.",
+            small_cover_url: "/tmp/south_park.jpg",
+            large_cover_url: "/tmp/south_park.jpg",
+            category: comedy
+            )
