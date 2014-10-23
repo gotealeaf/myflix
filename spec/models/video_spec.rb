69952c77e112096b398bb0bf067f1919df16f124
Arjun Rajkumar <arjunrajkumars@gmail.com>
2014-10-23 16:08:51 +0530

    tested for video validations

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -22,5 +22,23 @@ describe Video do
             )
     expect(vid1.category).to eq(comedy)
   end
+
+  it "has has to have title before saving" do
+    vid1 = Video.create(
+            description: "Follow the adventures of an endearingly ignorant dad, PETER GRIFFIN, and his hilariously odd family.",
+            small_cover_url: "/tmp/family_guy.jpg",
+            large_cover_url: "/tmp/family_guy.jpg"
+            )
+    expect(Video.count).to eq(0)
+  end
+
+  it "has has to have description before saving" do
+    vid1 = Video.create(
+            title: "Comedy1", 
+            small_cover_url: "/tmp/family_guy.jpg",
+            large_cover_url: "/tmp/family_guy.jpg"
+            )
+    expect(Video.count).to eq(0)
+  end
 end
 
