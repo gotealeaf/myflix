7a305cb28da929b28d484455a74efa3dae561ab6
Arjun Rajkumar <arjunrajkumars@gmail.com>
2014-10-23 16:21:32 +0530

    implemented shoulda-matchers

--- spec/models/controller_spec.rb
+++ spec/models/controller_spec.rb
@@ -1,31 +1,6 @@
 require 'spec_helper'
 
 describe Category do 
-  it "saves itself" do
-    cat = Category.new(name: "Comedy")
-    cat.save 
-    expect(Category.first).to eq(cat)
-  end
-
-  it "has many videos" do 
-    comedy = Category.create(name: "Comedy")
-        
-    vid1 = Video.create(
-            title: "Comedy1", 
-            description: "Follow the adventures of an endearingly ignorant dad, PETER GRIFFIN, and his hilariously odd family.",
-            small_cover_url: "/tmp/family_guy.jpg",
-            large_cover_url: "/tmp/family_guy.jpg",
-            category: comedy
-            )
-    vid2 = Video.create(
-            title: "Comedy2", 
-            description: "Monk is an American comedy-drama detective mystery television series created by Andy Breckman and starring Tony Shalhou.",
-            small_cover_url: "/tmp/monk.jpg",
-            large_cover_url: "/tmp/monk_large.jpg",
-            category: comedy
-            )
-    
-    expect(comedy.videos).to eq([vid1, vid2])
-  end
+  it { should have_many(:videos)}
 end
 
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -1,44 +1,8 @@
 require 'spec_helper'
 
 describe Video do 
-  it "saves itself" do 
-    video = Video.new(
-                    title: "Family Guy", 
-                    description: "Follow the adventures of an endearingly ignorant dad, PETER GRIFFIN, and his hilariously odd family.",
-                    small_cover_url: "/tmp/family_guy.jpg"
-                    )
-    video.save
-    expect(Video.first).to eq(video)
-  end
-
-  it "belongs to category" do
-    comedy = Category.create(name: "Comedy")
-    vid1 = Video.create(
-            title: "Comedy1", 
-            description: "Follow the adventures of an endearingly ignorant dad, PETER GRIFFIN, and his hilariously odd family.",
-            small_cover_url: "/tmp/family_guy.jpg",
-            large_cover_url: "/tmp/family_guy.jpg",
-            category: comedy
-            )
-    expect(vid1.category).to eq(comedy)
-  end
-
-  it "has has to have title before saving" do
-    vid1 = Video.create(
-            description: "Follow the adventures of an endearingly ignorant dad, PETER GRIFFIN, and his hilariously odd family.",
-            small_cover_url: "/tmp/family_guy.jpg",
-            large_cover_url: "/tmp/family_guy.jpg"
-            )
-    expect(Video.count).to eq(0)
-  end
-
-  it "has has to have description before saving" do
-    vid1 = Video.create(
-            title: "Comedy1", 
-            small_cover_url: "/tmp/family_guy.jpg",
-            large_cover_url: "/tmp/family_guy.jpg"
-            )
-    expect(Video.count).to eq(0)
-  end
+  it { should belong_to(:category)}
+  it { should validate_presence_of(:title)}
+  it { should validate_presence_of(:description)}
 end
 
