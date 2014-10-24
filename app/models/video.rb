86ba1ce171353420f54475064e48f20bdcc1a01e
Arjun Rajkumar <arjunrajkumars@gmail.com>
2014-10-24 10:49:32 +0530

    implemented 'search_by_title' on the video model

--- app/models/video.rb
+++ app/models/video.rb
@@ -3,7 +3,8 @@ class Video < ActiveRecord::Base
   validates :title, :description, presence: true
 
   def self.search_by_title(search_term)
-
+    return [] if search_term.blank?
+    where("title LIKE ?", "%#{search_term}%").order("created_at DESC")
   end
 end
 
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -5,7 +5,38 @@ describe Video do
   it { should validate_presence_of(:title)}
   it { should validate_presence_of(:description)}
 
-  it "returns true if title is present"
-  it "returns false if title is absent"
+  describe "search_by_title" do 
+    it "returns empty array if there is match" do 
+      homeland = Video.create(title: "Homeland", description: "Spy Stories")
+      dexter = Video.create(title: "Dexter", description: "Detective")
+      expect(Video.search_by_title("hello")).to eq([])
+    end
+
+
+    it "returns array of one video for exact match" do
+      homeland = Video.create(title: "Homeland", description: "Spy Stories")
+      dexter = Video.create(title: "Dexter", description: "Detective")
+      expect(Video.search_by_title("Homeland")).to eq([homeland])
+    end
+
+    it "returns array of one video for partial match" do 
+      homeland = Video.create(title: "Homeland", description: "Spy Stories")
+      dexter = Video.create(title: "Dexter", description: "Detective")
+      expect(Video.search_by_title("Home")).to eq([homeland])
+    end
+    
+    it "returns array of multiple vidoes of all matches ordered by created_at" do
+      homeland = Video.create(title: "Homeland", description: "Spy Stories", created_at: 1.day.ago)
+      dexter = Video.create(title: "Dexter", description: "Detective")
+      expect(Video.search_by_title("e")).to eq([dexter, homeland])
+    end
+  
+    it "returns an empty array for a serh with an empty string" do
+      homeland = Video.create(title: "Homeland", description: "Spy Stories", created_at: 1.day.ago)
+      dexter = Video.create(title: "Dexter", description: "Detective")
+      expect(Video.search_by_title("")).to eq([])
+    end
+  end 
+  
 end
 
