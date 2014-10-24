0a2dc2004766caf8653d3150ba5c919559affd86
Arjun Rajkumar <arjunrajkumars@gmail.com>
2014-10-24 16:20:50 +0530

    implemented recent videos using TDD

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,8 @@
 class Category < ActiveRecord::Base
-  has_many :videos, -> { order("title")}
+  has_many :videos, -> { order("created_at DESC")}
   validates :name, presence: true
+
+  def recent_videos
+    videos.first(6)
+  end 
 end
--- spec/models/controller_spec.rb
+++ spec/models/controller_spec.rb
@@ -2,5 +2,36 @@ require 'spec_helper'
 
 describe Category do 
   it { should have_many(:videos)}
+
+describe "#recent_videos" do
+  
+  it "returns empty array if no movies in category" do 
+    comedy = Category.create(name: "Comedy")
+    expect(comedy.recent_videos).to eq([])
+  end
+
+  it "returns the entire array if no. of movies in category is 6 or less sorted by created_date" do
+    comedy = Category.create(name: "Comedy")
+    f_guy0 = Video.create(title: "Family Guy0", description: "Follow the adventures", category: comedy, created_at: 3.day.ago)
+    f_guy1 = Video.create(title: "Family Guy1", description: "Follow the adventures", category: comedy, created_at: 2.day.ago)
+    f_guy2 = Video.create(title: "Family Guy2", description: "Follow the adventures", category: comedy, created_at: 1.day.ago)
+    expect(comedy.recent_videos).to eq([f_guy2, f_guy1, f_guy0])
+  end
+
+  it "returns the recent 6 videos from in this categoy sorted by created_date" do
+    comedy = Category.create(name: "Comedy")
+    f_guy0 = Video.create(title: "Family Guy0", description: "Follow the adventures", category: comedy, created_at: 10.day.ago)
+    f_guy1 = Video.create(title: "Family Guy1", description: "Follow the adventures", category: comedy, created_at: 9.day.ago)
+    f_guy2 = Video.create(title: "Family Guy2", description: "Follow the adventures", category: comedy, created_at: 8.day.ago)
+    f_guy3 = Video.create(title: "Family Guy3", description: "Follow the adventures", category: comedy, created_at: 7.day.ago)
+    f_guy4 = Video.create(title: "Family Guy4", description: "Follow the adventures", category: comedy, created_at: 6.day.ago)
+    f_guy5 = Video.create(title: "Family Guy5", description: "Follow the adventures", category: comedy, created_at: 5.day.ago)
+    f_guy6 = Video.create(title: "Family Guy6", description: "Follow the adventures", category: comedy, created_at: 4.day.ago)
+    f_guy7 = Video.create(title: "Family Guy7", description: "Follow the adventures", category: comedy, created_at: 3.day.ago)
+    f_guy8 = Video.create(title: "Family Guy8", description: "Follow the adventures", category: comedy, created_at: 2.day.ago)
+    expect(comedy.recent_videos.count).to eq(6)
+  end
+end
+
 end
 
