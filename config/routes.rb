da157782b4b8f006496bd4256a198be2cbf717b6
Arjun Rajkumar <arjunrajkumars@gmail.com>
2014-10-24 12:41:11 +0530

    implemented search functionality

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Myflix::Application.routes.draw do
 
   get '/home', to: 'videos#index'
 
-  resources :videos, only: [:show]
-
+  resources :videos, only: [:show] do
+    collection do 
+      get :search, to: "videos#search"
+    end
+  end
+  
   resources :categories, only: [:show]
 end
