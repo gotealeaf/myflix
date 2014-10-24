13d6ed54af5341881d244904822b6fced61fe64c
Arjun Rajkumar <arjunrajkumars@gmail.com>
2014-10-24 09:58:39 +0530

    implementing search_by_title

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,4 +1,9 @@
 class Video < ActiveRecord::Base
   belongs_to :category
   validates :title, :description, presence: true
+
+  def self.search_by_title(search_term)
+
+  end
 end
+
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -4,5 +4,8 @@ describe Video do
   it { should belong_to(:category)}
   it { should validate_presence_of(:title)}
   it { should validate_presence_of(:description)}
+
+  it "returns true if title is present"
+  it "returns false if title is absent"
 end
 
