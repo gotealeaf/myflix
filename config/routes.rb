86df96fa6e81d413604a1bf0a7e3c5c1188d4f76
Arjun Rajkumar <arjunrajkumars@gmail.com>
2014-10-28 09:48:21 +0530

    implemented front page

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Myflix::Application.routes.draw do
-  get 'ui(/:action)', controller: 'ui'
-
-  get '/home', to: 'videos#index'
-
+  root to: "pages#front"
+  
   resources :videos, only: [:show] do
     collection do 
       get :search, to: "videos#search"
     end
   end
-  
   resources :categories, only: [:show]
+
+  get 'ui(/:action)', controller: 'ui'
+  get 'register', to: "users#new"
+  
+  resources :users, only: [:create]
 end
