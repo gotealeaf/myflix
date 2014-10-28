454fdcbd582553683a1f856ba2f843af21e565b1
Arjun Rajkumar <arjunrajkumars@gmail.com>
2014-10-28 10:57:38 +0530

    implemented authentication

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,4 +1,6 @@
 class VideosController < ApplicationController
+  before_filter :require_user
+  
   def index
     @categories = Category.all
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Myflix::Application.routes.draw do
   root to: "pages#front"
-  
+  get 'home', to: "videos#index"
+
   resources :videos, only: [:show] do
     collection do 
       get :search, to: "videos#search"
@@ -11,6 +12,7 @@ Myflix::Application.routes.draw do
   get 'ui(/:action)', controller: 'ui'
   get 'register', to: "users#new"
   get 'sign_in', to: "sessions#new"
+  get 'sign_out', to: 'sessions#destroy'
 
   resources :users, only: [:create]
   resources :sessions, only: [:create]
