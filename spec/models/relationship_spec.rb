1d05b72fbd0f5739e467ca4602b1638fe129a1cf
Darren Conley <dcprime@gmail.com>
2014-10-29 19:34:45 +0000

    updated validation test for Relationship model

--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -6,4 +6,11 @@ describe Relationship do
   it { should belong_to(:following).class_name('User').with_foreign_key('following_id') }
   it { should validate_uniqueness_of(:following_id).scoped_to(:follower_id) }
   
+  it "raises a validation error if user tries to follow themself" do
+    darren = Fabricate(:user)
+    relationship = Relationship.new(follower: darren, following: darren)
+    relationship.valid?
+    expect(relationship.errors.full_messages).to include ('Follower cannot be the same person as the one being followed')
+  end
+  
 end
