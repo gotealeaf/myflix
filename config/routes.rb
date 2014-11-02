eaf13359cb02fc8e4d85780f43f6586d3827b02c
Arjun Rajkumar <arjunrajkumars@gmail.com>
2014-11-02 10:11:09 +0530

    added ability to add and remove videos from queue

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Myflix::Application.routes.draw do
   get 'sign_out', to: 'sessions#destroy'
   get 'my_queue', to: 'queue_items#index'
   
+  resources :queue_items, only: [:create, :destroy]
   resources :users, only: [:create]
   resources :sessions, only: [:create]
 end
