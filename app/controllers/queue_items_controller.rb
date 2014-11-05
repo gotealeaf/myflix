98ff13dd8375fa6eddbfef4ba00fb50a8624d5e6
Arjun Rajkumar <arjunrajkumars@gmail.com>
2014-11-05 15:11:53 +0530

    refactor queue controller

--- app/controllers/queue_items_controller.rb
+++ app/controllers/queue_items_controller.rb
@@ -14,14 +14,14 @@ class QueueItemsController < ApplicationController
   def destroy
     queue_item = QueueItem.find(params[:id])
     queue_item.destroy if current_user.queue_items.include?(queue_item)
-    normalize_queue_item_positions
+    current_user.normalize_queue_item_positions
     redirect_to my_queue_path
   end
 
   def update_queue
     begin
       update_queue_items
-      normalize_queue_item_positions
+      current_user.normalize_queue_item_positions
     rescue ActiveRecord::RecordInvalid
       flash[:error] = "Invalid position numbers"
     end
@@ -43,16 +43,10 @@ class QueueItemsController < ApplicationController
 
   def update_queue_items
     ActiveRecord::Base.transaction do 
-        params[:queue_items].each do |queue_item_data|
-          queue_item = QueueItem.find(queue_item_data["id"])
-          queue_item.update_attributes!(position: queue_item_data["position"]) if queue_item.user == current_user
-        end
+      params[:queue_items].each do |queue_item_data|
+        queue_item = QueueItem.find(queue_item_data["id"])
+        queue_item.update_attributes!(position: queue_item_data["position"]) if queue_item.user == current_user
       end
-  end
-
-  def normalize_queue_item_positions
-    current_user.queue_items.each_with_index do |queue_item, index|
-      queue_item.update_attributes(position: index+1)
     end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,11 @@ class User < ActiveRecord::Base
   has_secure_password validations: false
 
   has_many :queue_items,-> { order "position asc" }
+
+  def normalize_queue_item_positions
+    self.queue_items.each_with_index do |queue_item, index|
+      queue_item.update_attributes(position: index+1)
+    end
+  end
+
 end
