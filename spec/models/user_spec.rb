eac73645beba9894acaf54f899e99edeb27f30e1
Arjun Rajkumar <arjunrajkumars@gmail.com>
2014-11-08 18:21:47 +0530

    testing feature spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,4 +6,18 @@ describe User do
   it { should validate_presence_of(:full_name) }
   it { should have_secure_password }
   it { should have_many(:queue_items).order('position asc') }
+
+  describe "#queued_video?" do 
+    it "returns true when the user queued the video" do 
+      user = Fabricate(:user)
+      video = Fabricate(:video)
+      Fabricate(:queue_item, user:user, video:video)
+      user.queued_video?(video).should be_true
+    end
+    it "returns false when the user has not queued the video" do 
+      user = Fabricate(:user)
+      video = Fabricate(:video)
+      user.queued_video?(video).should be_false
+    end
+  end
 end
