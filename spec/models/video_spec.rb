6c1be6e1bf5dcd74a207a4f1769622d3aadb93d3
figochen <figocia@hotmail.com>
2014-12-16 00:44:14 +1100

    rspec

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -1,19 +1,18 @@
 require 'spec_helper'
 
 describe Video do 
-  it 'should_save' do
-    Video.all.each do |v|
-      v.delete
-    end
-
+  it 'should_save' do    
+    # Video.all.each do |v|
+    #   v.delete
+    # end
     video = Video.create    
-    Video.first.should == video  
+    # Video.first.should == video  
+    expect(Video.first).to eq(video)
   end  
 end
 
 
 describe Category do
-  it 'should has many Video' do
-    
+  it 'should has many Video' do    
   end
 end
