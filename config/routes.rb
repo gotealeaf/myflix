4f78bb49032efa7e6f9894eb73c299cea8f60245
figochen <figocia@hotmail.com>
2014-12-29 18:57:50 +1100

    formatting

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Myflix::Application.routes.draw do
-	root to: 'pages#front'
-	get '/home', to: 'videos#index'
+  root to: 'pages#front'
+  get '/home', to: 'videos#index'
   get 'ui(/:action)', controller: 'ui'
   get '/registration', to: 'users#new'
   
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
+
 
   resources :videos do
     collection do
