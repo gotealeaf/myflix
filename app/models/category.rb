ccf9232f12e4ca243d346cfdec1c9955e7ce5889
figochen <figocia@hotmail.com>
2015-01-07 17:44:33 +1100

    remove commented code in category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,9 +1,8 @@
-class Category < ActiveRecord::Base
+、class Category < ActiveRecord::Base
 	has_many :video_categories
 	has_many :videos, -> { order 'title' }, through: :video_categories
 
-  def recent_videos
-    # @recent_videos = videos.order(created_at: :desc).take(6)
+  def recent_videos    
     @recent_videos = videos.sort_by{|v| v.created_at}.reverse.take(6)
   end
 end
