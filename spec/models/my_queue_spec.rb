1f538bfe0cad9d594ea850e7ef157ea9ab60f8f4
figochen <figocia@hotmail.com>
2015-01-12 05:49:34 +1100

    remove my queue object because not neccessary

--- spec/models/my_queue_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'rails_helper'
-
-describe MyQueue do
-  it { should have_many(:videos).through(:my_queue_videos) }
-  it { should have_many(:my_queue_videos)}
-  it { should belong_to(:user) }
-end
