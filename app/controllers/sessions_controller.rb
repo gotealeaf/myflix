9a5f6dba4240f86ecc8808b3a2064a46140f083b
figochen <figocia@hotmail.com>
2015-01-27 08:38:42 +1100

    some info notice changes...

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,9 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
-      session[:user_id] = user.id      
-      redirect_to home_path, notice: 'Welcome, you have logged in!'
+      session[:user_id] = user.id   
+      flash[:notice] = 'Welcome, you have logged in!'
+      redirect_to home_path
     else
       flash[:error] = 'Username or password does not match.'
       redirect_to login_path
@@ -12,7 +13,8 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    session[:user_id] = nil    
-    redirect_to root_path, notice: 'You have logged out.'
+    session[:user_id] = nil   
+    flash[:notice]  = 'You have logged out.'
+    redirect_to root_path
   end
 end
