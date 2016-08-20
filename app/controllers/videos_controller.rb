b73d3687e8977c73f8d703f8c4cb7824df146ab1
Letladi <sletladi@gmail.com>
2016-08-21 00:02:49 +0200

    created video show page

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,5 +1,16 @@
 class VideosController < ApplicationController
+	before_action :get_video, only: [:show]
+
 	def index
 		@videos = Video.all
+	end
+
+	def show
+	end
+
+	private
+
+	def get_video
+		@video = Video.find(params[:id])
 	end
 end
