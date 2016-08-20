4556fb0f7b53ff09b1e893421ce167420d31554b
Letladi <sletladi@gmail.com>
2016-08-20 23:34:27 +0200

    created home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Myflix::Application.routes.draw do
   get 'ui(/:action)', controller: 'ui'
+
+  resources :videos, only: [:index, :show]
+  get 'home', to: 'videos#index' 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Video.create(title: 'Family Guy', 
+	description: "Family Guy is an American adult animated sitcom created by Seth MacFarlane for the Fox Broadcasting Company.",
+	small_cover_url: '/tmp/family_guy.jpg', large_cover_url: '/tmp/family_guy.jpg')
+
+Video.create(title: 'South Africa',
+	description: 'Adult animation series',
+	small_cover_url: '/tmp/south_park.jpg',
+	large_cover_url: '/tmp/south_park.jpg')
+
+Video.create(title: 'Monk',
+	description: 'Highly anticipated movie on monks',
+	small_cover_url: '/tmp/monk.jpg',
+	large_cover_url: '/tmp/large_large.jpg')
