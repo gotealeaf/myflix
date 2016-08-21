ccac845dca99f04bf29955f5750e0d010ce591ac
Letladi <sletladi@gmail.com>
2016-08-21 15:10:17 +0200

    completed search_by_title feature for videos

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160820222129) do
+ActiveRecord::Schema.define(version: 20160821111425) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,11 +21,13 @@ ActiveRecord::Schema.define(version: 20160820222129) do
   end
 
   create_table "videos", force: true do |t|
-    t.string  "title"
-    t.text    "description"
-    t.string  "small_cover_url"
-    t.string  "large_cover_url"
-    t.integer "category_id"
+    t.string   "title"
+    t.text     "description"
+    t.string   "small_cover_url"
+    t.string   "large_cover_url"
+    t.integer  "category_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "videos", ["category_id"], name: "index_videos_on_category_id", using: :btree
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -4,4 +4,32 @@ describe Video do
 	it { should belong_to(:category) }
 	it { should validate_presence_of(:title) }
 	it { should validate_presence_of(:description) }
+
+	describe 'search_by_title' do 
+		it 'returns an empty array when no records are found' do 
+			futurama = Video.create(title: 'Futurama', description: 'space travel')
+			back_to_the_future = Video.create(title: 'Back to The Future', description: 'Time Travel!')
+			expect(Video.search_by_title('hello')).to eq([])
+		end
+		it 'returns an array of one video for an exact match' do 
+			futurama = Video.create(title: 'Futurama', description: 'space travel')
+			back_to_the_future = Video.create(title: 'Back to The Future', description: 'Time Travel!')
+			expect(Video.search_by_title('Futurama')).to eq([futurama])
+		end
+		it 'returns an array of one video for a partial match' do
+			futurama = Video.create(title: 'Futurama', description: 'space travel')
+			back_to_the_future = Video.create(title: 'Back to The Future', description: 'Time Travel!')
+			expect(Video.search_by_title('urama')).to eq([futurama])
+		end
+		it 'returns an array of all matches ordered by created_at' do 
+			futurama = Video.create(title: 'Futurama', description: 'space travel', created_at: 1.day.ago)
+			back_to_the_future = Video.create(title: 'Back to The Future', description: 'Time Travel!')
+			expect(Video.search_by_title('Futur')).to eq([back_to_the_future, futurama])
+		end
+		it 'returns an empty array for a search with an empty string' do 
+			futurama = Video.create(title: 'Futurama', description: 'space travel', created_at: 1.day.ago)
+			back_to_the_future = Video.create(title: 'Back to The Future', description: 'Time Travel!')
+			expect(Video.search_by_title('')).to eq([])
+		end
+	end
 end
